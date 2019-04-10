50f4ad81210eaba1def332fb397b7ce9aed5a6d7
mimarz <mimarz@gmail.com>
2019-04-10 15:17:38 +0200

    Updated to use az for login to registry

--- onepager/deploy.sh
+++ onepager/deploy.sh
@@ -1,3 +1,5 @@
+az account set --subscription "Omnia Application Workspace - Non-Production - 02"
+az acr login --name edscontainers
 docker build . -t eds-onepager
 docker tag eds-onepager:latest edscontainers.azurecr.io/eds-onepager
 docker push edscontainers.azurecr.io/eds-onepager
