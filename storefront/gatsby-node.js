0b724fbc46a972e1ac54113aec3d23b14abec375
Victor Nystad <vnystad@gmail.com>
2019-04-29 12:25:10 +0200

    Add tabs, navigation and placeholder pages (#16)
    * show scroll track weather or not scrolling is neccessary
    
    …to avoid things jumping back and forth horizontally when navigating
    
    * add wikipages temporarily
    
    will be replaced after review
    
    * add content
    
    * add all the files
    
    * add content
    
    * add basic responsiveness
    
    * update files
    
    * update sidebar

---
+++ storefront/gatsby-node.js
@@ -0,0 +1,135 @@
+const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const { copy } = require('fs-extra');
+const { graphql } = require('gatsby')
+
+// Method that creates nodes based on the file system that we can use in our templates
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  // If the node type (file) is a markdown file
+  if (node.internal.type === 'Mdx') {
+    const dir = path.resolve(__dirname, '');
+    const fileNode = getNode(node.parent);
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `content`,
+      trailingSlash: false,
+    });
+
+    const currentPage = slug.split('/').pop();
+    const currentCategory = slug.split('/').filter( item => item.length > 0 ).shift() || 'homepage'
+
+    console.log('currentPage', currentPage)
+    console.log('currentCategory', currentCategory)
+
+    // example
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+
+    // example: react
+    createNodeField({
+      node,
+      name: `currentPage`,
+      value: currentPage,
+    });
+
+    createNodeField({
+      node,
+      name: `currentCategory`,
+      value: currentCategory,
+    })
+  }
+};
+
+// Method that creates the pages for our website
+exports.createPages = async ({ actions, graphql }) => {
+  const { createRedirect, createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allMdx {
+        edges {
+          node {
+            code {
+              scope
+            }
+            fields {
+              slug
+              currentPage
+              currentCategory
+            }
+            frontmatter {
+              tabs
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const pages = result.data.allMdx.edges
+
+  pages.map(({ node }) => {
+    const slug = node.fields.slug;
+    const currentPage = node.fields.currentPage;
+    const tabs = node.frontmatter.tabs === null ? [] : node.frontmatter.tabs;
+
+    /**
+     * index-pages are called the same as the folder, this removes the last segment
+     * so /resources/resources.mdx becomes /resources/
+     */
+    const currentPath =
+      node.frontmatter.tabs === null
+        ? slug.slice(0, slug.lastIndexOf(currentPage))
+        : slug;
+
+    createPage({
+      path: currentPath,
+      component: path.resolve(`./src/templates/page.js`),
+      context: {
+        slug,
+        currentPage,
+      },
+    });
+
+    /* we have tabs */
+    if (tabs.length > 1) {
+      const indexFilename = tabs[0].toLowerCase().split(' ').join('-');
+      const isIndex = currentPath.lastIndexOf(indexFilename) > 0
+
+      if (isIndex) {
+        const pathToParentDir = currentPath.slice(0, currentPath.lastIndexOf(indexFilename))
+
+        /* create index page for category */
+        createPage({
+          path: pathToParentDir,
+          component: path.resolve(`./src/templates/page.js`),
+          context: {
+            slug,
+            currentPage: `${pathToParentDir}${indexFilename}`,
+          },
+        });
+
+        /**
+         * Redirects from parent directory to the first page in tab-group.
+         * The first redirect with trailing slash, the second without
+         */
+        // createRedirect({
+        //   fromPath: `${pathToParentDir}`,
+        //   redirectInBrowser: true,
+        //   toPath: `${pathToParentDir}${indexFilename}`,
+        // });
+
+        // createRedirect({
+        //   fromPath: `${pathToParentDir.slice(0, pathToParentDir.length - 1)}`,
+        //   redirectInBrowser: true,
+        //   toPath: `${pathToParentDir}${indexFilename}`,
+        // });
+      }
+    }
+  });
+};
--- storefront/src/components/layout.js
+++ storefront/src/components/layout.js
@@ -1,34 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-
+import { Location } from '@reach/router'
 import Header from './header'
-import Sidebar from './sidebar'
+import Sidebar from './Sidebar'
 import './layout.css'
 
-const Layout = ({ children, location }) => (
-    <StaticQuery
-        query={graphql`
-            query SiteTitleQuery {
-                site {
-                    siteMetadata {
-                        title
-                    }
-                }
-            }
-        `}
-        render={data => (
-            <div className="Page">
-                <input id="MenuToggler" className="MenuToggler" type="checkbox" />
-                <Header />
-                <Sidebar location={ location } />
-                <nav className="TOC">TOC</nav>
-                <main className="Main">
-                    {children}
-                </main>
-            </div>
-        )}
-    />
+const Layout = ({ children }) => (
+  <div className="Page">
+    <input id="MenuToggler" className="MenuToggler" type="checkbox" />
+    <Header />
+    <Location>
+      {
+        ({ location }) => <Sidebar location={ location } />
+      }
+    </Location>
+    <nav className="TOC">TOC</nav>
+    <main className="Main">
+        { children }
+    </main>
+  </div>
 )
 
 Layout.propTypes = {
