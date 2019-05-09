6a66c414746c377543b1465a6c97bda616048552
Victor Nystad <vnystad@gmail.com>
2019-05-09 16:33:30 +0200

    update component status

--- storefront/src/components/ComponentStatus/ComponentStatus.jsx
+++ storefront/src/components/ComponentStatus/ComponentStatus.jsx
@@ -24,8 +24,45 @@ const ComponentStatus = () => {
     }
   `)
 
+  const headers = data.allComponentStatusYaml.edges[0].node.headers
+  const components = data.allComponentStatusYaml.edges[0].node.components
+  const camelify = title => title.toLowerCase().split(' ').map((word, index) => (
+    index > 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word
+  )).join('')
+
   return (
-    <p>Component Status</p>
+    <table className='ComponentSummaryTable' summary='some summary'>
+      <caption>Some caption</caption>
+      <thead>
+        <tr>
+          {
+            headers.map((text, index) => (
+              <th key={index}>{ text }</th>
+            ))
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          components.map((component, index) => (
+            <tr>
+              <th>{ component.component }</th>
+              {
+                Object.keys(component.status).map((key, index) => (
+                  <td>
+                    {
+                      component.status[key].map((status, index) => (
+                        <span className={`StatusBadge StatusBadge--${camelify(status)}`}>{ status }</span>
+                      ))
+                    }
+                  </td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   )
 }
 
