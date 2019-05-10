a5bf55bb8d234d171940b4a1d67e4734508bef30
Victor Nystad <vnystad@gmail.com>
2019-05-10 13:48:04 +0200

    update styling

--- storefront/src/components/ComponentStatus/ComponentStatus.jsx
+++ storefront/src/components/ComponentStatus/ComponentStatus.jsx
@@ -2,6 +2,7 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 import classNames from 'classnames'
+import styled from 'styled-components'
 
 const ComponentStatus = () => {
   const data = useStaticQuery(graphql`
@@ -10,6 +11,7 @@ const ComponentStatus = () => {
         edges {
           node {
             headers
+            summary
             components {
               component
               status {
@@ -25,45 +27,127 @@ const ComponentStatus = () => {
   `)
 
   const headers = data.allComponentStatusYaml.edges[0].node.headers
+  const summary = data.allComponentStatusYaml.edges[0].node.summary
   const components = data.allComponentStatusYaml.edges[0].node.components
   const camelify = title => title.toLowerCase().split(' ').map((word, index) => (
     index > 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word
   )).join('')
 
   return (
-    <table className='ComponentSummaryTable' summary='some summary'>
-      <caption>Some caption</caption>
+    <Table summary={summary}>
       <thead>
-        <tr>
+        <Tr>
           {
             headers.map((text, index) => (
-              <th key={index}>{ text }</th>
+              <ThH key={camelify(text)} scope='col'>{ text }</ThH>
             ))
           }
-        </tr>
+        </Tr>
       </thead>
       <tbody>
         {
           components.map((component, index) => (
-            <tr>
-              <th>{ component.component }</th>
+            <Tr key={component.component + index}>
+              <ThV key={component.component + index} scope='row'>{ component.component }</ThV>
               {
-                Object.keys(component.status).map((key, index) => (
-                  <td>
-                    {
-                      component.status[key].map((status, index) => (
-                        <span className={`StatusBadge StatusBadge--${camelify(status)}`}>{ status }</span>
-                      ))
-                    }
-                  </td>
+                Object.values(component.status).map((statuses, index) => (
+                  <Td key={camelify(statuses.join('-')) + index}>
+                    <Badges>
+                      {
+                        statuses.map((status, index) => (
+                          <Badge
+                            key={camelify(status) + index}
+                            variant={camelify(status)}
+                          >
+                            { status }
+                          </Badge>
+                        ))
+                      }
+                    </Badges>
+                  </Td>
                 ))
               }
-            </tr>
+            </Tr>
           ))
         }
       </tbody>
-    </table>
+    </Table>
   )
 }
+
+const Table = styled.table`
+  border-collapse: collapse;
+  * + * {
+    margin-top: 0;
+  }
+`
+
+Table.displayName = 'Table'
+
+const Tr = styled.tr`
+  tbody &:nth-child(odd) {
+    th, td {
+      background: white;
+    }
+  }
+`
+
+Tr.displayName = 'TableRow'
+
+const cellPadding = `
+  padding: 0.75em 1.5em;
+`
+
+const ThH = styled.th`
+  text-align: left;
+  ${cellPadding}
+  font-weight: 500;
+`
+
+ThH.displayName = 'TableHeaderHorizontal'
+
+const ThV = styled.th`
+  text-align: left;
+  ${cellPadding}
+  font-weight: 400;
+`
+
+ThV.displayName = 'TableHeaderVertical'
+
+const Td = styled.td`
+  ${cellPadding}
+`
+
+Td.displayName = 'TableCell'
+
+const badgeBgColors = {
+  underConstruction: 'lightgrey',
+  updating: 'lightgrey',
+  new: 'lightgrey',
+  notAvailable: 'lightgrey'
+}
+
+const Badges = styled.div`
+  display: inline-grid;
+  grid-gap: 0.5rem;
+  grid-auto-flow: column;
+`
+
+Badges.displayName = 'Badges'
+
+const Badge = styled.span`
+  background: ${({ variant }) => badgeBgColors[variant]};
+  margin-top: 0 !important; /* TODO: Remove lobotomized owl selector */
+  font-size: 0.625em;
+  border-radius: 1em;
+  display: inline-block;
+  height: 2em;
+  line-height: 2em;
+  white-space: nowrap;
+  padding-left: 1em;
+  padding-right: 1em;
+`
+
+Badge.displayName = 'Badge'
 
 export default ComponentStatus
