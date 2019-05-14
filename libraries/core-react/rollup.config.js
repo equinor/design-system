8a58cc55339e5ad2132e1ac17652208aaa68b23e
batfink <vnystad@gmail.com>
2019-05-14 14:57:07 +0200

    update rollup config

--- libraries/core-react/rollup.config.js
+++ libraries/core-react/rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import pkg from "./package.json.js.js";
+import pkg from "./package.json";
 
 const peerDeps = Object.keys(pkg.peerDependencies || {});
 
