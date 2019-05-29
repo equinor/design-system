7ceaf14fc252ad0661af4d6442e29a64a9ab1b50
Victor Nystad <vnystad@gmail.com>
2019-05-29 12:44:09 +0200

    add missing dependencies to eslint-plugin-import

--- libraries/core-react/pnpmfile.js
+++ libraries/core-react/pnpmfile.js
@@ -1,16 +1,23 @@
 module.exports = {
   hooks: {
-    readPackage
-  }
+    readPackage,
+  },
 }
 
 function readPackage(pkg, context) {
   if (pkg.name === 'enzyme-adapter-react-16') {
     pkg.dependencies = {
       ...pkg.dependencies,
-      "has": "^1.0.3",
+      has: '^1.0.3',
     }
     context.log('Added missing dependencies for enzyme-adapter-react-16')
+  }
+  if (pkg.name === 'eslint') {
+    pkg.dependencies = {
+      ...pkg.dependencies,
+      'eslint-import-resolver-node': '0.3.2',
+    }
+    context.log('Added missing dependencies for eslint-plugin-import')
   }
 
   return pkg
