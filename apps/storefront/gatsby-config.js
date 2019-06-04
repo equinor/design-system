0343f27948c2ddff1280a8c3b05c170195698b6b
Victor Nystad <vnystad@gmail.com>
2019-06-04 15:02:23 +0200

    add emojis and smartypants (#34)

--- apps/storefront/gatsby-config.js
+++ apps/storefront/gatsby-config.js
@@ -1,97 +1,107 @@
 require('dotenv').config()
 const path = require('path')
+const emoji = require(`remark-emoji`)
 
 module.exports = {
-    siteMetadata: {
-        title: 'Equinor Design System',
-        description:
-            'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-        author: '@chrisbiscardi',
-    },
-    plugins: [
-        {
-            resolve: `gatsby-mdx`,
+  siteMetadata: {
+    title: 'Equinor Design System',
+    description:
+      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@chrisbiscardi',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-mdx`,
+      options: {
+        mdPlugins: [emoji],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-smartypants`,
             options: {
-                defaultLayouts: { default: path.resolve('./src/components/layout.js') },
-                globalScope: `
-                import ComponentStatus from '${__dirname}/src/components/ComponentStatus';
-                import Embed from '${__dirname}/src/components/embed';
+              dashes: 'oldschool',
+            },
+          },
+        ],
+        defaultLayouts: { default: path.resolve('./src/components/layout.js') },
+        globalScope: `
+          import ComponentStatus from '${__dirname}/src/components/ComponentStatus';
+          import Embed from '${__dirname}/src/components/embed';
 
-                export default {
-                    ComponentStatus,
-                    Embed
-                }
-                `
-            },
-        },
-        'gatsby-plugin-react-helmet',
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `content`,
-                path: `${__dirname}/src/content`
-            }
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`
-            }
-        },
-        'gatsby-transformer-yaml',
-        {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-            name: `data`,
-            path: `${__dirname}/src/data`
+          export default {
+              ComponentStatus,
+              Embed
           }
-        },
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp',
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: 'gatsby-default-mdx-basic',
-                short_name: 'starter',
-                start_url: '/',
-                background_color: '#663399',
-                theme_color: '#663399',
-                display: 'minimal-ui',
-                icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-            },
-        },
-        {
-            resolve: `gatsby-plugin-postcss`,
-            options: {
-                postCssPlugins: [
-                    require('postcss-import'),
-                    require('autoprefixer'),
-                    require('postcss-custom-media'),
-                    require('postcss-nested'),
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-prefetch-google-fonts`,
-            options: {
-                fonts: [
-                    {
-                        family: 'Roboto',
-                        variants: [ `400`, `500`, `700`, `900` ],
-                    },
-                ],
-            },
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.app/offline
-        // 'gatsby-plugin-offline',
-    ],
+          `,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    'gatsby-transformer-yaml',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-default-mdx-basic',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require('postcss-import'),
+          require('autoprefixer'),
+          require('postcss-custom-media'),
+          require('postcss-nested'),
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: 'Roboto',
+            variants: [`400`, `500`, `700`, `900`],
+          },
+        ],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
 }
