52fc3b628dce93c23ffc6cef61936aeb95762cc6
Michael Marszalek <mmarsz@statoil.com>
2019-06-04 13:06:47 +0200

    background to indicate frame

--- apps/storefront/src/components/embed.jsx
+++ apps/storefront/src/components/embed.jsx
@@ -6,14 +6,15 @@ const Iframe = styled.iframe`
   height: 450px;
   width: 800px;
   border: none;
+  background-color: #2c2c2c;
 `
 
-const embed = (url) =>
+const parseUrl = (url) =>
   /www.figma.com/.test(url)
     ? `https://www.figma.com/embed?embed_host=astra&url=${url}`
     : url
 
-const Embed = ({ url }) => <Iframe allowfullscreen src={embed(url)} />
+const Embed = ({ url }) => <Iframe allowfullscreen src={parseUrl(url)} />
 
 Embed.propTypes = {
   /** Url to embed in iframe. Will manipulate www.figma.com urls into Figma Embed */
