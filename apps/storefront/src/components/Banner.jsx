f7717fa34f452e8992af8e5b2dfb5582779dc38d
Victor Nystad <vnystad@gmail.com>
2019-06-24 13:20:35 +0200

    Add banner (#46)

---
+++ apps/storefront/src/components/Banner.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+const Wrapper = styled.div`
+  background: darkgray;
+  z-index: 2;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: var(--Banner-height);
+  border-bottom: 1px solid var(--borderColor);
+  font-size: small;
+
+  a {
+    color: inherit;
+  }
+
+  & > :first-child {
+    margin: 0;
+    background: darkgray;
+    padding-left: 3rem;
+    padding-right: 1em;
+    display: grid;
+    align-items: center;
+
+    p {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    @media (min-width: 1200px) {
+      padding-left: 2rem;
+    }
+  }
+
+  & > :last-child {
+    display: grid;
+    align-items: center;
+    margin: 0;
+    background: #666;
+    padding-left: 2em;
+    padding-right: 2em;
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      background: #777;
+    }
+  }
+`
+
+const link = '//components/component-status'
+
+const Banner = () => (
+  <Wrapper>
+    <div>
+      <p>
+        The EDS is currently under construction. Check the{' '}
+        <Link to={link}>component status</Link> page for updates.
+      </p>
+    </div>
+    <Link to={link}>Component Status</Link>
+  </Wrapper>
+)
+
+export default Banner
--- apps/storefront/src/components/layout.jsx
+++ apps/storefront/src/components/layout.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 import { Location } from '@reach/router'
+import 'github-markdown-css'
 import Header from './header'
-import Sidebar from './Sidebar/Sidebar'
+import Sidebar from './Sidebar'
+import Banner from './Banner'
 import './layout.css'
 
 const Layout = ({ children }) => (
   <div className="Page">
+    <Banner />
     <input id="MenuToggler" className="MenuToggler" type="checkbox" />
     <Header />
     <Location>{({ location }) => <Sidebar location={location} />}</Location>
     {/* <nav className="TOC">TOC</nav> */}
-    <main className="Main">{children}</main>
+    <main className="Main markdown-body">{children}</main>
   </div>
 )
 
