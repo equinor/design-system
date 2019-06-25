09af864a28600cf026a91c5af6182addbcd4013f
Victor Nystad <vnystad@gmail.com>
2019-06-25 08:55:12 +0200

    Add links to status table (#47)

--- apps/storefront/src/components/Sidebar/Sidebar.jsx
+++ apps/storefront/src/components/Sidebar/Sidebar.jsx
@@ -2,83 +2,90 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 import classNames from 'classnames'
+import { kebabify } from '../../utils'
 import './Sidebar.css'
 
 const Sidebar = ({ location }) => {
   const data = useStaticQuery(graphql`
-      query SidebarQuery {
-          allNavigationYaml {
-            edges {
-              node {
-                title
-                subNav
-              }
-            }
+    query SidebarQuery {
+      allNavigationYaml {
+        edges {
+          node {
+            title
+            subNav
           }
+        }
       }
+    }
   `)
 
-  const kebabify = title => title.toLowerCase().split(' ').join('-')
+  const categories = data.allNavigationYaml.edges.map((item) =>
+    kebabify(item.node.title),
+  )
 
-  const categories = data.allNavigationYaml.edges.map(item => kebabify(item.node.title))
+  const isCurrentCategory = (category) =>
+    location.pathname.split('/').filter((item) => item.length > 0)[0] ===
+    category
 
-  const isCurrentCategory = category => location.pathname.split('/').filter(item => item.length > 0)[0] === category
-
-  const [ state, setState ] = useState({
-    checked: categories.map(item => isCurrentCategory(item))
+  const [state, setState] = useState({
+    checked: categories.map((item) => isCurrentCategory(item)),
   })
 
   const toggleCheckbox = (event, index) => {
     setState({
-      checked: categories.map((item, i) => index === i ? event.target.checked : false)
+      checked: categories.map((item, i) =>
+        index === i ? event.target.checked : false,
+      ),
     })
   }
 
   return (
-    <nav className='Sidebar'>
-      <label className='Sidebar-veil' htmlFor='MenuToggler' />
-      <div className='Sidebar-content'>
-        <ul className='Sidebar-menu'>
-          {
-            data.allNavigationYaml.edges.map((item, index) =>
-              <li
-                key={kebabify(item.node.title)}
-                className={classNames('Sidebar-menuItem', {
-                  'Sidebar-menuItem--borderTop': index > 0
-                })}
+    <nav className="Sidebar">
+      <label className="Sidebar-veil" htmlFor="MenuToggler" />
+      <div className="Sidebar-content">
+        <ul className="Sidebar-menu">
+          {data.allNavigationYaml.edges.map((item, index) => (
+            <li
+              key={kebabify(item.node.title)}
+              className={classNames('Sidebar-menuItem', {
+                'Sidebar-menuItem--borderTop': index > 0,
+              })}
+            >
+              <input
+                className="SubMenuToggler"
+                type="checkbox"
+                id={`SubMenuToggler-${index}`}
+                checked={state.checked[index]}
+                onChange={(e) => toggleCheckbox(e, index)}
+              />
+              <label
+                className="Sidebar-trigger"
+                htmlFor={`SubMenuToggler-${index}`}
               >
-                <input
-                  className='SubMenuToggler'
-                  type='checkbox'
-                  id={`SubMenuToggler-${index}`}
-                  checked={state.checked[index]}
-                  onChange={e => toggleCheckbox(e, index)}
-                />
-                <label className='Sidebar-trigger' htmlFor={`SubMenuToggler-${index}`}>
-                  { item.node.title }
-                  <svg className='Sidebar-menuArrow' viewBox='0 0 13 13'>
-                    <path d='M6,1 12,6.5 6,12' />
-                  </svg>
-                </label>
-                <ul className='Sidebar-menu'>
-                  {
-                    item.node.subNav && item.node.subNav.map(subItem => (
-                      <li key={kebabify(subItem)} className='Sidebar-menuItem'>
-                        <Link
-                          className='Sidebar-trigger Sidebar-trigger--small'
-                          to={`/${kebabify(item.node.title)}/${kebabify(subItem)}/`}
-                          activeClassName='is-active'
-                          partiallyActive
-                        >
-                          { subItem }
-                        </Link>
-                      </li>
-                    ))
-                  }
-                </ul>
-              </li>
-            )
-          }
+                {item.node.title}
+                <svg className="Sidebar-menuArrow" viewBox="0 0 13 13">
+                  <path d="M6,1 12,6.5 6,12" />
+                </svg>
+              </label>
+              <ul className="Sidebar-menu">
+                {item.node.subNav &&
+                  item.node.subNav.map((subItem) => (
+                    <li key={kebabify(subItem)} className="Sidebar-menuItem">
+                      <Link
+                        className="Sidebar-trigger Sidebar-trigger--small"
+                        to={`/${kebabify(item.node.title)}/${kebabify(
+                          subItem,
+                        )}/`}
+                        activeClassName="is-active"
+                        partiallyActive
+                      >
+                        {subItem}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
---
+++ apps/storefront/src/utils/utils.js
@@ -0,0 +1,14 @@
+export const kebabify = (str) =>
+  str
+    .toLowerCase()
+    .split(' ')
+    .join('-')
+
+export const camelify = (str) =>
+  str
+    .toLowerCase()
+    .split(' ')
+    .map((word, index) =>
+      index > 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word,
+    )
+    .join('')
