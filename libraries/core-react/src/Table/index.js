c71561551a134466144d848ad5eda772a74fd070
Michael Marszalek <mmarsz@equinor.com>
2019-08-22 11:46:49 +0200

    Tweaked storybook layout

--- libraries/core-react/src/Table/index.js
+++ libraries/core-react/src/Table/index.js
@@ -1,4 +1,13 @@
-export { default } from './TableBody'
-export { default } from './TableCell'
-export { default } from './TableHead'
-export { default } from './TableRow'
+import Table from './Table'
+import Body from './TableBody'
+import Cell from './TableCell'
+import Head from './TableHead'
+import Row from './TableRow'
+
+export default {
+  ...Table,
+  Body,
+  Cell,
+  Head,
+  Row,
+}
