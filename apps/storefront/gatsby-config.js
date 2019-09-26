492243d2813be6a3705cfb7823cde307bb8f4098
Michael Marszalek <mmarsz@equinor.com>
2019-09-26 14:36:38 +0200

    Changed images to assets folder

--- apps/storefront/gatsby-config.js
+++ apps/storefront/gatsby-config.js
@@ -47,8 +47,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
       },
     },
     {
@@ -76,13 +76,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-default-mdx-basic',
-        short_name: 'starter',
+        name: 'Equinor Design System',
+        short_name: 'EDS',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#fff',
+        theme_color: '#000',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     {
