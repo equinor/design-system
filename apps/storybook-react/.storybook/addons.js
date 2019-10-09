4ce3501427d43b9b551869b60379e5f109eb62b6
Victor Nystad <vnystad@gmail.com>
2019-10-09 10:56:26 +0200

    configure storybook

--- apps/storybook-react/.storybook/addons.js
+++ apps/storybook-react/.storybook/addons.js
@@ -1,2 +1,5 @@
 import '@storybook/addon-actions/register'
+import '@storybook/addon-links/register'
+import '@storybook/addon-storysource/register'
 import '@storybook/addon-a11y/register'
+import '@storybook/addon-knobs/register'
