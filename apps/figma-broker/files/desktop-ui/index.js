ba1aa7efe80aebc06f3e8fe745e186bea0e39d91
Michael Marszalek <mmarsz@equinor.com>
2019-11-28 08:25:42 +0100

    re-enabled button tokens export

--- apps/figma-broker/files/desktop-ui/index.js
+++ apps/figma-broker/files/desktop-ui/index.js
@@ -12,41 +12,41 @@ export const makeDesktopComponents = (figmaFile) => {
     const data = page.children
 
     switch (fixedPageName) {
-      // case 'buttons primary color':
-      //   components.push({
-      //     name: 'buttons-primary',
-      //     value: makeButtonsComponent(data, getStyle),
-      //     path: 'button',
-      //   })
-      //   break
-      // case 'buttons secondary color':
-      //   components.push({
-      //     name: 'buttons-secondary',
-      //     value: makeButtonsComponent(data, getStyle),
-      //     path: 'button',
-      //   })
-      //   break
-      // case 'buttons danger color':
-      //   components.push({
-      //     name: 'buttons-danger',
-      //     value: makeButtonsComponent(data, getStyle),
-      //     path: 'button',
-      //   })
-      //   break
-      // case 'buttons disabled':
-      //   components.push({
-      //     name: 'buttons-disabled',
-      //     value: makeButtonsComponent(data, getStyle),
-      //     path: 'button',
-      //   })
-      //   break
-      // case 'tables':
-      //   components.push({
-      //     name: 'table',
-      //     value: makeTablesComponent(data, getStyle),
-      //     path: 'table',
-      //   })
-      //   break
+      case 'buttons primary color':
+        components.push({
+          name: 'buttons-primary',
+          value: makeButtonsComponent(data, getStyle),
+          path: 'button',
+        })
+        break
+      case 'buttons secondary color':
+        components.push({
+          name: 'buttons-secondary',
+          value: makeButtonsComponent(data, getStyle),
+          path: 'button',
+        })
+        break
+      case 'buttons danger color':
+        components.push({
+          name: 'buttons-danger',
+          value: makeButtonsComponent(data, getStyle),
+          path: 'button',
+        })
+        break
+      case 'buttons disabled':
+        components.push({
+          name: 'buttons-disabled',
+          value: makeButtonsComponent(data, getStyle),
+          path: 'button',
+        })
+        break
+      case 'tables':
+        components.push({
+          name: 'table',
+          value: makeTablesComponent(data, getStyle),
+          path: 'table',
+        })
+        break
       case 'text fields':
         components.push({
           name: 'text-fields',
