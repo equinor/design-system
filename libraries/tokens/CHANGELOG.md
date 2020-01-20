ebea3debfb17abb1f06c14fa40f6eb9289d60bcf
Michael Marszalek <mmarsz@equinor.com>
2020-01-20 11:40:01 +0100

    Added compiled js bundle

--- libraries/tokens/CHANGELOG.md
+++ libraries/tokens/CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.1-alpha.6] - 2020-01-20
+
+### Added
+
+- Compiled bundle for esm and cjs modules
+
 ## [0.0.1-alpha.5] - 2019-12-13
 
 ### Changed
---
+++ libraries/tokens/rollup.config.js
@@ -0,0 +1,27 @@
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+
+// eslint-disable-next-line import/no-default-export
+export default [
+  {
+    input: 'index.js',
+    watch: {
+      clearScreen: true,
+    },
+    plugins: [commonjs()],
+    output: [
+      {
+        file: pkg.module,
+        name: pkg.name,
+        format: 'esm',
+        exports: 'named',
+      },
+      {
+        file: pkg.main,
+        name: pkg.name,
+        format: 'cjs',
+        exports: 'named',
+      },
+    ],
+  },
+]
