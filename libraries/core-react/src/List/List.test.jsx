ef0a50422f995fac1643baed13aa07b1c35033e5
Victor Nystad <vnys@equinor.com>
2020-02-06 09:51:35 +0100

    add list component

---
+++ libraries/core-react/src/List/List.test.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { render, cleanup, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
+import styled from 'styled-components'
+import { List } from '.'
+
+const { ListItem } = List
+
+afterEach(cleanup)
+
+const StyledList = styled(List)`
+  margin-left: 0;
+`
+
+describe('Lists', () => {
+  it('Renders an unordered list by default', () => {
+    const { container } = render(
+      <List>
+        <ListItem>Item 1</ListItem>
+      </List>,
+    )
+    expect(screen.queryByText('Item 1')).toBeInTheDocument()
+    expect(container.querySelector('ul')).toBeInTheDocument()
+  })
+  it('Renders an unordered list with variant bullet', () => {
+    const { container } = render(
+      <List variant="bullet">
+        <ListItem>Item 1</ListItem>
+      </List>,
+    )
+    expect(screen.queryByText('Item 1')).toBeInTheDocument()
+    expect(container.querySelector('ul')).toBeInTheDocument()
+  })
+  it('Renders an ordered list with variant numbered', () => {
+    const { container } = render(
+      <List variant="numbered">
+        <ListItem>Item 1</ListItem>
+      </List>,
+    )
+    expect(screen.queryByText('Item 1')).toBeInTheDocument()
+    expect(container.querySelector('ol')).toBeInTheDocument()
+  })
+  it('Can extend the css for the component', () => {
+    const { container } = render(
+      <StyledList>
+        <ListItem>Item 1</ListItem>
+      </StyledList>,
+    )
+    expect(container.firstChild).toHaveStyleRule('margin-left', '0')
+  })
+  it('Has start attribute if list variant is numbered', () => {
+    const { container } = render(
+      <List variant="numbered" start="10">
+        <ListItem>Item 1</ListItem>
+        <ListItem>Item 2</ListItem>
+      </List>,
+    )
+    expect(container.querySelector('ol')).toBeInTheDocument()
+    expect(container.firstChild).toHaveAttribute('start')
+  })
+})
--- libraries/core-react/src/index.js
+++ libraries/core-react/src/index.js
@@ -4,5 +4,9 @@ export { Typography } from './Typography'
 export { Table } from './Table'
 export { Divider } from './Divider'
 export { TextField } from './TextField'
+<<<<<<< HEAD
 export { Icon } from './Icon'
+=======
+export { List } from './List'
+>>>>>>> add list component
 /* eslint-enable */
