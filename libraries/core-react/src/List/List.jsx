44f88bef02db5b3b188afbcff0a773bdc9dcdf0a
Victor Nystad <vnys@equinor.com>
2020-02-06 10:02:58 +0100

    remove unused import

--- libraries/core-react/src/List/List.jsx
+++ libraries/core-react/src/List/List.jsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from 'react'
-import PropTypes, { resetWarningCache } from 'prop-types'
+import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 
 const StyledList = styled.div(
