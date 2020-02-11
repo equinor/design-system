99c93041d972fd7b34f03412dd2ab4d1a064461b
Victor Nystad <vnys@equinor.com>
2020-02-11 13:33:27 +0100

    use new config files

---
+++ apps/storybook-react/.storybook/preview.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { addParameters, addDecorator } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Equinor, sans-serif;
+  }
+`
+
+addParameters({
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+})
+
+addDecorator(withA11y)
+
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+))
