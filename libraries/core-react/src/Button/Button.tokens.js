9e7a082c0a350c984d52784e507a8c12767e7a94
Michael Marszalek <mmarsz@equinor.com>
2020-02-14 14:55:09 +0100

    Fixed text styling when having plain text in icon button. (Acronyms are popular in Equinor)

--- libraries/core-react/src/Button/Button.tokens.js
+++ libraries/core-react/src/Button/Button.tokens.js
@@ -1,14 +1,37 @@
-import primaryButtonTokens from '@equinor/eds-tokens/components/button/buttons-primary.json'
-import secondaryButtonTokens from '@equinor/eds-tokens/components/button/buttons-secondary.json'
-import dangerButtonTokens from '@equinor/eds-tokens/components/button/buttons-danger.json'
-import disabledButtonTokens from '@equinor/eds-tokens/components/button/buttons-disabled.json'
+import primary from '@equinor/eds-tokens/components/button/buttons-primary.json'
+import secondary from '@equinor/eds-tokens/components/button/buttons-secondary.json'
+import danger from '@equinor/eds-tokens/components/button/buttons-danger.json'
+import disabled from '@equinor/eds-tokens/components/button/buttons-disabled.json'
 
 const colors = {
-  primary: primaryButtonTokens,
-  secondary: secondaryButtonTokens,
-  danger: dangerButtonTokens,
-  disabled: disabledButtonTokens,
-  icon: primaryButtonTokens.ghost_icon,
+  primary: {
+    ...primary,
+    ghost_icon: {
+      ...primary.ghost_icon,
+      typography: primary.ghost.typography,
+    },
+  },
+  secondary: {
+    ...secondary,
+    ghost_icon: {
+      ...secondary.ghost_icon,
+      typography: secondary.ghost.typography,
+    },
+  },
+  danger: {
+    ...danger,
+    ghost_icon: {
+      ...danger.ghost_icon,
+      typography: danger.ghost.typography,
+    },
+  },
+  disabled: {
+    ...disabled,
+    ghost_icon: {
+      ...disabled.ghost_icon,
+      typography: disabled.ghost.typography,
+    },
+  },
 }
 
 export const button = {
