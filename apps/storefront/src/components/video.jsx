566b6a72f358885367727bf07ab93832c9a47fc4
Michael Marszalek <mmarsz@equinor.com>
2020-02-26 15:48:40 +0100

    Fixed video not being visible

--- apps/storefront/src/components/video.jsx
+++ apps/storefront/src/components/video.jsx
@@ -1,6 +1,16 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const Container = styled.div`
+  display: block;
+  width: 100%;
+  padding: 16px;
+  margin-bottom: 24px;
+  font-size: 18px;
+  background-color: #ebebeb !important;
+`
 
 const VideoBase = styled.video`
   height: 450px;
@@ -9,7 +19,39 @@ const VideoBase = styled.video`
   background-color: #2c2c2c;
 `
 
-const Video = ({ url, ...other }) => <VideoBase {...other} src={url} />
+const Video = ({ url, ...other }) => {
+  // StaticQuery does not support grapql queries so we have to for all videos and then find it....
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: { relativeDirectory: { eq: "video" } }) {
+        edges {
+          node {
+            publicURL
+            relativePath
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data)
+  let videoUrl = url
+
+  if (url.startsWith('video') && data.allFile.edges.length > 0) {
+    const video = data.allFile.edges.find(
+      ({ node }) => node.relativePath === url,
+    )
+    videoUrl = video ? video.node.publicURL : undefined
+  }
+
+  return videoUrl ? (
+    <VideoBase {...other} src={videoUrl} />
+  ) : (
+    <Container>
+      Ops! Can&apos;t find the video, please check your link is correct 🎥 {url}
+    </Container>
+  )
+}
 
 Video.propTypes = {
   /** Url to embed in iframe. Will manipulate www.figma.com urls into Figma Embed */
