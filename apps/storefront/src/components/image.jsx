16c74d1bb27a00020bdda1797d7da2b56c4cdc6a
Michael Marszalek <mmarsz@equinor.com>
2020-03-06 10:18:52 +0100

    Added image component

---
+++ apps/storefront/src/components/image.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const Container = styled.div`
+  display: block;
+  width: 100%;
+  padding: 16px;
+  margin-bottom: 24px;
+  font-size: 18px;
+  background-color: #ebebeb !important;
+`
+
+const ImageBase = styled.img`
+  height: 450px;
+  border: none;
+  background-color: #2c2c2c;
+`
+
+const Image = ({ url, ...other }) => {
+  // StaticQuery does not support grapql queries so we have to for all videos and then find it....
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: { relativeDirectory: { eq: "images" } }) {
+        edges {
+          node {
+            publicURL
+            relativePath
+          }
+        }
+      }
+    }
+  `)
+
+  let imageUrl = url
+
+  if (url.startsWith('images') && data.allFile.edges.length > 0) {
+    const image = data.allFile.edges.find(
+      ({ node }) => node.relativePath.toLowerCase() === url.toLowerCase(),
+    )
+    imageUrl = image ? image.node.publicURL : undefined
+  }
+
+  return imageUrl ? (
+    <ImageBase {...other} src={imageUrl} />
+  ) : (
+    <Container>
+      Ops! Can&apos;t find the image, please check if your link is correct 🏞️
+      {url}
+    </Container>
+  )
+}
+
+Image.propTypes = {
+  /** Url to embed in iframe. Will manipulate www.figma.com urls into Figma Embed */
+  url: PropTypes.string.isRequired,
+}
+
+export default Image
