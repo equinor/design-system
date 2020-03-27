dcf3c9907456eed2a15d4cc4f61ba473afe07b7a
Kristian Brynie Hollund <kristian.hollund@olavstoppen.no>
2020-03-27 11:05:32 +0100

    Removed unused token

--- libraries/core-react/src/TableOfContents/LinkItem.jsx
+++ libraries/core-react/src/TableOfContents/LinkItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { tableOfContents as tokens } from './TableOfContents.tokens'
 
-const { icon, focus, hover, colors } = tokens
+const { icon, focus, hover } = tokens
 
 const StyledLinkItem = styled.li`
   list-style: none;
