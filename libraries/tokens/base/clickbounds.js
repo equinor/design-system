938f8426a9c12b446c38119fa66c615de0a2205d
Michael Marszalek <mmarsz@equinor.com>
2020-04-01 12:19:58 +0200

    Updated tokens

--- libraries/tokens/base/clickbounds.js
+++ libraries/tokens/base/clickbounds.js
@@ -1,1 +1,5 @@
-export const clickbounds = { jumbo__base: '88px', default__base: '48px', default__input: '56px' }
+export const clickbounds = {
+  jumbo__base: '88px',
+  default__base: '48px',
+  default__input: '56px',
+}
--- libraries/tokens/base/elevation.js
+++ libraries/tokens/base/elevation.js
@@ -1,8 +1,13 @@
 export const elevation = {
-  raised: '0 1px 5px rgba(0, 0, 0, 0.2),0 3px 4px rgba(0, 0, 0, 0.12),0 2px 4px rgba(0, 0, 0, 0.14)',
+  raised:
+    '0 1px 5px rgba(0, 0, 0, 0.2),0 3px 4px rgba(0, 0, 0, 0.12),0 2px 4px rgba(0, 0, 0, 0.14)',
   none: '0 0 1px rgba(0, 0, 0, 0.14)',
-  overlay: '0 1px 10px rgba(0, 0, 0, 0.2),0 4px 5px rgba(0, 0, 0, 0.12),0 2px 4px rgba(0, 0, 0, 0.14)',
-  sticky: '0 4px 5px rgba(0, 0, 0, 0.2),0 3px 14px rgba(0, 0, 0, 0.12),0 8px 10px rgba(0, 0, 0, 0.14)',
-  temporary_nav: '0 7px 8px rgba(0, 0, 0, 0.2),0 5px 22px rgba(0, 0, 0, 0.12),0 12px 17px rgba(0, 0, 0, 0.14)',
-  above_scrim: '0 11px 15px rgba(0, 0, 0, 0.2),0 9px 46px rgba(0, 0, 0, 0.12),0 24px 38px rgba(0, 0, 0, 0.14)'
+  overlay:
+    '0 1px 10px rgba(0, 0, 0, 0.2),0 4px 5px rgba(0, 0, 0, 0.12),0 2px 4px rgba(0, 0, 0, 0.14)',
+  sticky:
+    '0 4px 5px rgba(0, 0, 0, 0.2),0 3px 14px rgba(0, 0, 0, 0.12),0 8px 10px rgba(0, 0, 0, 0.14)',
+  temporary_nav:
+    '0 7px 8px rgba(0, 0, 0, 0.2),0 5px 22px rgba(0, 0, 0, 0.12),0 12px 17px rgba(0, 0, 0, 0.14)',
+  above_scrim:
+    '0 11px 15px rgba(0, 0, 0, 0.2),0 9px 46px rgba(0, 0, 0, 0.12),0 24px 38px rgba(0, 0, 0, 0.14)',
 }
