114bb9403c08a2fc060003ebfd473c2ed275b0c3
Kristian Hollund <kristian.hollund@olavstoppen.no>
2020-04-02 09:13:31 +0200

    Table of contents (#275)
    * Started working on TOC v2
    
    * Added setup, currently started to add tokens and props
    
    * Updates
    
    * Sorting through tokens, figuring out which to use and adding, WIP on structure
    
    * Changes to tokens
    
    * Starting to look like it should, a bit harder to find correct tokens for this component
    
    * Added tests
    
    * Removed unused token
    
    * Unneeded stuff
    
    * Added rest of the tokens
    
    * Fixed relative import
    
    * Added focus as agreed on, updated relative import again as test fails otherwise
    
    * Removed pressed style from links, added prop comments

---
+++ libraries/core-react/src/TableOfContents/TableOfContents.tokens.js
@@ -0,0 +1,58 @@
+import { tokens } from '@equinor/eds-tokens'
+
+const {
+  colors: {
+    interactive: {
+      primary__resting: { rgba: primaryColor },
+      primary__hover_alt: { rgba: primaryHoverAlt },
+      primary__hover: { rgba: primaryHover },
+      focus: { rgba: focusColor },
+    },
+    text: {
+      static_icons__tertiary: { rgba: labelColor },
+    },
+  },
+  shape: {
+    corners: { borderRadius },
+  },
+  spacings: {
+    comfortable: { small: smallSpacings },
+  },
+  typography: {
+    paragraph: {
+      caption: { fontSize, lineHeight: fontLineHeight },
+    },
+  },
+} = tokens
+
+export const tableOfContents = {
+  icon: {
+    fill: primaryColor,
+  },
+  focus: {
+    border: {
+      color: focusColor,
+      borderRadius,
+    },
+  },
+  hover: {
+    background: primaryHoverAlt,
+    borderRadius,
+    icon: {
+      background: primaryHoverAlt,
+    },
+    typography: {
+      color: primaryHover,
+    },
+  },
+  labelText: {
+    color: labelColor,
+  },
+  links: {
+    fontSize,
+    fontLineHeight,
+  },
+  spacings: {
+    comfortable: { smallSpacings },
+  },
+}
