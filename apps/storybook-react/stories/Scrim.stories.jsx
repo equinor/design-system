bc988dbb6836ee22242089882857dd5e00bde607
Kristian Hollund <kristian.hollund@olavstoppen.no>
2020-04-06 09:35:48 +0200

    Dialog and Scrim (#269)
    * Built structure for the dialog and the basic styling, need to make more examples and fix some styling
    
    * Fixed some styling issues
    
    * Tests and style tweaks in variants
    
    * Updating for storybook variants etc.
    
    * Sorted out the issues with the story and knobs after changing the setup
    
    * Adding scrim to test usage of dialog
    
    * Updates for scrim
    
    * Need more props to handle a better demo of showing and hiding and passing on functions.
    
    * Issue with using props in style, wip
    
    * Tweaks after feedback
    
    * Clean up methods not used, cleaner css handled display
    
    * Updated styling, stories and tests for Scrim and Dialog
    
    * Added logic back in story for no button
    
    * Remove windows lf tweak
    
    * Some token tweaks
    
    * Updated for pull request comments. Issue with useeffect in scrim still
    
    * Need a better solution to combine dismiss and show,hide
    
    * Simplified a bit
    
    * Updated dismiss to be handled externally, but listened to from component with esc keypress
    
    * Removed comments and minor things
    
    * Fixed tests for dismissable scrim etc.
    
    * Updates to scrim after review
    
    * Most issues fixed I think, also added a line under scrollable content as it was in Figma but I missed it
    
    * Changes WIP
    
    * Added clickable overlay if dismissable. Added a container for content to prevent event bubbling. Updated with requested changes.

---
+++ apps/storybook-react/stories/Scrim.stories.jsx
@@ -0,0 +1,62 @@
+import React, { Fragment, useState } from 'react'
+import { withKnobs, select, text } from '@storybook/addon-knobs'
+import { Scrim, Button, Typography } from '@equinor/eds-core-react'
+
+import styled from 'styled-components'
+
+const Body = styled.div`
+  height: calc(100vh - 64px);
+  background: #ebebeb;
+  display: grid;
+  grid-template-rows: 1fr auto 1fr;
+  padding: 32px;
+  grid-gap: 32px;
+`
+
+const TestContent = styled.div`
+  background: rgba(255, 146, 0, 0.5);
+  border: 1px dashed #ff9200;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 8px;
+  height: calc(250px - 16px);
+  margin-bottom: 12px;
+  width: 350px;
+`
+
+export default {
+  title: 'Components|Scrim',
+  component: Scrim,
+}
+
+export const Page = () => {
+  const [visibleScrim, setVisibleScrim] = useState(false)
+  const handleClose = (event, closed) => {
+    if (closed) {
+      setVisibleScrim(closed)
+    } else {
+      setVisibleScrim(!visibleScrim)
+    }
+  }
+
+  return (
+    <Body>
+      <Typography variant="body_short">Top of page</Typography>
+      <Typography variant="body_short">Center page.</Typography>
+      <div>
+        <Button onClick={() => setVisibleScrim(true)}>Trigger Scrim</Button>
+      </div>
+      <Typography variant="body_short">Bottom of page</Typography>
+      {visibleScrim && (
+        <Scrim onClose={handleClose} isDismissable>
+          <TestContent>
+            <Typography variant="body_short">
+              Press close or hit 'ESC' to close scrim.
+            </Typography>
+            <Button onClick={() => setVisibleScrim(false)}>OK</Button>
+          </TestContent>
+        </Scrim>
+      )}
+    </Body>
+  )
+}
