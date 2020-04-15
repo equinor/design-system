a56d94461e70f8d6b80369edcc13a2b1a5beb6fb
Frida Erdal <fridaerdal@hotmail.no>
2020-04-15 11:34:55 +0200

    Css

---
+++ apps/storybook-react/stories/Tooltip.stories.jsx
@@ -0,0 +1,27 @@
+import React, { useState, Fragment } from 'react'
+import { withKnobs } from '@storybook/addon-knobs'
+import styled from 'styled-components'
+import { Tooltip, Typography, Button } from '@equinor/eds-core-react'
+
+const Wrapper = styled.div`
+  margin: 32px;
+  display: grid;
+  grid-gap: 24px;
+`
+
+export default {
+  title: 'Components|Tooltip',
+  component: Tooltip,
+}
+
+export function Example() {
+  const [toggle, setToggle] = useState(true)
+
+  return (
+    <Wrapper>
+      <Tooltip>
+        <Button>Anchor</Button>
+      </Tooltip>
+    </Wrapper>
+  )
+}
