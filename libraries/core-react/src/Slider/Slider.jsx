3a2f41f9810d6650db93481a098299c38de87c47
wenche <wenche.tollevsen@gmail.com>
2020-04-24 15:00:05 +0200

    Some CSS cleanup

--- libraries/core-react/src/Slider/Slider.jsx
+++ libraries/core-react/src/Slider/Slider.jsx
@@ -3,91 +3,107 @@ import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import { slider as tokens } from './Slider.tokens'
 
+const track = `
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background: none;'
+`
+const thumb = `
+  border: 2px solid #007079;
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  background: #ffffff;
+  cursor: pointer;
+  position: relative;
+  margin-top: 0;
+  z-index: 1;
+  pointer-events: auto;
+  `
+const thumbHover = `
+  box-shadow: 0px 0px 0px 6px #deedee;
+ `
+
+/** The two first gradients are hacks to avoid 2px too long slider bar on both edges. Better solution? */
+const fakeTrackBg = `
+  background: linear-gradient(90deg, #fff, #fff 3px, transparent 0),
+  linear-gradient(-90deg, #fff, #fff 3px, transparent 0),
+  linear-gradient(
+    0deg,
+    transparent,
+    transparent 20px,
+    #f7f7f7 20px,
+    #f7f7f7 24px,
+    transparent 0
+  );
+`
+
+const trackFill = `
+  grid-column: 1 / span 2;
+  grid-row: 2;
+  height: 4px;
+  margin-bottom: 4px;
+  background: #007079;
+  align-self: end;
+  content: '';
+`
+
 const Wrapper = styled.div`
+  --a: ${({ valA }) => valA};
+  --b: ${({ valB }) => valB};
+  --min: ${({ min }) => min};
+  --max: ${({ max }) => max};
+  --dif: calc(var(--max) - var(--min));
+  --realWidth: calc(100% - 12px);
   display: grid;
-  grid-template-rows: repeat(2, max-content) 12px 16px;
+  grid-template-rows: max-content 12px 16px;
   /* grid-gap: 0.5rem; */
   grid-template-columns: 1fr 1fr;
   margin: 1em auto;
   width: 100%;
   position: relative;
-  --a: ${(props) => props.valA};
-  --b: ${(props) => props.valB};
-  --min: ${(props) => props.min};
-  --max: ${(props) => props.max};
-  --dif: calc(var(--max) - var(--min));
-  /** The two first gradients are hacks to avoid 2px too long slider bar on both edges. Better solution? */
-  background: linear-gradient(90deg, #fff, #fff 3px, transparent 0),
-    linear-gradient(-90deg, #fff, #fff 3px, transparent 0),
-    linear-gradient(
-      0deg,
-      transparent,
-      transparent 20px,
-      #f7f7f7 20px,
-      #f7f7f7 24px,
-      transparent 0
-    );
 
+  ${fakeTrackBg}
   &::before,
   &::after {
-    grid-column: 1 / span 2;
-    grid-row: 3;
-    height: 4px;
-    margin-bottom: 4px;
-    background: #007079;
-    align-self: end;
-    content: '';
+    ${trackFill}
   }
   /** Faking the active region of the slider */
   &::before {
     margin-left: calc(
-      6px + (var(--a) - var(--min)) / var(--dif) * calc(100% - 12px)
+      6px + (var(--a) - var(--min)) / var(--dif) * var(--realWidth)
     );
-    width: calc((var(--b) - var(--a)) / var(--dif) * calc(100% - 12px));
+    width: calc((var(--b) - var(--a)) / var(--dif) * var(--realWidth));
   }
 
   &::after {
     margin-left: calc(
-      6px + (var(--b) - var(--min)) / var(--dif) * calc(100% - 12px)
+      6px + (var(--b) - var(--min)) / var(--dif) * var(--realWidth)
     );
-    width: calc((var(--a) - var(--b)) / var(--dif) * calc(100% - 12px));
+    width: calc((var(--a) - var(--b)) / var(--dif) * var(--realWidth));
   }
 `
 const WrapperLabel = styled.label`
+  --min: ${({ min }) => min};
+  --max: ${({ max }) => max};
+  --dif: calc(var(--max) - var(--min));
+  --value: ${({ value }) => value};
+  --realWidth: calc(100% - 12px);
   display: grid;
-  grid-template-rows: repeat(2, max-content) 12px 16px;
+  grid-template-rows: max-content 12px 16px;
   grid-template-columns: 1fr 1fr;
   margin: 1em auto;
   width: 100%;
   position: relative;
-  --min: ${(props) => props.min};
-  --max: ${(props) => props.max};
-  --dif: calc(var(--max) - var(--min));
-  --value: ${(props) => props.value};
-  background: linear-gradient(90deg, #fff, #fff 3px, transparent 0),
-    linear-gradient(-90deg, #fff, #fff 3px, transparent 0),
-    linear-gradient(
-      0deg,
-      transparent,
-      transparent 20px,
-      #f7f7f7 20px,
-      #f7f7f7 24px,
-      transparent 0
-    );
-  /*  &::before, */
+  ${fakeTrackBg}
   &::after {
-    grid-column: 1 / span 2;
-    grid-row: 3;
-    height: 4px;
-    margin-bottom: 4px;
-    background: #007079;
-    align-self: end;
-    content: '';
+    ${trackFill}
   }
 
   &::after {
     margin-right: calc(
-      (var(--max) - var(--value)) / var(--dif) * calc(100% - 12px)
+      (var(--max) - var(--value)) / var(--dif) * var(--realWidth)
     );
     /* Adjusting for start dot circle */
     margin-left: 3px;
@@ -98,7 +114,7 @@ const Label = styled.div`
   grid-column: 1/-1;
 `
 const Output = styled.output`
-  --val: ${(props) => props.value};
+  --val: ${({ value }) => value};
   width: fit-content;
   background: white;
   position: relative;
@@ -109,19 +125,19 @@ const Output = styled.output`
   padding: 0 5px;
   /* Idea: Transform negative ((width of outline elem - handle width) / 2 (half of width for centering)) */
   transform: translate(calc(-1 * calc((100% - 12px) / 2)));
-  grid-row: 4;
+  grid-row: 3;
   grid-column: 1 / 3;
   margin-left: calc((var(--val) - var(--min)) / var(--dif) * calc(100% - 12px));
 `
 const MinMaxValue = styled.span`
-  grid-row: 4;
+  grid-row: 3;
   font-size: 10px;
   color: #6f6f6f;
   position: absolute;
   left: 2px;
   text-align: left;
   margin-top: 6px;
-  /** Center lign the text with the dot */
+  /** Center align the text with the dot */
   transform: translate(calc(-1 * calc((100% - 8px) / 2)));
   &:last-child {
     left: auto;
@@ -132,10 +148,11 @@ const MinMaxValue = styled.span`
 const WrapperGroupLabel = styled.div`
   grid-row: 1;
   grid-column: 1 / 3;
-
+`
+const WrapperGroupLabelDots = styled(WrapperGroupLabel)`
   &:before,
   &:after {
-    content: ${({ minMaxDots }) => (minMaxDots ? ' ' : 'normal')};
+    content: ' ';
     display: block;
     width: 6px;
     height: 6px;
@@ -167,7 +184,7 @@ const StyledSlider = styled.input`
   width: 100%; /* Specific width is required for Firefox. */
   background: transparent;
   grid-column: 1 / -1;
-  grid-row: 3;
+  grid-row: 2;
   background: none; /* get rid of white Chrome background */
   color: #000;
   font: inherit; /* fix too small font-size in both Chrome & Firefox */
@@ -189,16 +206,15 @@ const StyledSlider = styled.input`
       outline-offset: 2px;
     }
     & + output {
-      /* color: darkorange; */
     }
   }
   &:hover,
   &:active {
     &::-webkit-slider-thumb {
-      box-shadow: 0px 0px 0px 6px #deedee;
+      ${thumbHover}
     }
     &::-moz-range-thumb {
-      box-shadow: 0px 0px 0px 6px #deedee;
+      ${thumbHover}
     }
   }
   &:before,
@@ -210,46 +226,17 @@ const StyledSlider = styled.input`
 
   /* Must be seperated code blocks for webkit and moz otherwise nothing will be applied */
   ::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    border: 2px solid #007079;
-    height: 12px;
-    width: 12px;
-    border-radius: 50%;
-    background: #ffffff;
-    cursor: pointer;
-    position: relative;
-    margin-top: 0;
-    z-index: 1;
-    pointer-events: auto;
+    ${thumb}
   }
   &::-moz-range-thumb {
-    -webkit-appearance: none;
-    border: 2px solid #007079;
-    height: 12px;
-    width: 12px;
-    border-radius: 50%;
-    background: #ffffff;
-    cursor: pointer;
-    position: relative;
-    margin-top: 0;
-    z-index: 1;
-    pointer-events: auto;
+    ${thumb}
   }
 
   &::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    background: none;
+    ${track}
   }
   &::-moz-range-track {
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    background: none;
-  }
-  ::-webkit-slider-progress {
-    background: green;
+    ${track}
   }
 `
 const SrOnlyLabel = styled.label`
@@ -319,9 +306,13 @@ export const Slider = forwardRef(function EdsSlider(
           max={max}
           min={min}
         >
-          <WrapperGroupLabel id="wrapperLabel" minMaxDots={minMaxDots}>
-            {label}
-          </WrapperGroupLabel>
+          {minMaxDots ? (
+            <WrapperGroupLabelDots id="wrapperLabel">
+              {label}
+            </WrapperGroupLabelDots>
+          ) : (
+            <WrapperGroupLabel id="wrapperLabel">{label}</WrapperGroupLabel>
+          )}
           <SrOnlyLabel htmlFor="a">Value A</SrOnlyLabel>
 
           <StyledSlider
@@ -363,7 +354,7 @@ export const Slider = forwardRef(function EdsSlider(
       ) : (
         <WrapperLabel max={max} min={min} value={valueZ}>
           {/*  Need an element for pseudo elems :/ */}
-          {minMaxDots && <WrapperGroupLabel />}
+          {minMaxDots && <WrapperGroupLabelDots />}
           <Label>{label}</Label>
           <StyledSlider
             type="range"
