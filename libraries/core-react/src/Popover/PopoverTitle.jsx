08ef9b3242a8cb9b0b53cb60ef90923c6d09d002
Frida Erdal <fridaerdal@hotmail.no>
2020-04-28 21:14:50 +0200

    Title component

---
+++ libraries/core-react/src/Popover/PopoverTitle.jsx
@@ -0,0 +1,41 @@
+import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { popover as tokens } from './Popover.tokens'
+
+const StyledPopoverTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  > :not(:first-child) {
+    margin-left: ${tokens.spacings.left};
+  }
+`
+
+export const PopoverTitle = forwardRef(function EdsPopoverTitle(
+  { children, className, ...rest },
+  ref,
+) {
+  const props = {
+    ...rest,
+    className,
+    ref,
+  }
+
+  return <StyledPopoverTitle {...props}>{children}</StyledPopoverTitle>
+})
+
+PopoverTitle.displayName = 'eds-popover-title'
+
+PopoverTitle.propTypes = {
+  /** @ignore */
+  children: PropTypes.node,
+  /** @ignore */
+  className: PropTypes.string,
+}
+
+PopoverTitle.defaultProps = {
+  className: '',
+  children: undefined,
+}
