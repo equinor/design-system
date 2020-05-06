d29528d6f59d5a409b3839a6a1c5eb4e0e70547a
wenche <wenche.tollevsen@gmail.com>
2020-05-06 14:31:00 +0200

    Some bling for the story book

--- apps/storybook-react/stories/Slider.stories.jsx
+++ apps/storybook-react/stories/Slider.stories.jsx
@@ -7,9 +7,9 @@ const Body = styled.div`
   /* height: calc(100vh - 64px); */
   /* background: #ebebeb; */
   display: grid;
-  grid-template-rows: 1fr auto 1fr;
+  grid-template-rows: min-width;
   padding: 32px;
-  grid-gap: 32px;
+  grid-gap: 4rem;
   position: relative;
 `
 
@@ -38,48 +38,60 @@ export const Examples = () => {
   }
   return (
     <Body>
-      <span id="range-slider-label">Range slider</span>
-      <Slider
-        value={value}
-        onChange={onChange}
-        ariaLabelledby="range-slider-label"
-      />
-      <p>
-        <small>Output from slider is {value.join(', ')}</small>
-      </p>
-      <span id="large-step-range-slider">Range slider with steps of 5</span>
-      <Slider
-        ariaLabelledby="large-step-range-slider"
-        step={5}
-        min={30}
-        minMaxDots={false}
-        value={[40, 60]}
-      />
-      <span id="date-range-slider">Date range slider with days</span>
-      <Slider
-        min={getUnixTime('2020-01-01')}
-        max={getUnixTime('2020-01-31')}
-        ariaLabelledby="date-range-slider"
-        step={60 * 60 * 24 * 1000}
-        value={[getUnixTime('2020-01-01'), getUnixTime('2020-01-31')]}
-        outputFunction={outputFunction}
-      />
-      <span id="simple-slider">Simple slider</span>
-      <Slider value={4} min={0} max={10} ariaLabelledby="simple-slider" />
-      <span id="even-simpler-slider">
-        Simple slider, no dots, no min or max values, steps of 10
-      </span>
-      <Slider
-        ariaLabelledby="even-simpler-slider"
-        value={50}
-        min={0}
-        max={100}
-        step={10}
-        minMaxDots={false}
-        minMaxValues={false}
-      />
-      <label id="disabled-slider">Disabled slider</label>
-      <Slider value={50} disabled={true} ariaLabelledby="disabled-slider" />
+      <div>
+        <span id="range-slider-label">Range slider</span>
+        <Slider
+          value={value}
+          onChange={onChange}
+          ariaLabelledby="range-slider-label"
+        />
+        <p>
+          <small>Output from slider is {value.join(', ')}</small>
+        </p>
+      </div>
+      <div>
+        <span id="large-step-range-slider">Range slider with steps of 5</span>
+        <Slider
+          ariaLabelledby="large-step-range-slider"
+          step={5}
+          min={30}
+          minMaxDots={false}
+          value={[40, 60]}
+        />
+      </div>
+      <div>
+        <span id="date-range-slider">Date range slider with days</span>
+        <Slider
+          min={getUnixTime('2020-01-01')}
+          max={getUnixTime('2020-01-31')}
+          ariaLabelledby="date-range-slider"
+          step={60 * 60 * 24 * 1000}
+          value={[getUnixTime('2020-01-01'), getUnixTime('2020-01-31')]}
+          outputFunction={outputFunction}
+        />
+      </div>
+      <div>
+        <span id="simple-slider">Simple slider</span>
+        <Slider value={4} min={0} max={10} ariaLabelledby="simple-slider" />
+      </div>
+      <div>
+        <span id="even-simpler-slider">
+          Simple slider, no dots, no min or max values, steps of 10
+        </span>
+        <Slider
+          ariaLabelledby="even-simpler-slider"
+          value={50}
+          min={0}
+          max={100}
+          step={10}
+          minMaxDots={false}
+          minMaxValues={false}
+        />
+      </div>
+      <div>
+        <label id="disabled-slider">Disabled slider</label>
+        <Slider value={50} disabled={true} ariaLabelledby="disabled-slider" />
+      </div>
     </Body>
   )
 }
