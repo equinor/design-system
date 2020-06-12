9d0a7b28732830014d10efc5e3bf21e5aeaf5940
Michael Marszalek <mmarsz@equinor.com>
2020-06-12 08:50:30 +0200

    Added icons + sprite to eds-static (#362)
    * Added icons + sprite
    
    * removed styling in svg sprite

--- apps/figma-broker/actions/createAssets.js
+++ apps/figma-broker/actions/createAssets.js
@@ -25,12 +25,11 @@ const svgPathData = R.pipe(
 
 const writeSVGSprite = (assets) => {
   const value = R.pipe(
-    R.find((x) => x.name === 'system'),
+    R.find((x) => x.name === 'system-icons'),
     R.prop('value'),
     R.reduce(
       (acc, val) =>
-        `${acc}${`
-    <symbol id="${val.name}" viewBox="${val.viewbox}">
+        `${acc}${`<symbol id="${val.name}" viewBox="${val.viewbox}">
       <title>${val.name}</title>
       <desc>${val.path}-${val.name}</desc>
       ${svgContent(val.value)}
@@ -38,13 +37,19 @@ const writeSVGSprite = (assets) => {
       '',
     ),
     (x) => `
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-      ${x}
+    <svg style="display: none;"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+     ${x}
     </svg>
     `,
   )(assets)
 
-  writeResults([{ name: 'system', value }], `${PATHS.ASSETS_ICONS}`, 'svg')
+  writeResults(
+    [{ name: 'system-icons', value }],
+    `${PATHS.ASSETS_ICONS}`,
+    'svg',
+  )
 }
 
 const writeJsFile = (assets) => {
@@ -153,8 +158,8 @@ export async function createAssets(ctx) {
   // Write svg to files
 
   // TODO: Disabled for now as not sure if needed yet and not to polute repo with 600+ svgs yet...
-  // writeSVGs(assetsWithSvg)
-  // writeSVGSprite(assetsWithSvg)
+  writeSVGs(assetsWithSvg)
+  writeSVGSprite(assetsWithSvg)
 
   writeJsonAssets(assetsWithSvg)
   writeJsFile(assetsWithSvg)
