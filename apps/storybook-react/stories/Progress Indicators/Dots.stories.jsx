9d35e2fd9b628a5e8150cd687b8bd03dc1c13186
Frida Erdal <fridaerdal@hotmail.no>
2020-06-17 17:44:05 +0200

    Header level change

--- apps/storybook-react/stories/Progress Indicators/Dots.stories.jsx
+++ apps/storybook-react/stories/Progress Indicators/Dots.stories.jsx
@@ -28,13 +28,13 @@ export default {
 export const Variants = () => {
   return (
     <Body>
-      <Typography variant="h4">White</Typography>
+      <Typography variant="h2">White</Typography>
       <Wrapper>
         <Background>
           <DotProgress variant="white" />
         </Background>
       </Wrapper>
-      <Typography variant="h4">Green</Typography>
+      <Typography variant="h2">Green</Typography>
       <Wrapper>
         <DotProgress variant="green" />
       </Wrapper>
