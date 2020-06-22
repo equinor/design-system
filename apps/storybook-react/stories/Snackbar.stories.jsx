e235e10d97f7ac8e80140872acddea2ca23df375
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-06-22 12:44:57 +0200

    Add version with Moss green 34 as action button color

--- apps/storybook-react/stories/Snackbar.stories.jsx
+++ apps/storybook-react/stories/Snackbar.stories.jsx
@@ -1,9 +1,18 @@
 import React, { Fragment, useState } from 'react'
 import { withKnobs, select, text } from '@storybook/addon-knobs'
 import { Snackbar, Button, Typography } from '@equinor/eds-core-react'
+import { tokens } from '@equinor/eds-tokens'
 
 const { SnackbarAction } = Snackbar
 import styled from 'styled-components'
+
+const {
+  colors: {
+    infographic: {
+      primary__moss_green_34: { rgba: buttonColor },
+    },
+  },
+} = tokens
 
 const Wrapper = styled.div`
   display: grid;
@@ -11,6 +20,10 @@ const Wrapper = styled.div`
   padding: 32px 0;
   padding-bottom: 8rem;
   grid-gap: 2rem;
+`
+
+const StyledButton = styled(Button)`
+  color: ${buttonColor};
 `
 
 export default {
@@ -47,7 +60,7 @@ export const Example = () => {
         >
           Your changes was saved
           <SnackbarAction>
-            <Button variant="ghost">Undo</Button>
+            <StyledButton variant="ghost">Undo</StyledButton>
           </SnackbarAction>
         </Snackbar>
       </div>
