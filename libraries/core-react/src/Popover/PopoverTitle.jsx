4d765eb44354db49f1146f9d33a4e75129696898
Frida Erdal <31915755+pomfrida@users.noreply.github.com>
2020-06-23 09:51:11 +0200

    Overflow handling for Popover content (#376)
    * Overflow: hidden
    
    * Multiline title with overflow
    
    * Removed empty line

--- libraries/core-react/src/Popover/PopoverTitle.jsx
+++ libraries/core-react/src/Popover/PopoverTitle.jsx
@@ -9,11 +9,14 @@ import { popover as tokens } from './Popover.tokens'
 const StyledPopoverTitle = styled.div`
   ${typographyTemplate(tokens.header)}
   margin-right: 48px;
+  max-width: 498px;
+  overflow: hidden;
 `
 
 const StyledDivider = styled((props) => <Divider {...props} />)`
   margin-left: -16px;
   margin-right: -16px;
+  max-width: 560px;
 `
 
 export const PopoverTitle = forwardRef(function EdsPopoverTitle(
