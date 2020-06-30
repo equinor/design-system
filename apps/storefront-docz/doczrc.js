4d1d7bf041510a75073d4627bc397681ad1bde34
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-06-30 10:51:10 +0200

    Use doczrc config just to make sure it works

---
+++ apps/storefront-docz/doczrc.js
@@ -0,0 +1,18 @@
+/* eslint import/no-default-export: off */
+export default {
+  themeConfig: {
+    initialColorMode: "dark",
+    // Show errors above playground editor
+    showLiveError: false,
+    // Show preview of the code inside playground
+    showLivePreview: true,
+    // Show editor when a playground is rendered
+    showPlaygroundEditor: false,
+    // Show dark/light mode switch toggle in header
+    showDarkModeSwitch: false,
+    // Display edit this page button on every page
+    showMarkdownEditButton: true,
+    // Wrap the playground editor and preview in iframes to avoid style/script collisions
+    useScopingInPlayground: false,
+  },
+}
