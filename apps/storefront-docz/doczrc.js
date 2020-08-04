3a9d48015fe9529f73b29a0cec647e27b9f4acd7
Victor Nystad <vnys@equinor.com>
2020-08-04 09:03:46 +0200

    add config

--- apps/storefront-docz/doczrc.js
+++ apps/storefront-docz/doczrc.js
@@ -1,5 +1,6 @@
 /* eslint import/no-default-export: off*/
 export default {
+  files: '**/*.mdx',
   themeConfig: {
     // “The initialColorMode flag no longer works, use initialColorModeName instead” it says on https://theme-ui.com/migrating/
     // Didn’t help, but keeping it here for now
@@ -17,4 +18,46 @@ export default {
     // Wrap the playground editor and preview in iframes to avoid style/script collisions
     useScopingInPlayground: false,
   },
+  menu: [
+    {
+      title: 'Getting Started',
+      route: '/getting-started/',
+      children: [],
+    },
+    {
+      title: 'Guidelines',
+      route: '/guidelines/',
+      children: [],
+    },
+    {
+      title: 'Assets',
+      route: '/assets/',
+      children: [],
+    },
+    {
+      title: 'Components',
+      route: '/components/',
+      children: [],
+    },
+    {
+      title: 'Patterns',
+      route: '/patterns/',
+      children: [],
+    },
+    {
+      title: 'Updates',
+      route: '/updates/',
+      children: [],
+    },
+    {
+      title: 'Help',
+      route: '/help/',
+      children: [],
+    },
+    {
+      title: 'Contributing',
+      route: '/contributing/',
+      children: [],
+    },
+  ],
 }
