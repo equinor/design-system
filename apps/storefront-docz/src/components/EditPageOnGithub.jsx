d659a53f59e9a79bfb3033248bd6408990e29100
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-08-05 12:41:34 +0200

    Refactor github edit link to a separate component. Use primary color for link.

---
+++ apps/storefront-docz/src/components/EditPageOnGithub.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useThemeUI } from 'theme-ui'
+
+export const EditPageOnGithub = ({ slug }) => {
+  const context = useThemeUI()
+  const { theme } = context
+
+  return (
+    <a
+      href={`https://github.com/equinor/design-system/tree/documentation/apps/storefront/src/content/${
+        slug === '/' ? `index` : slug
+      }.mdx`}
+      style={{
+        display: 'block',
+        marginTop: '3rem',
+        color: theme.colors.primary,
+      }}
+    >
+      <span role="img" aria-label="Pencil">
+        ✏️
+      </span>{' '}
+      Edit this page on GitHub
+    </a>
+  )
+}
+
+EditPageOnGithub.propTypes = {
+  slug: PropTypes.string.isRequired,
+}
