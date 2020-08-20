04bb11cd2336b9537eb21778934f9545eb385b4d
Frida Erdal <fridaerdal@hotmail.no>
2020-08-20 10:40:20 +0200

    Removed unused code

--- libraries/core-react/src/Pagination/PaginationItem.jsx
+++ libraries/core-react/src/Pagination/PaginationItem.jsx
@@ -1,9 +1,7 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { Button } from '../Button'
-import { Typography } from '../Typography'
 import { pagination as tokens } from './Pagination.tokens'
-//import { paginationControl } from './paginationControl'
 
 export const PaginationItem = forwardRef(function PaginationItem(
   { page, selected, onClick, ...other },
@@ -17,20 +15,16 @@ export const PaginationItem = forwardRef(function PaginationItem(
   }
   const currentColor = selected ? tokens.selectedColor : null
 
-  if (page === 'ELLIPSIS') {
-    return <Typography>...</Typography>
-  } else {
-    return (
-      <Button
-        style={{ background: currentColor }}
-        variant="ghost_icon"
-        onClick={onClick}
-        {...props}
-      >
-        {page}
-      </Button>
-    )
-  }
+  return (
+    <Button
+      style={{ background: currentColor }}
+      variant="ghost_icon"
+      onClick={onClick}
+      {...props}
+    >
+      {page}
+    </Button>
+  )
 })
 
 PaginationItem.displayName = 'eds-pagination-item'
