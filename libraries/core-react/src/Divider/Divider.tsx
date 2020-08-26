761901a014ea9646cd85f672749d5734349d4ff6
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-08-26 14:58:47 +0200

    First try for typescript support for Divider

---
+++ libraries/core-react/src/Divider/Divider.tsx
@@ -0,0 +1,47 @@
+import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { divider as tokens } from './Divider.tokens'
+
+type StyleProps = {
+  backgroundColor: string
+  marginTop: string
+  marginBottom: string
+  dividerHeight: string
+}
+
+const StyledDivider = styled.hr<StyleProps>`
+  border: 'none';
+  background-color: ${(props) => props.backgroundColor};
+  margin-top: ${(props) => props.marginTop};
+  margin-bottom: ${(props) => props.marginBottom};
+  height: ${(props) => props.dividerHeight};
+`
+
+type Props = {
+  color?: 'lighter' | 'light' | 'medium'
+  variant?: 'small' | 'medium'
+  className?: string
+}
+
+export const Divider = forwardRef<HTMLHRElement, Props>((props, ref) => {
+  const { color = 'medium', variant = 'medium', className } = props
+  const styleProps = {
+    backgroundColor: tokens.color[color],
+    marginTop: tokens[variant].spacings.top,
+    marginBottom: tokens[variant].spacings.bottom,
+    dividerHeight: tokens.height,
+  }
+  return <StyledDivider {...styleProps} className={className} ref={ref} />
+})
+
+Divider.displayName = 'eds-divider'
+
+Divider.propTypes = {
+  // Valid colors
+  color: PropTypes.oneOf(['lighter', 'light', 'medium']),
+  // Vertical spacing
+  variant: PropTypes.oneOf(['small', 'medium']),
+  /** @ignore */
+  className: PropTypes.string,
+}
