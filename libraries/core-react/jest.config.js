6ecbfcdfb0fc5dbad1c9a8ce1b7ec1df55da83c2
Victor Nystad <vnys@equinor.com>
2020-08-28 10:47:21 +0200

    update jest config (#525)

--- libraries/core-react/jest.config.js
+++ libraries/core-react/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['./rtl.setup.ts'],
   transform: {
+    '.js': 'babel-jest',
     '.(ts|tsx)': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
