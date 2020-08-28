c1709160e3b15cf9fb09cd2d4c09dfc212441de0
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-08-28 09:45:51 +0200

    Remove old rollup typescript plugin

--- libraries/core-react/rollup.config.js
+++ libraries/core-react/rollup.config.js
@@ -4,7 +4,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
 import polyfill from 'rollup-plugin-polyfill'
-//import typescript from 'rollup-plugin-typescript2'
 import typescript from '@rollup/plugin-typescript'
 
 import pkg from './package.json'
