ca7a68288457f8b20fdb86764f8179e693be2845
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-08-28 09:43:50 +0200

    Removed configuration of typescript in storybook main

--- apps/storybook-react/.storybook/main.js
+++ apps/storybook-react/.storybook/main.js
@@ -13,14 +13,4 @@ module.exports = {
       },
     },
   ],
-  /* typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
-  }, */
 }
