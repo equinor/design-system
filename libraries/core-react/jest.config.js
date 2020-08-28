22175302ebc458297ad09574c309b06d5392bdf4
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-08-28 09:59:26 +0200

    Typescript support for Divider (#520)
    * First try for typescript support for Divider
    
    * In the middle of using proptables from ts components in the storybook
    
    * Lot's of lot's of tweeks to make prop tables in storybook working. This commit is kind of a proof of concept
    
    * Remove classname, key and ref from props table
    
    * Removed debug env
    
    * Removed configuration of typescript in storybook main
    
    * Remove old testing stuffs
    
    * Remove old rollup typescript plugin
    
    * Delete copy'n pasted babel typescript transpiler, we use rollup plugin instead
    
    * Remove testing stuffs

---
+++ libraries/core-react/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  verbose: true,
+  setupFilesAfterEnv: ['./rtl.setup.ts'],
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+}
