a1fadc6e51b80cc0e129f86ed935f5058f7e4136
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-08-28 09:48:22 +0200

    Remove testing stuffs

--- libraries/core-react/src/Divider/Divider.test.tsx
+++ libraries/core-react/src/Divider/Divider.test.tsx
@@ -24,12 +24,12 @@ afterEach(cleanup)
 
 describe('Divider', () => {
   it('Has medium colour and medium spacing as default', () => {
-    const { container } = render(<Divider color="light" />)
+    const { container } = render(<Divider />)
     expect(container.firstChild).toHaveStyleRule('background-color', medium)
     expect(container.firstChild).toHaveStyleRule('margin-top', spacingMedium)
   })
   it('Sets backgroundColor to light according to color prop', () => {
-    const { container } = render(<Divider color="light" />)
+    const { container } = render(<Divider />)
     expect(container.firstChild).toHaveStyleRule('background-color', light)
   })
   it('Sets backgroundColor to lighter according to color prop', () => {
@@ -37,7 +37,7 @@ describe('Divider', () => {
     expect(container.firstChild).toHaveStyleRule('background-color', lighter)
   })
   it('Sets topMargin to small according to variant prop', () => {
-    const { container } = render(<Divider color="light" variant="small" />)
+    const { container } = render(<Divider variant="small" />)
     expect(container.firstChild).toHaveStyleRule('margin-top', spacingSmall)
   })
   it('Can extend the css for the component', () => {
