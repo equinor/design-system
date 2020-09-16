3233092e8b4cb44e81241bdd3a89eeb91f8292e3
Frida Erdal <31915755+pomfrida@users.noreply.github.com>
2020-09-16 10:41:21 +0200

    Divider width 100% (#609)
    * Divider width 100%
    
    * Fixed divider in Popover to width: auto because of margins

--- libraries/core-react/src/Popover/PopoverTitle.jsx
+++ libraries/core-react/src/Popover/PopoverTitle.jsx
@@ -16,6 +16,7 @@ const StyledPopoverTitle = styled.div`
 const StyledDivider = styled((props) => <Divider {...props} />)`
   margin-left: -16px;
   margin-right: -16px;
+  width: auto;
   max-width: 560px;
 `
 
