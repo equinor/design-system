e12d56c5769b94c5a2c3fe86a152896142912d3a
Michael Marszalek <mmarsz@equinor.com>
2020-09-21 07:59:30 +0200

    Added description to .add

--- libraries/core-react/src/Icon/library.ts
+++ libraries/core-react/src/Icon/library.ts
@@ -5,7 +5,8 @@ type IconRecord = Record<Name, IconData>
 
 let _icons: IconRecord = {}
 let count = 0
-
+/** Add icons to library to be used for rendering using name.
+This needs to be done lonly once */
 export const add = (icons: IconRecord): void => {
   _icons = {
     ..._icons,
