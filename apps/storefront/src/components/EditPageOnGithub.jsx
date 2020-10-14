3b34aa1473924b09e9a0ec879dfdf615c8351436
Sven van Steenis <sven.vsteenis@gmail.com>
2020-10-14 17:00:59 +0200

    ✨ refactor EditPageOnGitHub
    - replace pencil with EDS Icon
    - use styled-components instead of inline css

--- apps/storefront/src/components/EditPageOnGithub.jsx
+++ apps/storefront/src/components/EditPageOnGithub.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useThemeUI } from 'theme-ui'
+import { Icon } from '@equinor/eds-core-react'
+import { edit_text } from '@equinor/eds-icons'
+import styled from 'styled-components'
+
+Icon.add({ edit_text })
+
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  margin-top: 3rem;
+  color: ${(props) => props.color || '#007079'};
+
+  & > svg {
+    margin-right: 8px;
+  }
+`
 
 export const EditPageOnGithub = ({ slug }) => {
   const context = useThemeUI()
   const { theme } = context
 
   return (
-    <a
+    <Link
+      color={theme.colors.primary}
       href={`https://github.com/equinor/design-system/blob/develop/apps/storefront/${slug}`}
-      style={{
-        display: 'block',
-        marginTop: '3rem',
-        color: theme.colors.primary,
-      }}
     >
-      <span role="img" aria-label="Pencil">
-        ✏️
-      </span>{' '}
+      <Icon name="edit_text" size={16} />
       Edit this page on GitHub
-    </a>
+    </Link>
   )
 }
 
