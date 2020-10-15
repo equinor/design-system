70521f8c6617f79786ae35749e96ce5b03b128cf
Michael Marszalek <mmarsz@equinor.com>
2020-10-15 08:38:07 +0200

    Fixed test failing when using json files

--- libraries/core-react/jest.config.js
+++ libraries/core-react/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['./rtl.setup.ts'],
   transform: {
-    '.js': 'babel-jest',
+    '.(js)': 'babel-jest',
     '.(ts|tsx)': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
