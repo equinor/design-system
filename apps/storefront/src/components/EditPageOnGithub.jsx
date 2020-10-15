ccc287f116dfe7e1ed58495200bbdf70be1c20ae
Sven van Steenis <SvSven@users.noreply.github.com>
2020-10-15 13:54:52 +0200

    fix link to edit page on github in storefront (#676)
    * fix link to edit page on github
    
    * ✨ refactor EditPageOnGitHub
    
    - replace pencil with EDS Icon
    - use styled-components instead of inline css
    
    * fix: rename Link => ExternalLink styled component

--- apps/storefront/src/components/EditPageOnGithub.jsx
+++ apps/storefront/src/components/EditPageOnGithub.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useThemeUI } from 'theme-ui'
+import { Icon } from '@equinor/eds-core-react'
+import { edit_text } from '@equinor/eds-icons'
+import styled from 'styled-components'
+
+Icon.add({ edit_text })
+
+const ExternalLink = styled.a`
+  display: flex;
+  align-items: center;
+  margin-top: 3rem;
+  color: ${(props) => props.color || '#007079'};
+
+  & > svg {
+    margin-right: 8px;
+  }
+`
 
 export const EditPageOnGithub = ({ slug }) => {
   const context = useThemeUI()
   const { theme } = context
 
   return (
-    <a
-      href={`https://github.com/equinor/design-system/tree/documentation/apps/storefront/src/content/${
-        slug === '/' ? `index` : slug
-      }.mdx`}
-      style={{
-        display: 'block',
-        marginTop: '3rem',
-        color: theme.colors.primary,
-      }}
+    <ExternalLink
+      color={theme.colors.primary}
+      href={`https://github.com/equinor/design-system/blob/develop/apps/storefront/${slug}`}
     >
-      <span role="img" aria-label="Pencil">
-        ✏️
-      </span>{' '}
+      <Icon name="edit_text" size={16} />
       Edit this page on GitHub
-    </a>
+    </ExternalLink>
   )
 }
 
