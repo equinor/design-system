32e8a43896833a94cf99b4ab18ad0a33b683ec23
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-10-16 07:47:02 +0200

    Table typescript take two (#682)
    * convert to typescript
    
    * Change suffix
    
    * Radius optional

---
+++ libraries/core-react/src/Table/Body.tsx
@@ -0,0 +1,12 @@
+import React, { HTMLAttributes } from 'react'
+import styled from 'styled-components'
+
+const TableBase = styled.tbody``
+
+type Props = HTMLAttributes<HTMLTableSectionElement>
+
+export const Body = ({ children, ...props }: Props): JSX.Element => {
+  return <TableBase {...props}>{children}</TableBase>
+}
+
+Body.displayName = 'eds-table-body'
--- libraries/core-react/src/Table/Head.tsx
+++ libraries/core-react/src/Table/Head.tsx
@@ -1,39 +1,31 @@
-// @ts-nocheck
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { FunctionComponent } from 'react'
 import styled, { css } from 'styled-components'
-import { getTokens } from './Table.tokens'
+import { getTokens, TableCell } from './Table.tokens'
+import type { Border } from '@equinor/eds-tokens'
 
-const borderTemplate = (borders) =>
+const borderTemplate = (borders: { bottom: Border }): string =>
   Object.keys(borders).reduce((acc, val) => {
-    const { color, width } = borders[val]
+    const { color, width }: Border = borders[val] as Border
     return `${acc} border-${val}: ${width} solid ${color}; \n`
   }, '')
 
-const StyledTableHead = styled.thead`
+type StyledTableHeadProps = {
+  token: TableCell
+}
+
+const StyledTableHead = styled.thead<StyledTableHeadProps>`
   ${({ token: { borders, background } }) => css`
       ${borderTemplate(borders)}
       background: ${background};`}
 `
 
-export const Head = ({ children, ...props }) => {
+export const Head: FunctionComponent = ({ children, ...props }) => {
   const token = getTokens('th', 'text')
   return (
     <StyledTableHead token={token} {...props}>
       {children}
     </StyledTableHead>
   )
-}
-
-Head.propTypes = {
-  /** @ignore */
-  className: PropTypes.string,
-  /** @ignore */
-  children: PropTypes.node.isRequired,
-}
-
-Head.defaultProps = {
-  className: '',
 }
 
 Head.displayName = 'eds-table-head'
