b2eebcff95ef69c1ec496bf583750a460a12aa90
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-10-19 07:34:11 +0200

    Snackbar types (#687)
    * 🚧 Rename to ts
    
    * 🏷️ Add types for snackbar

--- libraries/core-react/src/Snackbar/index.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { SnackbarAction } from './SnackbarAction'
-import { Snackbar } from './Snackbar'
-
-Snackbar.SnackbarAction = SnackbarAction
-
-export { Snackbar }
-
-/* export { useSnackbars } from './snackbar-hook' */
