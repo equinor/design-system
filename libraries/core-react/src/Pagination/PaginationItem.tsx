e754dfa7639a1d3d55e91144396e26f748b0d505
Frida Erdal <fridaerdal@hotmail.no>
2020-10-19 14:53:30 +0200

    PaginationItem to typescript

--- libraries/core-react/src/Pagination/PaginationItem.tsx
+++ libraries/core-react/src/Pagination/PaginationItem.tsx
@@ -1,50 +1,37 @@
-// @ts-nocheck
 import React, { forwardRef } from 'react'
-import PropTypes from 'prop-types'
 import { Button } from '../Button'
 import { pagination as tokens } from './Pagination.tokens'
 
-export const PaginationItem = forwardRef(function PaginationItem(
-  { page, selected, onClick, ...other },
-  ref,
-) {
-  const props = {
-    ref,
-    page,
-    selected,
-    ...other,
-  }
-  const currentColor = selected ? tokens.selectedColor : null
+type Props = {
+  // Current page number
+  page: number
+  // If current page is selected
+  selected: boolean
+  // Click function
+  onClick?: () => void
+} & Omit<JSX.IntrinsicElements['button'], 'color' | 'ref'>
 
-  return (
-    <Button
-      style={{ background: currentColor }}
-      variant="ghost_icon"
-      onClick={onClick}
-      {...props}
-    >
-      {page}
-    </Button>
-  )
-})
+export const PaginationItem = forwardRef<HTMLButtonElement, Props>(
+  function PaginationItem({ page, selected, onClick, ...other }, ref) {
+    const props = {
+      ref,
+      page,
+      selected,
+      ...other,
+    }
+    const currentColor = selected ? tokens.selectedColor : null
+
+    return (
+      <Button
+        style={{ background: currentColor }}
+        variant="ghost_icon"
+        onClick={onClick}
+        {...props}
+      >
+        {page}
+      </Button>
+    )
+  },
+)
 
 PaginationItem.displayName = 'eds-pagination-item'
-
-PaginationItem.propTypes = {
-  // Current page number
-  page: PropTypes.number.isRequired,
-  // If current page is selected
-  selected: PropTypes.bool.isRequired,
-  // Click function
-  onClick: PropTypes.func,
-  /** @ignore */
-  children: PropTypes.node,
-  /** @ignore */
-  className: PropTypes.string,
-}
-
-PaginationItem.defaultProps = {
-  className: '',
-  onClick: () => {},
-  children: undefined,
-}
--- libraries/core-react/src/Pagination/paginationControl.ts
+++ libraries/core-react/src/Pagination/paginationControl.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import React from 'react'
 
 export function PaginationControl(pages, activePage) {
