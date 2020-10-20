99f8b4489b632fb7b15336114022330de217a6f4
Frida Erdal <fridaerdal@hotmail.no>
2020-10-20 09:13:43 +0200

    removed proptypes

--- libraries/core-react/src/Pagination/Pagination.tsx
+++ libraries/core-react/src/Pagination/Pagination.tsx
@@ -5,7 +5,6 @@ import React, {
   MouseEvent,
   KeyboardEvent,
 } from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Button } from '../Button'
 import { Icon } from '../Icon'
