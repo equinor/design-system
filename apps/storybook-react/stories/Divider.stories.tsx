0075c61f8dd61a859f717165a315164a3afc2cdd
Victor Nystad <vnys@equinor.com>
2020-10-23 18:44:31 +0200

    🎨 Remove unused imports

--- apps/storybook-react/stories/Divider.stories.tsx
+++ apps/storybook-react/stories/Divider.stories.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, { FunctionComponent, HTMLProps } from 'react'
-import styled, { StyledFunction, ThemedStyledFunction } from 'styled-components'
+import React from 'react'
+import styled from 'styled-components'
 
 import { Divider, DividerProps } from '@equinor/eds-core-react'
 import { Story, Meta } from '@storybook/react/types-6-0'
