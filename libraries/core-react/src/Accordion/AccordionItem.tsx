5d50c288c3316f994490cc90a5927bf688e67e2b
Frida Erdal <frida.erdal@olavstoppen.no>
2020-10-23 14:10:36 +0200

    🚚 Props renamed to AccordionItemProps
    Props should be named as their respective Component

--- libraries/core-react/src/Accordion/AccordionItem.tsx
+++ libraries/core-react/src/Accordion/AccordionItem.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef, useState, useEffect, ReactElement } from 'react'
 import type { AccordionProps } from './Accordion.types'
 
-type Props = {
+type AccordionItemProps = {
   index?: number
   accordionId?: string
   /** Is AccordionItem expanded */
@@ -11,57 +11,59 @@ type Props = {
 } & JSX.IntrinsicElements['div'] &
   AccordionProps
 
-const AccordionItem = forwardRef<HTMLDivElement, Props>(function AccordionItem(
-  {
-    headerLevel,
-    chevronPosition,
-    index = 0,
-    accordionId,
-    isExpanded,
-    children,
-    disabled,
-    ...props
+const AccordionItem = forwardRef<HTMLDivElement, AccordionItemProps>(
+  function AccordionItem(
+    {
+      headerLevel,
+      chevronPosition,
+      index = 0,
+      accordionId,
+      isExpanded,
+      children,
+      disabled,
+      ...props
+    },
+    ref,
+  ) {
+    const [expanded, setExpanded] = useState(isExpanded)
+
+    const toggleExpanded = () => {
+      setExpanded(!expanded)
+    }
+
+    const Children = React.Children.map(children, (child, childIndex) => {
+      const headerId = `${accordionId}-header-${index + 1}`
+      const panelId = `${accordionId}-panel-${index + 1}`
+
+      return childIndex === 0
+        ? React.cloneElement(child as ReactElement, {
+            isExpanded: expanded,
+            toggleExpanded,
+            id: headerId,
+            panelId,
+            headerLevel,
+            chevronPosition,
+            parentIndex: index,
+            disabled,
+          })
+        : React.cloneElement(child as ReactElement, {
+            hidden: !expanded,
+            id: panelId,
+            headerId,
+          })
+    })
+
+    useEffect(() => {
+      setExpanded(isExpanded)
+    }, [isExpanded])
+
+    return (
+      <div {...props} ref={ref}>
+        {Children}
+      </div>
+    )
   },
-  ref,
-) {
-  const [expanded, setExpanded] = useState(isExpanded)
-
-  const toggleExpanded = () => {
-    setExpanded(!expanded)
-  }
-
-  const Children = React.Children.map(children, (child, childIndex) => {
-    const headerId = `${accordionId}-header-${index + 1}`
-    const panelId = `${accordionId}-panel-${index + 1}`
-
-    return childIndex === 0
-      ? React.cloneElement(child as ReactElement, {
-          isExpanded: expanded,
-          toggleExpanded,
-          id: headerId,
-          panelId,
-          headerLevel,
-          chevronPosition,
-          parentIndex: index,
-          disabled,
-        })
-      : React.cloneElement(child as ReactElement, {
-          hidden: !expanded,
-          id: panelId,
-          headerId,
-        })
-  })
-
-  useEffect(() => {
-    setExpanded(isExpanded)
-  }, [isExpanded])
-
-  return (
-    <div {...props} ref={ref}>
-      {Children}
-    </div>
-  )
-})
+)
 
 // AccordionItem.displayName = 'AccordionItem'
 
