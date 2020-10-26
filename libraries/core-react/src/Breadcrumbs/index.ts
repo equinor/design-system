c64b6bb984c897de03020125c68c4c137c403b20
Frida Erdal <frida.erdal@olavstoppen.no>
2020-10-26 10:33:20 +0100

    🚚 Export Props

--- libraries/core-react/src/Breadcrumbs/index.ts
+++ libraries/core-react/src/Breadcrumbs/index.ts
@@ -1,4 +1,7 @@
-import { Breadcrumbs as BaseComponent } from './Breadcrumbs'
+import {
+  Breadcrumbs as BaseComponent,
+  BreadcrumbsProps as Props,
+} from './Breadcrumbs'
 import { Breadcrumb } from './Breadcrumb'
 
 type BreadcrumbsTypes = typeof BaseComponent & {
@@ -10,3 +13,4 @@ const Breadcrumbs = BaseComponent as BreadcrumbsTypes
 Breadcrumbs.Breadcrumb = Breadcrumb
 
 export { Breadcrumbs }
+export type BreadcrumbsProps = Props
