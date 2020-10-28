6bd8e2a6344d7058418999402cf10a5c3d9a9216
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-10-28 11:24:46 +0100

    ♻️ Refactor linear progress

--- apps/storybook-react/stories/Progress Indicators/Linear.stories.tsx
+++ apps/storybook-react/stories/Progress Indicators/Linear.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { LinearProgress } from '@equinor/eds-core-react'
+import { LinearProgress, LinearProgressProps } from '@equinor/eds-core-react'
 import styled from 'styled-components'
+import { Meta, Story } from '@storybook/react'
 
 const Wrapper = styled.div`
   display: grid;
@@ -15,9 +16,39 @@ const Wrapper = styled.div`
 export default {
   title: 'Components/Progress Indicators/Linear',
   component: LinearProgress,
+} as Meta
+
+export const Default: Story<LinearProgressProps> = (args) => {
+  const { value = 0, variant } = args
+  let controllableValue: number | null
+  variant === 'indeterminate'
+    ? (controllableValue = null)
+    : (controllableValue = value)
+  const [progress, setProgress] = useState(controllableValue)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 100
+        }
+        const diff = Math.random() * 10
+        return Math.min(oldProgress + diff, 100)
+      })
+    }, 500)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+  return <LinearProgress {...args} value={progress} />
 }
 
-export const Determinate = () => {
+export const Indeterminate: Story<LinearProgressProps> = () => (
+  <LinearProgress />
+)
+
+export const Determinate: Story<LinearProgressProps> = () => {
   const [progress, setProgress] = useState(0)
 
   useEffect(() => {
@@ -35,19 +66,7 @@ export const Determinate = () => {
       clearInterval(timer)
     }
   }, [])
-  return (
-    <Wrapper>
-      <LinearProgress variant="determinate" value={progress} />
-    </Wrapper>
-  )
-}
-
-export const Indeterminate = () => {
-  return (
-    <Wrapper>
-      <LinearProgress />
-    </Wrapper>
-  )
+  return <LinearProgress variant="determinate" value={progress} />
 }
 
 Determinate.storyName = 'Determinate'
--- libraries/core-react/src/Progress/index.ts
+++ libraries/core-react/src/Progress/index.ts
@@ -1,4 +1,4 @@
-export { LinearProgress } from './Linear/LinearProgress'
+export * from './Linear/LinearProgress'
 export * from './Circular/CircularProgress'
 export { StarProgress } from './Star/StarProgress'
 export * from './Dots/DotProgress'
