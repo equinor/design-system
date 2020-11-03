a62a08fefa7311a14d1193a093ea1fbb07bcd3fd
Michael Marszalek <mmarsz@equinor.com>
2020-11-03 08:56:11 +0100

    Icon typescript story (#812)
    * ♻️ Renamed story
    
    * 🚧  updating story
    
    * 🚧 Working story but typescript error
    
    * 🐛 Fixed typescript error in story

--- libraries/core-react/src/Icon/index.ts
+++ libraries/core-react/src/Icon/index.ts
@@ -1,4 +1,4 @@
-import { Icon as IconComponent } from './Icon'
+import { Icon as IconComponent, IconProps } from './Icon'
 import { add } from './library'
 import type { IconType } from './Icon.types'
 
@@ -7,3 +7,4 @@ const Icon = IconComponent as IconType
 Icon.add = add
 
 export { Icon }
+export type { IconProps }
