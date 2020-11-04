53480b993975d096f48efc028b0429ef438fbbd0
Victor Nystad <vnys@equinor.com>
2020-11-04 17:35:55 +0100

    🎨 Update stories

--- apps/storybook-react/stories/Dialog.stories.tsx
+++ apps/storybook-react/stories/Dialog.stories.tsx
@@ -1,25 +1,37 @@
 import React, { Fragment, useState } from 'react'
-import { withKnobs, select } from '@storybook/addon-knobs'
-import { Dialog, Button, Scrim, Typography } from '@equinor/eds-core-react'
+import {
+  Dialog,
+  DialogProps,
+  Button,
+  Scrim,
+  Typography,
+} from '@equinor/eds-core-react'
 import styled from 'styled-components'
+import { Story, Meta } from '@storybook/react/types-6-0'
 
 const { Actions, Title, CustomContent } = Dialog
 
+export default {
+  title: 'Components/Dialog',
+  component: Dialog,
+  subcomponents: { Actions, Title, CustomContent },
+  parameters: {
+    docs: {
+      description: {
+        component: `A basic dialog component`,
+      },
+    },
+  },
+} as Meta
+
 const Body = styled.div`
-  height: calc(100vh - 64px);
+  height: 200px;
   background: #ebebeb;
   display: grid;
   grid-template-rows: 1fr auto 1fr;
   padding: 32px;
   grid-gap: 32px;
-`
-
-const BodyTypes = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  align-items: start;
-  padding: 32px;
-  grid-gap: 32px;
+  position: relative;
 `
 
 const TempButtonWrapper = styled.div`
@@ -81,60 +93,43 @@ const ACTION_CHOICES = {
   ),
 }
 
-export default {
-  title: 'Components/Dialog',
-  component: Dialog,
-}
+export const Default: Story<DialogProps> = () => (
+  <Dialog>
+    <Title>{TITLE_CHOICES['text']}</Title>
+    <CustomContent>{CUSTOM_CONTENT_CHOICES['description']}</CustomContent>
+    <Actions>
+      <TempButtonWrapper>
+        <Button>Cancel</Button>
+        <Button>OK</Button>
+      </TempButtonWrapper>
+    </Actions>
+  </Dialog>
+)
 
-export const knobs = () => {
+export const WithScrim: Story<DialogProps> = () => {
   const [visibleScrim, setVisibleScrim] = useState(false)
   const handleClose = () => {
     setVisibleScrim(!visibleScrim)
   }
-
-  const titleChoice = select('Title', [...Object.keys(TITLE_CHOICES)], 'text')
-  const contentChoice = select(
-    'CustomContent',
-    [...Object.keys(CUSTOM_CONTENT_CHOICES)],
-    'description',
-  )
-  const actionsChoice = select(
-    'Actions',
-    [...Object.keys(ACTION_CHOICES)],
-    'buttons',
-  )
-
   return (
     <Body>
-      <Typography variant="body_short">Top of page</Typography>
-      <Typography variant="body_short">Center page.</Typography>
       <div>
         <Button onClick={() => setVisibleScrim(true)}>Trigger Dialog</Button>
       </div>
-      <Typography variant="body_short">Bottom of page</Typography>
       {visibleScrim && (
         <Scrim onClose={handleClose}>
           <Dialog>
-            <Title>{TITLE_CHOICES[titleChoice]}</Title>
-            <CustomContent
-              scrollable={contentChoice === 'scroll' ? true : false}
-            >
-              {CUSTOM_CONTENT_CHOICES[contentChoice]}
+            <Title>{TITLE_CHOICES['text']}</Title>
+            <CustomContent scrollable>
+              {CUSTOM_CONTENT_CHOICES['scroll']}
             </CustomContent>
             <Actions>
-              {actionsChoice === 'buttons' ? (
-                <TempButtonWrapper>
-                  <Button
-                    variant="ghost"
-                    onClick={() => setVisibleScrim(false)}
-                  >
-                    Cancel
-                  </Button>
-                  <Button onClick={() => setVisibleScrim(false)}>OK</Button>
-                </TempButtonWrapper>
-              ) : (
-                ACTION_CHOICES[actionsChoice]
-              )}
+              <TempButtonWrapper>
+                <Button variant="ghost" onClick={() => setVisibleScrim(false)}>
+                  Cancel
+                </Button>
+                <Button onClick={() => setVisibleScrim(false)}>OK</Button>
+              </TempButtonWrapper>
             </Actions>
           </Dialog>
         </Scrim>
@@ -143,53 +138,41 @@ export const knobs = () => {
   )
 }
 
-export const types = () => {
-  const [visibleScrim, setVisibleScrim] = useState(true)
-  const handleClose = () => {
-    setVisibleScrim(!visibleScrim)
-  }
+export const TextPlusAction: Story<DialogProps> = () => (
+  <Dialog>
+    <Title>Text + actions</Title>
+    <CustomContent>{CUSTOM_CONTENT_CHOICES['description']}</CustomContent>
+    <Actions>{ACTION_CHOICES['buttons']}</Actions>
+  </Dialog>
+)
 
-  return (
-    visibleScrim && (
-      <Scrim onClose={handleClose} isDismissable={false}>
-        <BodyTypes>
-          <Dialog>
-            <Title>Text + actions</Title>
-            <CustomContent>
-              {CUSTOM_CONTENT_CHOICES['description']}
-            </CustomContent>
-            <Actions>{ACTION_CHOICES['buttons']}</Actions>
-          </Dialog>
-          <Dialog>
-            <Title>Placeholder + actions</Title>
-            <CustomContent>{CUSTOM_CONTENT_CHOICES['empty']}</CustomContent>
-            <Actions>{ACTION_CHOICES['buttons']}</Actions>
-          </Dialog>
-          <Dialog>
-            <Title>Placeholder</Title>
-            <CustomContent>
-              {CUSTOM_CONTENT_CHOICES['emptyLarge']}
-            </CustomContent>
-            <Actions>{ACTION_CHOICES['none']}</Actions>
-          </Dialog>
-          <Dialog>
-            <Title>Scrollable + actions</Title>
-            <CustomContent scrollable>
-              {CUSTOM_CONTENT_CHOICES['scroll']}
-            </CustomContent>
-            <Actions>{ACTION_CHOICES['buttons']}</Actions>
-          </Dialog>
-          <Dialog>
-            <CustomContent>
-              {CUSTOM_CONTENT_CHOICES['description']}
-            </CustomContent>
-            <Actions>{ACTION_CHOICES['buttons']}</Actions>
-          </Dialog>
-        </BodyTypes>
-      </Scrim>
-    )
-  )
-}
+export const PlaceholderPlusAction: Story<DialogProps> = () => (
+  <Dialog>
+    <Title>Placeholder + actions</Title>
+    <CustomContent>{CUSTOM_CONTENT_CHOICES['empty']}</CustomContent>
+    <Actions>{ACTION_CHOICES['buttons']}</Actions>
+  </Dialog>
+)
 
-knobs.storyName = 'With knobs'
-knobs.decorators = [withKnobs]
+export const PlaceholderOnly: Story<DialogProps> = () => (
+  <Dialog>
+    <Title>Placeholder</Title>
+    <CustomContent>{CUSTOM_CONTENT_CHOICES['emptyLarge']}</CustomContent>
+    <Actions>{ACTION_CHOICES['none']}</Actions>
+  </Dialog>
+)
+
+export const ScrollablePlusActions: Story<DialogProps> = () => (
+  <Dialog>
+    <Title>Scrollable + actions</Title>
+    <CustomContent scrollable>{CUSTOM_CONTENT_CHOICES['scroll']}</CustomContent>
+    <Actions>{ACTION_CHOICES['buttons']}</Actions>
+  </Dialog>
+)
+
+export const NoTitle: Story<DialogProps> = () => (
+  <Dialog>
+    <CustomContent>{CUSTOM_CONTENT_CHOICES['description']}</CustomContent>
+    <Actions>{ACTION_CHOICES['buttons']}</Actions>
+  </Dialog>
+)
