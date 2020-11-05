6ded6a678cd46062222f3564b5c1ec91a9ccf1b5
Frida Erdal <frida.erdal@olavstoppen.no>
2020-11-05 13:21:18 +0100

    ♻️ Table element types conformed

--- libraries/core-react/src/Table/Cell.tsx
+++ libraries/core-react/src/Table/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { TdHTMLAttributes, ThHTMLAttributes } from 'react'
+import React, { TdHTMLAttributes } from 'react'
 import styled, { css } from 'styled-components'
 import type { Border } from '@equinor/eds-tokens'
 import { getTokens, TableCell } from './Table.tokens'
