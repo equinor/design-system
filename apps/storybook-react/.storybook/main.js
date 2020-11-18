c3a380b1a2232fe812e0feda7509055e3615a8bd
Victor Nystad <vnys@equinor.com>
2020-11-18 12:42:27 +0100

    update globs

--- apps/storybook-react/.storybook/main.js
+++ apps/storybook-react/.storybook/main.js
@@ -3,16 +3,10 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
 module.exports = {
-  stories: [
-    '../docs/**/*.story.mdx',
-    '../stories/**/*.stor(y|ies).(jsx|mdx|tsx)',
-  ],
+  stories: ['../docs/**/*.story.mdx', '../stories/**/*.stories.@(jsx|mdx|tsx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    // '@storybook/addon-storysource',
-    '@storybook/addon-a11y',
-    '@storybook/addon-knobs',
     {
       name: '@storybook/addon-docs',
       options: {
