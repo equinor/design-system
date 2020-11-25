e878cc471adbff1f915fc4e716b80f94af1bc707
Michael Marszalek <mmarsz@equinor.com>
2020-11-25 12:08:30 +0100

    ✨ Added interactions base token (#915)
    * ✨ Added interactions base token
    
    * ♻️ lowercasing blendmode

---
+++ libraries/tokens/base/interactions.ts
@@ -0,0 +1,15 @@
+export const interactions = {
+  pressed_dark_overlay: {
+    blendMode: 'darken',
+    pressedColor: 'rgba(0, 0, 0, 0.2)',
+  },
+  focused: {
+    style: 'dashed',
+    color: 'rgba(0, 112, 121, 1)',
+    width: '1px',
+  },
+  pressed_light_overlay: {
+    blendMode: 'pass_through',
+    pressedColor: 'rgba(255, 255, 255, 0.2)',
+  },
+}
