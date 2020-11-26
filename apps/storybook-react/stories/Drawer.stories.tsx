ff5a9e201e19932139025979d8bd96961aa401cc
Frida Erdal <frida.erdal@olavstoppen.no>
2020-11-26 13:23:07 +0100

    📝 Drawer story

---
+++ apps/storybook-react/stories/Drawer.stories.tsx
@@ -0,0 +1,239 @@
+import React, { Fragment, HTMLAttributes } from 'react'
+import styled from 'styled-components'
+import { Drawer, TopBar, Icon, Typography } from '@equinor/eds-core-react'
+import { Story, Meta } from '@storybook/react'
+
+import {
+  account_circle,
+  accessible,
+  notifications,
+  fullscreen,
+  menu,
+  business,
+  meeting_room,
+  cafe,
+  world,
+} from '@equinor/eds-icons'
+
+const {
+  DrawerContainer,
+  DrawerList,
+  DrawerTreeItem,
+  DrawerItem,
+  DrawerSubtitle,
+  DrawerLabel,
+} = Drawer
+const { Header, CustomContent } = TopBar
+const {
+  DrawerContainer,
+  DrawerLabel,
+  DrawerSubtitle,
+  DrawerItem,
+  DrawerList,
+} = Drawer
+
+const icons = {
+  account_circle,
+  accessible,
+  notifications,
+  fullscreen,
+  menu,
+  business,
+  meeting_room,
+  cafe,
+  world,
+}
+
+Icon.add(icons)
+
+type WrapperProps = { tabIndex?: string } & HTMLAttributes<HTMLDivElement>
+
+const Wrapper = styled.div<WrapperProps>`
+  display: grid;
+  grid-template-columns: 256px auto;
+  grid-template-rows: auto;
+`
+
+const Body = styled.div`
+  height: 100%;
+  min-height: 750px;
+  background: #ebebeb;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  > * {
+    margin-left: 40px;
+  }
+`
+
+type Props = {}
+
+export default {
+  title: 'Components/Drawer',
+  component: Drawer,
+  subcomponents: {
+    DrawerContainer,
+    DrawerLabel,
+    DrawerSubtitle,
+    DrawerItem,
+    DrawerList,
+  },
+} as Meta
+
+const menu_items = []
+
+export const Default: Story<Props> = () => {
+  return (
+    <Fragment>
+      <TopBar>
+        <Header>
+          {' '}
+          <Icon name="menu" size={16} />
+        </Header>
+        <CustomContent>Drawer example</CustomContent>
+      </TopBar>
+      <Wrapper tabIndex="0">
+        <Drawer>
+          <DrawerContainer>
+            <DrawerList level="grandparent">
+              <DrawerItem>
+                <Icon name="account_circle" size={16} />
+                <DrawerLabel>Grandparent 1</DrawerLabel>
+                <DrawerList level="parent">
+                  <DrawerItem>
+                    <DrawerLabel>Parent 1</DrawerLabel>
+                    <DrawerList>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                  <DrawerItem>
+                    <DrawerLabel>Parent 2</DrawerLabel>
+                    <DrawerList>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                </DrawerList>
+              </DrawerItem>
+              <DrawerItem>
+                <Icon name="accessible" size={16} />
+                <DrawerLabel>Grandparent 2</DrawerLabel>
+                <DrawerList level="parent" open>
+                  <DrawerItem>
+                    <DrawerLabel>Parent 1</DrawerLabel>
+                    <DrawerList open>
+                      <DrawerItem active>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1 active
+                        </Typography>
+                      </DrawerItem>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 2
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                  <DrawerItem>
+                    <DrawerLabel>Parent 2</DrawerLabel>
+                    <DrawerList>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                  <DrawerItem>
+                    <DrawerLabel>Parent 3</DrawerLabel>
+                    <DrawerList>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                  <DrawerItem>
+                    <DrawerLabel>Parent 4</DrawerLabel>
+                    <DrawerList>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1
+                        </Typography>
+                      </DrawerItem>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 2
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                </DrawerList>
+              </DrawerItem>
+              <DrawerItem>
+                <Icon name="notifications" size={16} />
+                <DrawerLabel
+                  variant="body_short"
+                  title="Grandparent with a really long name 3"
+                >
+                  Grandparent with a really long name 3
+                </DrawerLabel>
+                <DrawerList level="parent">
+                  <DrawerItem>
+                    <DrawerLabel>Parent 1</DrawerLabel>
+                    <DrawerList>
+                      <DrawerItem>
+                        <Typography variant="body_short" link href={'#href'}>
+                          Child 1
+                        </Typography>
+                      </DrawerItem>
+                    </DrawerList>
+                  </DrawerItem>
+                </DrawerList>
+              </DrawerItem>
+            </DrawerList>
+            <DrawerSubtitle name="Grandparent orphans" />
+            <DrawerList level="grandparent">
+              <DrawerItem>
+                <Icon name="business" size={16} />
+                <DrawerLabel>Grandparent orphans 1</DrawerLabel>
+                <DrawerList>
+                  <DrawerItem>
+                    <Typography variant="body_short" link href={'#href'}>
+                      Child 1
+                    </Typography>
+                  </DrawerItem>
+                  <DrawerItem>
+                    <Typography variant="body_short" link href={'#href'}>
+                      Child 2
+                    </Typography>
+                  </DrawerItem>
+                </DrawerList>
+              </DrawerItem>
+            </DrawerList>
+          </DrawerContainer>
+        </Drawer>
+        <Body>
+          <p>Top of page</p>
+          <p>Middle of page</p>
+          <p>Bottom of page</p>
+        </Body>
+      </Wrapper>
+    </Fragment>
+  )
+}
