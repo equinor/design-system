542e220d01cb8631edf89fe3028144167c351ec3
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-11-26 08:53:28 +0100

    ♻️ Reuse items across examples

--- apps/storybook-react/stories/Select/MultiSelect.stories.tsx
+++ apps/storybook-react/stories/Select/MultiSelect.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { MultiSelect } from '@equinor/eds-core-react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import styled from 'styled-components'
+import { items } from './data'
 
 export default {
   title: 'Components/Select/MultiSelect',
@@ -12,26 +13,12 @@ const Container = styled.div`
   margin-bottom: 150px;
 `
 
-const items = [
-  'Oslo',
-  'Rogaland',
-  'Møre og Romsdal',
-  'Nordland',
-  'Viken',
-  'Innlandet',
-  'Vestfold og Telemark',
-  'Agder',
-  'Vestland',
-  'Trøndelag',
-  'Troms og Finnmark',
-]
-
 export const Default: Story = (args) => (
   <Container>
     <MultiSelect
+      label="Choose an element"
       {...args}
       items={items}
-      label="Choose an element"
     ></MultiSelect>
   </Container>
 )
--- apps/storybook-react/stories/Select/SingleSelect.stories.tsx
+++ apps/storybook-react/stories/Select/SingleSelect.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SingleSelect, SingleSelectProps } from '@equinor/eds-core-react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import styled from 'styled-components'
+import { items } from './data'
 
 export default {
   title: 'Components/Select/SingleSelect',
@@ -12,26 +13,12 @@ const Container = styled.div`
   margin-bottom: 150px;
 `
 
-const items = [
-  'Oslo',
-  'Rogaland',
-  'Møre og Romsdal',
-  'Nordland',
-  'Viken',
-  'Innlandet',
-  'Vestfold og Telemark',
-  'Agder',
-  'Vestland',
-  'Trøndelag',
-  'Troms og Finnmark',
-]
-
 export const Default: Story<SingleSelectProps> = (args) => (
   <Container>
     <SingleSelect
+      label="Choose an element"
       {...args}
       items={items}
-      label="Choose an element"
     ></SingleSelect>
   </Container>
 )
