fac717cc35c9b36f675da2420e59256dfbfe8509
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-11-27 10:17:55 +0100

    🚨 Bonus: Fix eslint warning

--- apps/storefront/src/gatsby-theme-docz/wrapper.js
+++ apps/storefront/src/gatsby-theme-docz/wrapper.js
@@ -3,13 +3,11 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet-async'
-import SEO from '../components/seo'
 // The doc prop contains some metadata about the page being rendered that you can use.
 // Except that it is undefined :/
 const Wrapper = ({ children }) => {
   return (
     <>
-      {/*   <SEO /> */}
       <Helmet>
         <meta charSet="utf-8" />
         <link
