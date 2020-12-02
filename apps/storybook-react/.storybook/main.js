0a60278c85020324e88211365cda2f22d43f9637
Victor Nystad <vnys@equinor.com>
2020-12-02 09:37:50 +0100

    🐛 Fix storybook (#937)
    Copy libraries into storybook in Docker to avoid problems with pnpm when building the storybook for production

--- apps/storybook-react/.storybook/main.js
+++ apps/storybook-react/.storybook/main.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 module.exports = {
-  // typescript: {
-  //   reactDocgen: 'react-docgen',
-  // },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
   stories: [
     '../docs/**/*.story.mdx',
     '../stories/**/*.stor(y|ies).(jsx|mdx|tsx)',
@@ -19,4 +25,34 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config, { configType }) => {
+    // production build in Docker, where “libraries” is copied into the storybook root
+
+    const isProduction = configType === 'PRODUCTION'
+
+    const pathToLibraries = isProduction ? '../libraries' : '../../../libraries'
+
+    config.resolve.alias['styled-components'] = path.resolve(
+      './node_modules',
+      'styled-components',
+    )
+
+    config.resolve.alias['@hooks'] = path.resolve(
+      __dirname,
+      `${pathToLibraries}/${isProduction ? `eds-` : ''}core-react/src/hooks`,
+    )
+    config.resolve.alias['@utils'] = path.resolve(
+      __dirname,
+      `${pathToLibraries}/${isProduction ? `eds-` : ''}core-react/src/utils`,
+    )
+
+    isProduction
+      ? (config.resolve.alias['@equinor'] = path.resolve(
+          __dirname,
+          pathToLibraries,
+        ))
+      : null
+
+    return config
+  },
 }
