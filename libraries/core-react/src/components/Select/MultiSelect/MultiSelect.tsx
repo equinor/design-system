5ecbd137c802c9ed2ee1c9752eb309553386cc8f
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-12-08 11:04:59 +0100

    🚨 Remove old eslint stuff

--- libraries/core-react/src/components/Select/MultiSelect/MultiSelect.tsx
+++ libraries/core-react/src/components/Select/MultiSelect/MultiSelect.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import * as React from 'react'
 import { forwardRef, SelectHTMLAttributes, useState } from 'react'
 import {
