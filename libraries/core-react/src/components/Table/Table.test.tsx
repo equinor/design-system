269c251b0ac8bbcfe4679e9734b1c52dcf91a390
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2020-12-15 08:48:51 +0100

    Tests for table component (#971)
    * ✅ Test for numeric and header cell
    
    * ✅ Add more tests
    
    Quite difficult to avoid implementation details
    
    * ✅ Lot's of issues to use tokens in test
    
    Issues with this white space in the token value, seems like prettier is adding a whitespace. Just trimmed it...
    
    * ✅ Use tokens for background color. Add border bottom as expect

--- libraries/core-react/src/components/Table/Table.test.tsx
+++ libraries/core-react/src/components/Table/Table.test.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-undef */
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import { Table } from '.'
+import styled from 'styled-components'
+import { token } from './Cell/DataCell.tokens'
 
-const { Caption } = Table
+const { Caption, Cell, Head, Row, Body } = Table
 
 afterEach(cleanup)
 
@@ -17,6 +19,19 @@ describe('Caption', () => {
       </Table>,
     )
     expect(container.querySelector('Caption')).toBeInTheDocument()
+  })
+  it('Prints the caption', () => {
+    const text = "I'm da caption"
+    const { getByText } = render(
+      <Table>
+        <Head>
+          <Row>
+            <Cell as="th">{text}</Cell>
+          </Row>
+        </Head>
+      </Table>,
+    )
+    expect(getByText(text)).toBeDefined()
   })
   it('Renders a table with caption, and caption-side set default to top', () => {
     const { container } = render(
@@ -39,5 +54,84 @@ describe('Caption', () => {
       'caption-side',
       'bottom',
     )
+  })
+})
+
+describe('Table', () => {
+  it('Can render a cell as a header cell', () => {
+    const text = 'Name'
+    const { getByText, container } = render(
+      <Table>
+        <Head>
+          <Row>
+            <Cell as="th">{text}</Cell>
+          </Row>
+        </Head>
+      </Table>,
+    )
+
+    const headerCell = getByText(text)
+    const th = container.querySelector('th')
+    expect(headerCell).toEqual(th)
+  })
+
+  it('Renders the header row visually different than the other rows by using a background colour', () => {
+    const header = 'Header'
+    const body = 'Body content'
+    const { getByText } = render(
+      <Table>
+        <Head>
+          <Row>
+            <Cell as="th">{header}</Cell>
+          </Row>
+        </Head>
+        <Body>
+          <Row>
+            <Cell>{body}</Cell>
+          </Row>
+        </Body>
+      </Table>,
+    )
+    const headElement = getByText(header).closest('thead')
+    const regularContent = getByText(body)
+    const cellBackground = token.background.replace(/ /g, '')
+
+    expect(headElement).not.toHaveStyleRule('background', cellBackground)
+    expect(headElement).toHaveStyleRule(
+      'border-bottom',
+      '2px solid rgba(220,220,220,1)',
+    )
+    expect(regularContent).toHaveStyleRule('background', cellBackground)
+  })
+
+  it('Adjusts font for better readability if the text is a number', () => {
+    const text = '369470'
+    const { getByText } = render(
+      <Table>
+        <Body>
+          <Row>
+            <Cell variant="numeric">{text}</Cell>
+          </Row>
+        </Body>
+      </Table>,
+    )
+    const typography = {
+      ...token.typography,
+      ...token.variants.numeric.typography,
+    }
+    const trimmedFontFeature = typography.fontFeature.replace(/\s*,\s*/g, ',')
+    expect(getByText(text)).toHaveStyleRule(
+      'font-feature-settings',
+      trimmedFontFeature,
+    )
+  })
+
+  const StyledTable = styled(Table)`
+    clip-path: unset;
+  `
+
+  it('Can extend the css for the component', () => {
+    const { container } = render(<StyledTable />)
+    expect(container.firstChild).toHaveStyleRule('clip-path', 'unset')
   })
 })
