712ecdca5371303cc153055b0b2f7e9a5f1df8fe
Victor Nystad <vnys@equinor.com>
2021-01-24 16:36:18 +0100

    add addon-a11y

--- libraries/core-react/.storybook/main.js
+++ libraries/core-react/.storybook/main.js
@@ -6,7 +6,11 @@ module.exports = {
     '../stories/components/**/*.stories.@(ts|tsx|mdx)',
     // '../stories/screens/**/*.@(ts|tsx|mdx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
   webpackFinal: async (config) => {
     config.resolve.alias['@components'] = path.resolve(__dirname, '../src')
     config.resolve.alias['@hooks'] = path.resolve(__dirname, '../src/hooks')
