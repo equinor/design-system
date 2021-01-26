71ab7a3556fd202a1df845e0d3b35b39e277b222
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-01-26 14:45:33 +0100

    🚚 Rename list sub component

--- libraries/core-react/src/components/List/index.ts
+++ libraries/core-react/src/components/List/index.ts
@@ -2,12 +2,17 @@ import { List as BaseComponent, ListProps } from './List'
 import { ListItem } from './ListItem'
 
 type ListCompoundProps = typeof BaseComponent & {
+  // Deprectated
   ListItem: typeof ListItem
+  // New
+  Item: typeof ListItem
 }
 
 const List = BaseComponent as ListCompoundProps
-
+// Deprecated
 List.ListItem = ListItem
+// New
+List.Item = ListItem
 
 export { List }
 export type { ListProps }
