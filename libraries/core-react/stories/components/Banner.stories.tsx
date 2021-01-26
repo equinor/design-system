0cfb4f158da3c32b6225fa867b6f80e4b4a698af
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-01-26 13:23:30 +0100

    🚚 Rename Banner sub components

--- libraries/core-react/stories/components/Banner.stories.tsx
+++ libraries/core-react/stories/components/Banner.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Meta, Story } from '@storybook/react'
-import { Banner, Icon, Button, BannerProps } from '@components'
+import { Banner, Icon as EdsIcon, Button, BannerProps } from '@components'
 import { save, thumbs_up, thumbs_down, mood_sad } from '@equinor/eds-icons'
 
 const icons = {
@@ -11,43 +11,51 @@ const icons = {
   mood_sad,
 }
 
-const { BannerIcon, BannerMessage, BannerActions } = Banner
-Icon.add(icons)
+const { Icon, Message, Actions } = Banner
+EdsIcon.add(icons)
 
-const InlineBannerActions = styled(BannerActions)`
+const Wrapper = styled(Actions)`
   display: flex;
 `
 
 export default {
   title: 'Components/Banner',
   component: Banner,
-  subcomponents: { BannerMessage, BannerIcon, BannerActions },
+  subcomponents: { Message, Icon, Actions },
+  parameters: {
+    docs: {
+      description: {
+        component: `Banners display important notifications and related optional actions.
+        `,
+      },
+    },
+  },
 } as Meta
 
 export const Basic: Story<BannerProps> = (args) => (
   <Banner {...args}>
-    <BannerMessage>
+    <Banner.Message>
       This tag is not being preserved yet. Click start preservation to enable
       writing preservation records.
-    </BannerMessage>
+    </Banner.Message>
   </Banner>
 )
 
 export const IconAndText: Story<BannerProps> = () => (
   <>
     <Banner>
-      <BannerIcon>
-        <Icon name="thumbs_up" />
-      </BannerIcon>
-      <BannerMessage>
+      <Banner.Icon>
+        <EdsIcon name="thumbs_up" />
+      </Banner.Icon>
+      <Banner.Message>
         We are in the making of a new design for this page.
-      </BannerMessage>
+      </Banner.Message>
     </Banner>
     <Banner>
-      <BannerIcon variant="warning">
-        <Icon name="thumbs_down" />
-      </BannerIcon>
-      <BannerMessage>Some warning information.</BannerMessage>
+      <Banner.Icon variant="warning">
+        <EdsIcon name="thumbs_down" />
+      </Banner.Icon>
+      <Banner.Message>Some warning information.</Banner.Message>
     </Banner>
   </>
 )
@@ -55,29 +63,29 @@ export const IconAndText: Story<BannerProps> = () => (
 export const TextAndAction: Story<BannerProps> = () => (
   <>
     <Banner>
-      <BannerMessage>
+      <Banner.Message>
         You are signed on with another account than Equinor account:
         name.lastname@mail.com.
-      </BannerMessage>
-      <InlineBannerActions>
+      </Banner.Message>
+      <Wrapper>
         <Button variant="ghost" style={{ marginRight: '1rem' }}>
           Sign out
         </Button>
         <Button variant="ghost">OK</Button>
-      </InlineBannerActions>
+      </Wrapper>
     </Banner>
     <Banner>
-      <BannerMessage>
+      <Banner.Message>
         You are signed on with another account than Equinor account:
         name.lastname@mail.com. This means you have to do something to be able
         to use this service.
-      </BannerMessage>
-      <InlineBannerActions placement="bottom">
+      </Banner.Message>
+      <Wrapper placement="bottom">
         <Button variant="ghost" style={{ marginRight: '1rem' }}>
           Sign out
         </Button>
         <Button variant="ghost">OK</Button>
-      </InlineBannerActions>
+      </Wrapper>
     </Banner>
   </>
 )
@@ -85,31 +93,31 @@ export const TextAndAction: Story<BannerProps> = () => (
 export const IconAndTextAndActions: Story<BannerProps> = () => (
   <>
     <Banner>
-      <BannerIcon variant="warning">
-        <Icon name="mood_sad" />
-      </BannerIcon>
-      <BannerMessage>
+      <Banner.Icon variant="warning">
+        <EdsIcon name="mood_sad" />
+      </Banner.Icon>
+      <Banner.Message>
         This tag is not being preserved yet. Click start preservation to enable
         writing preservation records.
-      </BannerMessage>
-      <BannerActions>
+      </Banner.Message>
+      <Banner.Actions>
         <Button>Action</Button>
-      </BannerActions>
+      </Banner.Actions>
     </Banner>
     <Banner>
-      <BannerIcon>
-        <Icon name="save" />
-      </BannerIcon>
-      <BannerMessage>
+      <Banner.Icon>
+        <EdsIcon name="save" />
+      </Banner.Icon>
+      <Banner.Message>
         I&apos;m such a really really long message about some sad saving news
         that there is not enough space for the actions on my left. That&apos;s
         why the actions have been located at the bottom using the placement prop
         instead.
-      </BannerMessage>
-      <InlineBannerActions placement="bottom">
+      </Banner.Message>
+      <Wrapper placement="bottom">
         <Button style={{ marginRight: '1rem' }}>First action</Button>
         <Button color="secondary">Second action</Button>
-      </InlineBannerActions>
+      </Wrapper>
     </Banner>
   </>
 )
