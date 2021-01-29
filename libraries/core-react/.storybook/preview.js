b8b11caa5f84c24fd737167d584898761f9df7cc
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-01-29 10:40:46 +0100

    ➕ Experiments with info addon for Zeroheight (#1091)

--- libraries/core-react/.storybook/preview.js
+++ libraries/core-react/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+
+addDecorator(withInfo)
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewMode: 'docs',
--- libraries/core-react/stories/components/Button.stories.tsx
+++ libraries/core-react/stories/components/Button.stories.tsx
@@ -40,6 +40,7 @@ export default {
         `,
       },
     },
+    info: {},
   },
 } as Meta
 
