ec8eef9c6be3b380c4f99a0e126c6f5d41ecc995
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-09 15:34:22 +0100

    ✅ Add some more tests

--- libraries/core-react/src/components/Label/Label.test.tsx
+++ libraries/core-react/src/components/Label/Label.test.tsx
@@ -10,10 +10,27 @@ afterEach(cleanup)
 describe('Label', () => {
   it('Has correct label text', () => {
     const labelText = 'Some label'
-    const { queryByText } = render(
-      <Label htmlFor="test-label" label={labelText} />,
-    )
+    const { queryByText } = render(<Label label={labelText} />)
 
     expect(queryByText(labelText)).toBeInTheDocument()
+  })
+
+  it('Can add a meta text', () => {
+    const labelText = 'Some label'
+    const metaText = 'Meta'
+    const { queryByText } = render(<Label label={labelText} meta={metaText} />)
+
+    expect(queryByText(metaText)).toBeInTheDocument()
+  })
+
+  const StyledLabel = styled(Label)`
+    margin-top: 48px;
+  `
+  it('Can extend the css of the component', () => {
+    const labelText = 'Some label'
+
+    const { container } = render(<StyledLabel label={labelText} />)
+
+    expect(container.firstChild).toHaveStyleRule('margin-top', '48px')
   })
 })
