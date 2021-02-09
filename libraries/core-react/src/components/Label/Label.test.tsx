47f5864c132e354374170700eb80ffb3df881222
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-09 14:46:58 +0100

    ✅ Start writing tests for Label

---
+++ libraries/core-react/src/components/Label/Label.test.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+import * as React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import 'jest-styled-components'
+import styled from 'styled-components'
+import { Label } from './Label'
+
+afterEach(cleanup)
+
+describe('Label', () => {
+  it('Has correct label text', () => {
+    const labelText = 'Some label'
+    const { queryByText } = render(
+      <Label inputId="test-label" label={labelText} />,
+    )
+
+    expect(queryByText(labelText)).toBeInTheDocument()
+  })
+})
