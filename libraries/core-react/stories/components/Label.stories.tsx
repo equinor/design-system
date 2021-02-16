c85526f0da39c4b79475b5c077974b344fa11f8a
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-16 15:29:42 +0100

    📝 Add some stories for Input

--- libraries/core-react/stories/components/Label.stories.tsx
+++ libraries/core-react/stories/components/Label.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-import { Label, LabelProps } from '@components'
+import { Label, LabelProps, Input } from '@components'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 export default {
@@ -11,8 +10,6 @@ export default {
       description: {
         component: `The **Label** component is intended to use if you need to more flexibility than the wrapped <code>TextField</code> or
         select components give.<br/>
-        ♿  Note on accessibility:<br/>
-        It's important to link the <code>Label</code> to the corresponding input element. Use the React version of the html for attribute <code>htmlFor</code>
         `,
       },
     },
@@ -36,12 +33,20 @@ export const Disabled: Story<LabelProps> = () => (
   />
 )
 
-export const Accessible: Story<LabelProps> = () => {
-  // To wrap the input component is not yet supported
+export const Accessiblity: Story<LabelProps> = () => {
+  // To wrap the input component inside the label element is not yet supported
   return (
     <>
       <Label label="I use the htmlFor prop" htmlFor="speed" />
-      <input type="text" id="speed" />
+      <Input type="text" id="speed" />
     </>
   )
+}
+Accessiblity.parameters = {
+  docs: {
+    // The story now contains a description
+    storyDescription: `It's important to link the <code>Label</code> to the corresponding input element. Use the React version of the html for attribute <code>htmlFor</code>
+    as <code>Label</code> does not support to wrap the input field.
+    `,
+  },
 }
