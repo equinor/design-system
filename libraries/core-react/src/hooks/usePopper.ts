6627b96fde7d14eb1dd03a4a9303848f254af07a
Frida Erdal <frida.erdal@olavstoppen.no>
2021-02-16 15:22:55 +0100

    ♻️ usePopper hook for reusability

---
+++ libraries/core-react/src/hooks/usePopper.ts
@@ -0,0 +1,56 @@
+import { MutableRefObject } from 'react'
+// import * as PopperJS from '@popperjs/core'
+import { usePopper as reactPopper } from 'react-popper'
+
+export const usePopper = (
+  anchorRef: MutableRefObject<HTMLElement>,
+  popperRef: MutableRefObject<HTMLElement>,
+  arrowRef: HTMLElement | string,
+  placement:
+    | 'auto'
+    | 'auto-start'
+    | 'auto-end'
+    | 'top'
+    | 'top-start'
+    | 'top-end'
+    | 'bottom'
+    | 'bottom-start'
+    | 'bottom-end'
+    | 'right'
+    | 'right-start'
+    | 'right-end'
+    | 'left'
+    | 'left-start'
+    | 'left-end',
+): {
+  styles: { [key: string]: any }
+  attributes: { [key: string]: { [key: string]: string } }
+  //   state: PopperJS.State | null
+  //   update: PopperJS.Instance['update'] | null
+  //   forceUpdate: PopperJS.Instance['forceUpdate'] | null
+} => {
+  const { styles, attributes } = reactPopper(
+    anchorRef.current,
+    popperRef.current,
+
+    {
+      placement,
+      modifiers: [
+        {
+          name: 'arrow',
+          options: {
+            element: arrowRef,
+          },
+        },
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 10],
+          },
+        },
+      ],
+    },
+  )
+
+  return { styles, attributes }
+}
