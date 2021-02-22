8bdc2319b7423f3a9218657e6fa7baaa55d68d78
Frida Erdal <frida.erdal@olavstoppen.no>
2021-02-22 14:33:05 +0100

    🗑 Popover story cleanup

--- libraries/core-react/stories/components/Popover.stories.tsx
+++ libraries/core-react/stories/components/Popover.stories.tsx
@@ -32,294 +32,7 @@ export default {
   },
 } as Meta
 
-// TODO: Clean up after popper JS spike
-// export const Default: Story<PopoverProps> = (args) => {
-//   const [active, setActive] = React.useState(false)
-//   const handleToggle = () => {
-//     setActive(!active)
-//   }
-//   return (
-//     <div style={{ margin: '10em' }}>
-//       <Popover {...args} onClose={handleToggle} open={active}>
-//         <PopoverAnchor>
-//           <Button id="1" onClick={handleToggle}>
-//             Click me!
-//           </Button>
-//         </PopoverAnchor>
-//         <PopoverTitle>Title</PopoverTitle>
-//         <PopoverContent>
-//           <Typography variant="body_short">Content</Typography>
-//         </PopoverContent>
-//         <CardActions>
-//           <Button onClick={handleToggle}>Cancel</Button>
-//           <Button onClick={handleToggle}>OK</Button>
-//         </CardActions>
-//       </Popover>
-//     </div>
-//   )
-// }
-
-// export const Placements: Story<PopoverProps> = () => {
-//   const [active, setActive] = React.useState('')
-
-//   const handleClick = (event: React.SyntheticEvent) => {
-//     setActive(event.currentTarget.id)
-//   }
-
-//   const handleClose = () => {
-//     setActive('')
-//   }
-
-//   const Content = () => (
-//     <>
-//       <PopoverTitle>Title </PopoverTitle>
-//       <PopoverContent>
-//         <Typography variant="body_short">Content</Typography>
-//       </PopoverContent>
-//       <CardActions>
-//         <Button onClick={handleClose}>Cancel</Button>
-//         <Button onClick={handleClose}>OK</Button>
-//       </CardActions>
-//     </>
-//   )
-
-//   return (
-//     <Body>
-//       <TextWrapper>
-//         <Typography variant="h3">Placement</Typography>
-//         <Typography variant="body_long">
-//           Popover has 12 placement choices. The placement is relative to the
-//           anchor element. Remember, you should always put the anchor element
-//           inside the PopoverAnchor tag
-//         </Typography>
-//       </TextWrapper>
-//       <Typography variant="h5" as="h4">
-//         Top
-//       </Typography>
-//       <Wrapper>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '1'}
-//           placement="topLeft"
-//         >
-//           <PopoverAnchor>
-//             <Button id="1" onClick={handleClick}>
-//               Top left
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover onClose={handleClose} open={active === '2'} placement="top">
-//           <PopoverAnchor>
-//             <Button id="2" onClick={handleClick}>
-//               Top
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '3'}
-//           placement="topRight"
-//         >
-//           <PopoverAnchor>
-//             <Button id="3" onClick={handleClick}>
-//               Top right
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//       </Wrapper>
-
-//       <Typography variant="h5" as="h4">
-//         Bottom
-//       </Typography>
-//       <Wrapper>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '4'}
-//           placement="bottomLeft"
-//         >
-//           <PopoverAnchor>
-//             <Button id="4" onClick={handleClick}>
-//               Bottom left
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover onClose={handleClose} open={active === '5'} placement="bottom">
-//           <PopoverAnchor>
-//             <Button id="5" onClick={handleClick}>
-//               Bottom
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '6'}
-//           placement="bottomRight"
-//         >
-//           <PopoverAnchor>
-//             <Button id="6" onClick={handleClick}>
-//               Bottom right
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//       </Wrapper>
-
-//       <Typography variant="h5" as="h4">
-//         Left
-//       </Typography>
-//       <Wrapper>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '7'}
-//           placement="leftTop"
-//         >
-//           <PopoverAnchor>
-//             <Button id="7" onClick={handleClick}>
-//               Left top
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover onClose={handleClose} open={active === '8'} placement="left">
-//           <PopoverAnchor>
-//             <Button id="8" onClick={handleClick}>
-//               Left
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '9'}
-//           placement="leftBottom"
-//         >
-//           <PopoverAnchor>
-//             <Button id="9" onClick={handleClick}>
-//               Left bottom
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//       </Wrapper>
-//       <Typography variant="h5" as="h4">
-//         Right
-//       </Typography>
-//       <Wrapper>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '10'}
-//           placement="rightTop"
-//         >
-//           <PopoverAnchor>
-//             <Button id="10" onClick={handleClick}>
-//               Right top
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover onClose={handleClose} open={active === '11'} placement="right">
-//           <PopoverAnchor>
-//             <Button id="11" onClick={handleClick}>
-//               Right
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//         <Popover
-//           onClose={handleClose}
-//           open={active === '12'}
-//           placement="rightBottom"
-//         >
-//           <PopoverAnchor>
-//             <Button id="12" onClick={handleClick}>
-//               Right bottom
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//       </Wrapper>
-//     </Body>
-//   )
-// }
-
-// export const ActivationTypes: Story<PopoverProps> = () => {
-//   const [active, setActive] = React.useState('')
-
-//   const handleClick = (event: React.SyntheticEvent) => {
-//     setActive(event.currentTarget.id)
-//   }
-
-//   const handleHover = (event: React.SyntheticEvent) => {
-//     const current = event.currentTarget.id
-//     setTimeout(() => {
-//       setActive(current)
-//     }, 300)
-//   }
-
-//   const handleClose = () => {
-//     setActive('')
-//   }
-
-//   const Content = () => (
-//     <>
-//       <PopoverTitle>Title </PopoverTitle>
-//       <PopoverContent>
-//         <Typography variant="body_short">Content</Typography>
-//       </PopoverContent>
-//       <CardActions>
-//         <Button onClick={handleClose}>Cancel</Button>
-//         <Button onClick={handleClose}>OK</Button>
-//       </CardActions>
-//     </>
-//   )
-
-//   return (
-//     <Body>
-//       <TextWrapper>
-//         <Typography variant="h3">Activation types</Typography>
-//         <Typography variant="body_long">
-//           Popovers can be activated by hover or click. To dismiss a popover, use
-//           the close icon, press the ESC key, open another popover or click
-//           outside the popover. If there are no actions in the popover, then the
-//           close icon should be the first focusable element.
-//         </Typography>
-//       </TextWrapper>
-//       <Wrapper>
-//         <Popover onClose={handleClose} open={active === '1'}>
-//           <PopoverAnchor>
-//             <Button id="1" onClick={handleClick}>
-//               On Click
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//       </Wrapper>
-//       <Typography variant="body_long">
-//         Remember to use both onMouseEnter and onFocus attributes to your trigger
-//         element to be able to open the popover by tab iterations. A timeout
-//         delay on onMouseEnter is recommended to avoid unwanted trigger of the
-//         popup while browsing.
-//       </Typography>
-//       <Wrapper>
-//         <Popover onClose={handleClose} open={active === '2'}>
-//           <PopoverAnchor>
-//             <Button id="2" onMouseEnter={handleHover} onFocus={handleClick}>
-//               On Hover
-//             </Button>
-//           </PopoverAnchor>
-//           <Content />
-//         </Popover>
-//       </Wrapper>
-//     </Body>
-//   )
-// }
-
-export const WithPopper: Story<PopoverProps> = (args) => {
+export const Default: Story<PopoverProps> = (args) => {
   const [openState, setOpenState] = useState(false)
 
   const handleToggle = () => {
@@ -329,7 +42,7 @@ export const WithPopper: Story<PopoverProps> = (args) => {
   const referenceElement = useRef(null)
 
   return (
-    <div style={{ position: 'relative', margin: '10em' }}>
+    <div style={{ margin: '10em' }}>
       <Button ref={referenceElement} onClick={handleToggle}>
         Click me!
       </Button>
@@ -346,5 +59,82 @@ export const WithPopper: Story<PopoverProps> = (args) => {
         </Popover>
       )}
     </div>
+  )
+}
+
+export const ActivationTypes: Story<PopoverProps> = () => {
+  const [active, setActive] = React.useState(null)
+
+  const handleClick = (num: 1 | 2) => {
+    setActive(num)
+  }
+
+  const handleHover = (num: 1 | 2) => {
+    setTimeout(() => {
+      setActive(num)
+    }, 300)
+  }
+
+  const handleClose = () => {
+    setActive(null)
+  }
+
+  const Content = () => (
+    <>
+      <PopoverTitle>Title </PopoverTitle>
+      <PopoverContent>
+        <Typography variant="body_short">Content</Typography>
+      </PopoverContent>
+      <CardActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>OK</Button>
+      </CardActions>
+    </>
+  )
+  const refOne = useRef(null)
+  const refTwo = useRef(null)
+
+  return (
+    <Body>
+      <TextWrapper>
+        <Typography variant="h3">Activation types</Typography>
+        <Typography variant="body_long">
+          Popovers can be activated by hover or click. To dismiss a popover, use
+          the close icon, press the ESC key, open another popover or click
+          outside the popover. If there are no actions in the popover, then the
+          close icon should be the first focusable element.
+        </Typography>
+      </TextWrapper>
+      <Wrapper>
+        <Button ref={refOne} onClick={() => handleClick(1)}>
+          Activate on click
+        </Button>
+        {active === 1 && (
+          <Popover anchorEl={refOne} onClose={handleClose}>
+            <Content />
+          </Popover>
+        )}
+      </Wrapper>
+      <Typography variant="body_long">
+        Remember to use both onMouseEnter and onFocus attributes to your trigger
+        element to be able to open the popover by tab iterations. A timeout
+        delay on onMouseEnter is recommended to avoid unwanted trigger of the
+        popup while browsing.
+      </Typography>
+      <Wrapper>
+        <Button
+          ref={refTwo}
+          onMouseEnter={() => handleHover(2)}
+          onFocus={() => handleClick(2)}
+        >
+          On Hover
+        </Button>
+        {active === 2 && (
+          <Popover anchorEl={refTwo} onClose={handleClose}>
+            <Content />
+          </Popover>
+        )}
+      </Wrapper>
+    </Body>
   )
 }
