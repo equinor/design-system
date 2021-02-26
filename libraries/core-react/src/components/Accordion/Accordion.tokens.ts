ba9131c67bf028f4577e9be105a8a325183d9e9f
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-26 14:04:22 +0100

    ♻️ Remove CSS vars in Accordion tokens file (initial, deprecated approach)

--- libraries/core-react/src/components/Accordion/Accordion.tokens.ts
+++ libraries/core-react/src/components/Accordion/Accordion.tokens.ts
@@ -1,5 +1,5 @@
 import { tokens } from '@equinor/eds-tokens'
-import type { Typography } from '@equinor/eds-tokens'
+import type { Typography, Border } from '@equinor/eds-tokens'
 
 const {
   typography: {
@@ -41,32 +41,32 @@ const token = {
   header: {
     typography,
     color: {
-      default: `var(--eds_text_static_icons__default, ${typography.color})`,
-      disabled: `var(--eds_interactive_disabled__text, ${disabledColor})`,
-      activated: `var(--eds_interactive_primary__resting, ${activatedColor})`,
+      default: typography.color,
+      disabled: disabledColor,
+      activated: activatedColor,
     },
     background: {
-      default: `var(--eds_ui_background__default, ${backgroundDefault})`,
+      default: backgroundDefault,
       /* Mismatch i Figma!!!! --eds_ui_background__light */
-      hover: `var(--eds_ui_background__raised, ${backgroundHover})`,
+      hover: backgroundHover,
     },
   },
   /* Mismatch i Figma!! 
-  border: `1px solid var(--eds_ui_background__medium, ${borderColor})`,
+  border: `1px solid var(--eds_ui_background__medium, ${borderColor,
   */
-  border: `1px solid var(--eds_ui_background__lighten, ${borderColor})`,
-  outline: `1px dashed var(--eds_interactive_focus, ${focusOutlineColor})`,
+  border: borderColor,
+  outline: focusOutlineColor,
   outlineOffset: '2px',
   chevronColor: {
-    default: `var(--eds_interactive_primary__resting, ${activatedColor})`,
-    disabled: `var(--eds_interactive_disabled__fill, ${disabledIconColor})`,
+    default: activatedColor,
+    disabled: disabledIconColor,
   },
   iconColor: {
     interactive: {
-      color: `var(--eds_interactive_primary__resting, ${activatedColor})`,
+      color: activatedColor,
     },
     static: {
-      color: `var(--eds_text_static_icons__default, ${focusHoverColor})`,
+      color: focusHoverColor,
     },
   },
 }
