f6fc6b2677fdc0e7e3b7fb8ca5a1cf93d7e409b1
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-12 13:46:45 +0100

    💄 Add a tiny top margin to the unit

--- libraries/core-react/src/components/TextField/InputWrapper.tsx
+++ libraries/core-react/src/components/TextField/InputWrapper.tsx
@@ -61,7 +61,11 @@ export const InputWithAdornments = styled.div<InputWithAdornmentsType>`
 `
 
 const Unit = styled.span`
-  ${typographyTemplate(textfield.entities.unit.typography)}
+  ${typographyTemplate(textfield.entities.unit.typography)};
+  /*   Yes, we don't like magic numbers, but if you have both unit and icon, 
+  the unit is slightly off due to line-height and font */
+  display: inline-block;
+  margin-top: 3px;
 `
 
 type AdornmentsType = {
