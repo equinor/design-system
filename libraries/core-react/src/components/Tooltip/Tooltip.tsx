83b6124253a20ae3a6c4b10649fd02f95e08dc98
mimarz <mimarz@gmail.com>
2021-04-08 13:05:42 +0200

    🔥 removed tooltip arrow shadow

--- libraries/core-react/src/components/Tooltip/Tooltip.tsx
+++ libraries/core-react/src/components/Tooltip/Tooltip.tsx
@@ -77,7 +77,6 @@ const TooltipArrow = styled.svg<ArrowProps>`
   height: ${tokens.arrow.height};
   position: absolute;
   fill: ${tokens.background};
-  filter: drop-shadow(-4px 0px 2px rgba(0, 0, 0, 0.2));
 `
 
 export type TooltipProps = {
