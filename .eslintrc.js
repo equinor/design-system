9ce7e4e9fb0b736cdc6be9a8c2163f8486cc9a9a
mimarz <mimarz@gmail.com>
2021-04-09 15:01:00 +0200

    🚨 its the start of something

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:testing-library/react',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['prettier', 'testing-library'],
-  parser: '@babel/eslint-parser',
+  plugins: ['testing-library', 'jsx-a11y', 'react', 'import', 'prettier'],
+  globals: {
+    test: true,
+    expect: true,
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
@@ -18,21 +28,25 @@ module.exports = {
       jsx: true,
     },
     requireConfigFile: false,
+    project: './tsconfig.eslint.json',
   },
-  env: {
-    browser: true,
-    node: true,
-  },
+
   settings: {
     react: {
-      version: '^17.0.1',
+      version: 'detect', // uses latest when "detect"
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: ['libraries/*/tsconfig.json'],
+      },
     },
   },
-  globals: {
-    test: true,
-    expect: true,
-  },
   rules: {
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'error',
     'prettier/prettier': ['error'],
     'no-unused-vars': [
       'error',
@@ -45,7 +59,6 @@ module.exports = {
     'import/prefer-default-export': ['off'],
     'no-underscore-dangle': ['off'],
     'no-use-before-define': ['off'],
-    'import/no-unresolved': ['off'],
     'react/require-default-props': [
       'error',
       {
@@ -90,41 +103,41 @@ module.exports = {
       },
     ],
   },
-  overrides: [
-    {
-      files: ['**/*.jsx'],
-    },
-    {
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.eslint.json'],
-        ecmaVersion: 2020,
-      },
-      plugins: ['@typescript-eslint'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      files: ['**/*.ts', '**/*.tsx'],
-      rules: {
-        'react/prop-types': 'off',
-        'react/jsx-filename-extension': [
-          'error',
-          {
-            extensions: ['.jsx', '.tsx'],
-          },
-        ],
-        /* set rules to warn */
-        '@typescript-eslint/no-explicit-any': 1,
-        '@typescript-eslint/no-unsafe-assignment': 1,
-        '@typescript-eslint/no-unsafe-member-access': 1,
-        '@typescript-eslint/no-unsafe-call': 1,
-        '@typescript-eslint/no-unsafe-return': 1,
-        '@typescript-eslint/restrict-plus-operands': 1,
-        '@typescript-eslint/ban-ts-comment': 1,
-        'react/require-default-props': 1,
-      },
-    },
-  ],
+  // overrides: [
+  //   {
+  //     files: ['**/*.jsx'],
+  //   },
+  //   {
+  //     parser: '@typescript-eslint/parser',
+  //     parserOptions: {
+  //       tsconfigRootDir: __dirname,
+  //       project: ['./tsconfig.eslint.json'],
+  //       ecmaVersion: 2020,
+  //     },
+  //     plugins: ['@typescript-eslint'],
+  //     extends: [
+  //       'plugin:@typescript-eslint/recommended',
+  //       'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  //     ],
+  //     files: ['**/*.ts', '**/*.tsx'],
+  //     rules: {
+  //       'react/prop-types': 'off',
+  //       'react/jsx-filename-extension': [
+  //         'error',
+  //         {
+  //           extensions: ['.jsx', '.tsx'],
+  //         },
+  //       ],
+  //       /* set rules to warn */
+  //       '@typescript-eslint/no-explicit-any': 1,
+  //       '@typescript-eslint/no-unsafe-assignment': 1,
+  //       '@typescript-eslint/no-unsafe-member-access': 1,
+  //       '@typescript-eslint/no-unsafe-call': 1,
+  //       '@typescript-eslint/no-unsafe-return': 1,
+  //       '@typescript-eslint/restrict-plus-operands': 1,
+  //       '@typescript-eslint/ban-ts-comment': 1,
+  //       'react/require-default-props': 1,
+  //     },
+  //   },
+  // ],
 }
