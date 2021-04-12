26c299091415fee0e6427e02348d27f226772271
mimarz <mimarz@gmail.com>
2021-04-12 10:55:01 +0200

    somewhat working setup, missing some rules and js stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,28 +7,40 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:testing-library/react',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/errors',
     'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
-  plugins: ['testing-library', 'jsx-a11y', 'react', 'import', 'prettier'],
+  plugins: [
+    'testing-library',
+    'react',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'import',
+    'prettier',
+  ],
   globals: {
     test: true,
     expect: true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
-    requireConfigFile: false,
-    project: './tsconfig.eslint.json',
+    ecmaVersion: 11,
+    sourceType: 'module',
+    project: [
+      './tsconfig.eslint.json',
+      './libraries/*/tsconfig.json',
+      './libraries/*/*/tsconfig.json',
+    ],
+    tsconfigRootDir: __dirname,
   },
 
   settings: {
@@ -40,25 +52,31 @@ module.exports = {
     },
     'import/resolver': {
       typescript: {
-        project: ['libraries/*/tsconfig.json'],
+        project: [
+          './libraries/*/tsconfig.json',
+          './libraries/*/*/tsconfig.json',
+        ],
       },
     },
   },
   rules: {
-    'react/prop-types': 'off',
-    'import/no-unresolved': 'error',
     'prettier/prettier': ['error'],
-    'no-unused-vars': [
-      'error',
-      {
-        varsIgnorePattern: '^React$',
-      },
-    ],
+    'no-underscore-dangle': ['off'],
+    'no-use-before-define': ['off'],
+    // 'no-unused-vars': [
+    //   'error',
+    //   {
+    //     varsIgnorePattern: '^React$',
+    //   },
+    // ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'import/no-unresolved': 'error',
+    'import/default': 'off',
     'import/no-default-export': ['error'],
     'import/newline-after-import': ['error'],
     'import/prefer-default-export': ['off'],
-    'no-underscore-dangle': ['off'],
-    'no-use-before-define': ['off'],
+    'react/prop-types': 'off',
     'react/require-default-props': [
       'error',
       {
@@ -80,12 +98,13 @@ module.exports = {
         ignorePureComponents: true,
       },
     ],
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.jsx'],
-      },
-    ],
+    // '@typescript-eslint/no-explicit-any': 1,
+    // '@typescript-eslint/no-unsafe-assignment': 1,
+    // '@typescript-eslint/no-unsafe-member-access': 1,
+    // '@typescript-eslint/no-unsafe-call': 1,
+    // '@typescript-eslint/no-unsafe-return': 1,
+    // '@typescript-eslint/restrict-plus-operands': 1,
+    // '@typescript-eslint/ban-ts-comment': 1,
     'react/no-unused-prop-types': ['error'],
     'react/no-array-index-key': ['error'],
     'react/no-typos': ['error'],
@@ -103,6 +122,15 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/*.test.ts*'],
+      extends: ['plugin:testing-library/react'],
+    },
+    {
+      files: ['**/*.jsx'],
+    },
+  ],
   // overrides: [
   //   {
   //     files: ['**/*.jsx'],
