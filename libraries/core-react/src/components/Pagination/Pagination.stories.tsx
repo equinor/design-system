cb16833e4568fd51afd4263fc672d8429dd6e9e7
Michael Marszalek <mmarsz@equinor.com>
2021-04-21 10:50:28 +0200

    ♻️Updated Pagination tokens (#1285)
    * ♻️ Updated tokens
    
    * ♻️ Cleaned up story
    
    * 🔥 Removed unused variables

--- libraries/core-react/src/components/Pagination/Pagination.stories.tsx
+++ libraries/core-react/src/components/Pagination/Pagination.stories.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { Pagination, PaginationProps, Typography } from '../..'
-import styled from 'styled-components'
+import { Pagination, PaginationProps } from '../..'
 import { Story, Meta } from '@storybook/react'
-
-const PaddedTypography = styled(Typography)`
-  margin: 16px 0;
-`
 
 export default {
   title: 'Components/Pagination',
@@ -32,28 +27,14 @@ export default {
   },
 } as Meta
 
-export const Default: Story<PaginationProps> = (args) => {
-  return <Pagination {...args} />
-}
+export const Default: Story<PaginationProps> = (args) => (
+  <Pagination {...args} />
+)
 
-export const Variants: Story<PaginationProps> = () => {
-  return (
-    <>
-      <PaddedTypography variant="h2">Length before truncation</PaddedTypography>
-      <Pagination totalItems={7} itemsPerPage={1} />
-      <PaddedTypography variant="h2">Truncated</PaddedTypography>
-      <Pagination totalItems={8} itemsPerPage={1} />
-      <PaddedTypography variant="h2">
-        With current and total items indicator
-      </PaddedTypography>
-      <Pagination totalItems={140} itemsPerPage={3} withItemIndicator />
-      <PaddedTypography variant="h2">Default page</PaddedTypography>
-      <Pagination
-        totalItems={140}
-        itemsPerPage={3}
-        withItemIndicator
-        defaultPage={6}
-      />
-    </>
-  )
-}
+export const Truncated: Story<PaginationProps> = () => (
+  <Pagination totalItems={8} itemsPerPage={1} />
+)
+
+export const WithIndicator: Story<PaginationProps> = () => (
+  <Pagination totalItems={140} itemsPerPage={3} withItemIndicator />
+)
