f84879de8742117689fd449168249ae4fc889cf6
Frida Erdal <31915755+pomfrida@users.noreply.github.com>
2021-04-22 09:39:24 +0200

    ♻️ Updated List tokens (#1291)
    * ♻️ Updated tokens
    
    * 🔥 Removed unused Typography type

--- libraries/core-react/src/components/List/List.tokens.ts
+++ libraries/core-react/src/components/List/List.tokens.ts
@@ -1,13 +1,11 @@
 import { tokens } from '@equinor/eds-tokens'
-import type { Typography } from '@equinor/eds-tokens'
+import type { ComponentToken } from '@equinor/eds-tokens'
 
 const {
   typography: { paragraph },
 } = tokens
 
-type ListToken = {
-  typography: Typography
-}
+type ListToken = ComponentToken
 
 export const list: ListToken = {
   typography: paragraph.body_short,
