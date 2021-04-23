fa3b09f6f770e75886a9ef5c88a7a8190f1758d4
Frida Erdal <frida.erdal@olavstoppen.no>
2021-04-23 10:39:57 +0200

    ✅ Fixed tests

--- libraries/core-react/src/components/Divider/Divider.test.tsx
+++ libraries/core-react/src/components/Divider/Divider.test.tsx
@@ -4,21 +4,22 @@ import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import styled from 'styled-components'
-import { divider as tokens } from './Divider.tokens'
+import * as tokens from './Divider.tokens'
 import { Divider } from '.'
+import { trimSpaces } from '../../utils'
 
 const StyledDivider = styled(Divider)`
   position: relative;
 `
 const {
-  color: { lighter, light, medium },
-  entities: {
-    small: {
-      spacings: { top: spacingSmall },
-    },
-    medium: {
-      spacings: { top: spacingMedium },
-    },
+  divider: {
+    color: { lighter, light, mediumColor },
+  },
+  small: {
+    spacings: { top: spacingSmall },
+  },
+  medium: {
+    spacings: { top: spacingMedium },
   },
 } = tokens
 
@@ -27,16 +28,25 @@ afterEach(cleanup)
 describe('Divider', () => {
   it('Has medium colour and medium spacing as default', () => {
     const { container } = render(<Divider />)
-    expect(container.firstChild).toHaveStyleRule('background-color', medium)
+    expect(container.firstChild).toHaveStyleRule(
+      'background-color',
+      trimSpaces(mediumColor),
+    )
     expect(container.firstChild).toHaveStyleRule('margin-top', spacingMedium)
   })
   it('Sets backgroundColor to light according to color prop', () => {
     const { container } = render(<Divider color="light" />)
-    expect(container.firstChild).toHaveStyleRule('background-color', light)
+    expect(container.firstChild).toHaveStyleRule(
+      'background-color',
+      trimSpaces(light),
+    )
   })
   it('Sets backgroundColor to lighter according to color prop', () => {
     const { container } = render(<Divider color="lighter" />)
-    expect(container.firstChild).toHaveStyleRule('background-color', lighter)
+    expect(container.firstChild).toHaveStyleRule(
+      'background-color',
+      trimSpaces(lighter),
+    )
   })
   it('Sets topMargin to small according to variant prop', () => {
     const { container } = render(<Divider variant="small" />)
