73033789db69fa5cca9f355bfbd997484ef9ad5e
Frida Erdal <frida.erdal@olavstoppen.no>
2021-05-25 15:00:32 +0200

    ♻️ Destructure values

--- libraries/core-react/src/hooks/useAutoResize.ts
+++ libraries/core-react/src/hooks/useAutoResize.ts
@@ -9,21 +9,19 @@ export const useAutoResize = (
   useEffect(() => {
     const handleResize = () => {
       targetEl.style.height = 'auto'
-      let newHeight = targetEl.clientHeight
+      const { scrollHeight, clientHeight } = targetEl
+      let newHeight = clientHeight
 
       if (maxHeight > newHeight) {
-        newHeight = Math.min(
-          maxHeight,
-          Math.max(targetEl.scrollHeight, newHeight),
-        )
+        newHeight = Math.min(maxHeight, Math.max(scrollHeight, newHeight))
 
-        if (targetEl.scrollHeight > maxHeight) {
+        if (scrollHeight > maxHeight) {
           targetEl.style.overflow = 'auto'
         } else {
           targetEl.style.overflow = 'hidden'
         }
 
-        if (newHeight > targetEl.clientHeight) {
+        if (newHeight > clientHeight) {
           targetEl.style.height = `${newHeight}px`
         }
       }
