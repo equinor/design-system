b802c4007d2600b430c0ec1991f98ebb1c1aa470
Frida Erdal <frida.erdal@olavstoppen.no>
2021-05-25 11:47:59 +0200

    🔥 Remove some code

--- libraries/core-react/src/hooks/useAutoResize.ts
+++ libraries/core-react/src/hooks/useAutoResize.ts
@@ -10,14 +10,14 @@ export const useAutoResize = (
     const handleResize = () => {
       targetEl.style.height = 'auto'
       let newHeight = targetEl.clientHeight
-
       if (!maxHeight || maxHeight > newHeight) {
         newHeight = Math.max(targetEl.scrollHeight, newHeight)
         if (maxHeight) {
           newHeight = Math.min(maxHeight, newHeight)
           if (targetEl.scrollHeight > maxHeight) {
-            console.log(targetEl.scrollHeight, newHeight, maxHeight)
             targetEl.style.overflow = 'auto'
+          } else {
+            targetEl.style.overflow = 'hidden'
           }
         }
         if (newHeight > targetEl.clientHeight) {
