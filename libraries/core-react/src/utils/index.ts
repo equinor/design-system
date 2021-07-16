9ae1c550bbf76f9f7a1c2fd86673150ffc2883e0
mimarz <mimarz@gmail.com>
2021-07-16 11:34:55 +0200

    ♻️ Clarified test code usage

--- libraries/core-react/src/utils/index.ts
+++ libraries/core-react/src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './templates'
-export * from './test'
 export { setReactInputValue } from './setReactInputValue'
 
 export const trimSpaces = (text: string): string => text.replace(/ /g, '')
