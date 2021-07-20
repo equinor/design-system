683f6e8c1e838c1261e79baa6573a495f409eac7
mimarz <mimarz@gmail.com>
2021-07-20 12:47:39 +0200

    🚨 Fixed linting warnings in Pagination tests

--- libraries/core-react/src/components/Pagination/Pagination.test.tsx
+++ libraries/core-react/src/components/Pagination/Pagination.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { render, cleanup, screen } from '@testing-library/react'
+import { render, cleanup, screen, within } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import styled from 'styled-components'
@@ -17,37 +17,30 @@ describe('Pagination', () => {
     expect(asFragment()).toMatchSnapshot()
   })
   it('can extend the css for the component', () => {
-    const { container } = render(<StyledPagination totalItems={10} />)
-    const pagination = container.firstChild
+    render(<StyledPagination totalItems={10} />)
+    const pagination = screen.getByRole('navigation')
     expect(pagination).toHaveStyleRule('position', 'absolute')
   })
   it('has aria label', () => {
-    const { container } = render(<Pagination totalItems={10} />)
-    const pagination = container.firstChild
+    render(<Pagination totalItems={10} />)
+    const pagination = screen.getByRole('navigation')
     expect(pagination).toHaveAttribute('aria-label', 'pagination')
   })
   it('has aria label on pages', () => {
-    const { getAllByRole } = render(
-      <Pagination totalItems={100} defaultPage={3} />,
-    )
-    expect(getAllByRole('listitem')[0].firstChild).toHaveAttribute(
-      'aria-label',
-      'Go to previous page',
-    )
-    expect(getAllByRole('listitem')[1].firstChild).toHaveAttribute(
-      'aria-label',
-      'Go to page 1',
-    )
-    expect(getAllByRole('listitem')[3].firstChild).toHaveAttribute(
-      'aria-label',
-      'Current page, page 3',
-    )
+    render(<Pagination totalItems={100} defaultPage={3} />)
+    expect(
+      within(screen.getAllByRole('listitem')[0]).getByRole('button'),
+    ).toHaveAttribute('aria-label', 'Go to previous page')
+    expect(
+      within(screen.getAllByRole('listitem')[1]).getByRole('button'),
+    ).toHaveAttribute('aria-label', 'Go to page 1')
+    expect(
+      within(screen.getAllByRole('listitem')[3]).getByRole('button'),
+    ).toHaveAttribute('aria-label', 'Current page, page 3')
   })
   it('should render correct number of pages', () => {
-    const { getAllByRole } = render(
-      <Pagination totalItems={4} itemsPerPage={1} />,
-    )
-    expect(getAllByRole('listitem')).toHaveLength(6) // totalItems + 2: the < + > buttons)
+    render(<Pagination totalItems={4} itemsPerPage={1} />)
+    expect(screen.getAllByRole('listitem')).toHaveLength(6) // totalItems + 2: the < + > buttons)
   })
   it('should render ellipsis if number of pages is larger than 7', () => {
     render(<Pagination totalItems={8} itemsPerPage={1} />)
@@ -60,14 +53,12 @@ describe('Pagination', () => {
     expect(screen.getAllByLabelText('Ellipsis of pages')[1]).toBeDefined()
   })
   it('should render next and previous buttons', () => {
-    const { getAllByRole } = render(
-      <Pagination totalItems={3} itemsPerPage={1} />,
-    )
-    expect(getAllByRole('button')[0]).toHaveAttribute(
+    render(<Pagination totalItems={3} itemsPerPage={1} />)
+    expect(screen.getAllByRole('button')[0]).toHaveAttribute(
       'aria-label',
       'Go to previous page',
     )
-    expect(getAllByRole('button')[4]).toHaveAttribute(
+    expect(screen.getAllByRole('button')[4]).toHaveAttribute(
       'aria-label',
       'Go to next page',
     )
