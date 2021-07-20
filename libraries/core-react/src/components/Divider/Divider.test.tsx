98bab0572e7007c331afd0703c8332ce28ea5e2e
mimarz <mimarz@gmail.com>
2021-07-20 10:07:21 +0200

    🚨 Fixed linting errors in Divider tests

--- libraries/core-react/src/components/Divider/Divider.test.tsx
+++ libraries/core-react/src/components/Divider/Divider.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import styled from 'styled-components'
@@ -32,34 +32,34 @@ describe('Divider', () => {
     expect(asFragment()).toMatchSnapshot()
   })
   it('Has medium colour and medium spacing as default', () => {
-    const { container } = render(<Divider />)
-    expect(container.firstChild).toHaveStyleRule(
-      'background-color',
-      trimSpaces(mediumColor),
-    )
-    expect(container.firstChild).toHaveStyleRule('margin-top', spacingMedium)
+    render(<Divider />)
+    const divider = screen.getByRole('separator')
+    expect(divider).toHaveStyleRule('background-color', trimSpaces(mediumColor))
+    expect(divider).toHaveStyleRule('margin-top', spacingMedium)
   })
   it('Sets backgroundColor to light according to color prop', () => {
-    const { container } = render(<Divider color="light" />)
-    expect(container.firstChild).toHaveStyleRule(
-      'background-color',
-      trimSpaces(light),
-    )
+    render(<Divider color="light" />)
+    const divider = screen.getByRole('separator')
+
+    expect(divider).toHaveStyleRule('background-color', trimSpaces(light))
   })
   it('Sets backgroundColor to lighter according to color prop', () => {
-    const { container } = render(<Divider color="lighter" />)
-    expect(container.firstChild).toHaveStyleRule(
-      'background-color',
-      trimSpaces(lighter),
-    )
+    render(<Divider color="lighter" />)
+    const divider = screen.getByRole('separator')
+
+    expect(divider).toHaveStyleRule('background-color', trimSpaces(lighter))
   })
   it('Sets topMargin to small according to variant prop', () => {
-    const { container } = render(<Divider variant="small" />)
-    expect(container.firstChild).toHaveStyleRule('margin-top', spacingSmall)
+    render(<Divider variant="small" />)
+    const divider = screen.getByRole('separator')
+
+    expect(divider).toHaveStyleRule('margin-top', spacingSmall)
   })
   it('Can extend the css for the component', () => {
-    const { container } = render(<StyledDivider variant="small" />)
-    expect(container.firstChild).toHaveStyleRule('position', 'relative')
-    expect(container.firstChild).toHaveStyleRule('margin-top', spacingSmall)
+    render(<StyledDivider variant="small" />)
+    const divider = screen.getByRole('separator')
+
+    expect(divider).toHaveStyleRule('position', 'relative')
+    expect(divider).toHaveStyleRule('margin-top', spacingSmall)
   })
 })
