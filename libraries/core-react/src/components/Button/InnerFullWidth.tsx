2ad5a152babf437dc05e7aa81cfe195045e1d1bd
mimarz <mimarz@gmail.com>
2021-07-28 10:13:34 +0200

    Changed inner button to use css

--- libraries/core-react/src/components/Button/InnerFullWidth.tsx
+++ libraries/core-react/src/components/Button/InnerFullWidth.tsx
@@ -1,45 +1,5 @@
 import { forwardRef, Children as ReactChildren, HTMLAttributes } from 'react'
-import styled from 'styled-components'
-
-const FullWidthCenterContent = styled.span`
-  text-align: center;
-  flex: 1;
-`
-
-const FullWidthInner = styled.span`
-  height: 100%;
-  display: flex;
-  align-items: center;
-
-  > img:first-child,
-  > svg:first-child {
-    margin-right: 8px;
-  }
-
-  > img:last-child,
-  > svg:last-child {
-    margin-left: 8px;
-  }
-
-  > img:only-child,
-  > svg:only-child {
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  > span:first-child {
-    margin-left: 32px;
-  }
-
-  > span:last-child {
-    margin-right: 32px;
-  }
-
-  > span:only-child {
-    margin-right: 0;
-    margin-left: 0;
-  }
-`
+import './button.inner.css'
 
 export const InnerFullWidth = forwardRef<
   HTMLSpanElement,
@@ -48,11 +8,15 @@ export const InnerFullWidth = forwardRef<
   // We need everything in elements for proper flexing 💪
   const updatedChildren = ReactChildren.map(children, (child) =>
     typeof child !== 'object' ? (
-      <FullWidthCenterContent>{child}</FullWidthCenterContent>
+      <span style={{ textAlign: 'center', flex: 1 }}>{child}</span>
     ) : (
       child
     ),
   )
 
-  return <FullWidthInner ref={ref}>{updatedChildren}</FullWidthInner>
+  return (
+    <span className="eds-btn-inner--full-width" ref={ref}>
+      {updatedChildren}
+    </span>
+  )
 })
