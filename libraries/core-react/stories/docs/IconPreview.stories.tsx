410405cd65f7929d366d0a033de653c4a9572f9b
Frida Erdal <frida.erdal@olavstoppen.no>
2021-08-12 14:51:36 +0200

    🔥 Remove eslint ignore

--- libraries/core-react/stories/docs/IconPreview.stories.tsx
+++ libraries/core-react/stories/docs/IconPreview.stories.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Story, Meta } from '@storybook/react'
 import styled from 'styled-components'
 import { Icon, Button, Typography, TableOfContents } from '../../src'
