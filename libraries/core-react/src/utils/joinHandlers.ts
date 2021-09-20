ecf08551e42fcb13c26a756d3e248e8b21bd0ce2
mimarz <mimarz@gmail.com>
2021-09-20 09:30:42 +0200

    ♻️ Optional callback handlers

--- libraries/core-react/src/utils/joinHandlers.ts
+++ libraries/core-react/src/utils/joinHandlers.ts
@@ -1,8 +1,8 @@
 type Callback<T> = (e: T) => void
 
 export const joinHandlers = <T>(
-  handler1: Callback<T>,
-  handler2: Callback<T>,
+  handler1?: Callback<T>,
+  handler2?: Callback<T>,
 ): Callback<T> => {
   const callback: Callback<T> = (event) => {
     handler1 && handler1(event)
