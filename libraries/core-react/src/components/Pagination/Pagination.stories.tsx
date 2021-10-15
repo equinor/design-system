4e6e06ce34b0a16d2fdcabaea0496d62f19eb59f
oddvernes <oddbjorn.overnes@gmail.com>
2021-10-15 13:49:20 +0200

    📝 Added story for dynamic itemsPerpage

--- libraries/core-react/src/components/Pagination/Pagination.stories.tsx
+++ libraries/core-react/src/components/Pagination/Pagination.stories.tsx
@@ -1,4 +1,5 @@
-import { Pagination, PaginationProps } from '../..'
+import { useState } from 'react'
+import { Pagination, PaginationProps, Button } from '../..'
 import { Story, Meta } from '@storybook/react'
 
 export default {
@@ -37,3 +38,68 @@ export const Truncated: Story<PaginationProps> = () => (
 export const WithIndicator: Story<PaginationProps> = () => (
   <Pagination totalItems={140} itemsPerPage={3} withItemIndicator />
 )
+
+export const Dynamic: Story<PaginationProps> = () => {
+  const [currentPageIndex, setCurrentPageIndex] = useState(6)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+
+  const changePage = (pageIndex) => {
+    setCurrentPageIndex(pageIndex)
+  }
+  const changePageSize = (perPage) => {
+    setItemsPerPage(perPage)
+  }
+
+  const getVariantForPageSizeButton = (pageSize) => {
+    if (pageSize === itemsPerPage) return 'outlined'
+    return 'ghost'
+  }
+
+  return (
+    <>
+      <p>
+        Current page is: <b>{currentPageIndex}</b>
+      </p>
+      <div
+        style={{
+          display: 'flex',
+          gap: '1ch',
+          alignItems: 'center',
+          marginBottom: '2ch',
+        }}
+      >
+        <p>Items per page:</p>
+        <Button
+          onClick={() => changePageSize(10)}
+          variant={getVariantForPageSizeButton(10)}
+        >
+          10
+        </Button>
+        <Button
+          onClick={() => changePageSize(20)}
+          variant={getVariantForPageSizeButton(20)}
+        >
+          20
+        </Button>
+      </div>
+
+      <div>
+        <Pagination
+          totalItems={100}
+          itemsPerPage={itemsPerPage}
+          defaultPage={currentPageIndex}
+          onChange={(e, p) => changePage(p)}
+        />
+      </div>
+    </>
+  )
+}
+Dynamic.storyName = 'Dynamic itemsPerPage'
+Dynamic.parameters = {
+  docs: {
+    description: {
+      story:
+        'if `itemsPerPage` changes dynamically, the active page is set to 1',
+    },
+  },
+}
