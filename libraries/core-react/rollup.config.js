c0af348fcb474274dcd25d880c580f23197c71d7
oddvernes <oddbjorn.overnes@gmail.com>
2021-11-10 15:31:02 +0100

    🚧 wip: unbundled esm, removed umd packgage

--- libraries/core-react/rollup.config.js
+++ libraries/core-react/rollup.config.js
@@ -9,23 +9,22 @@ import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 
 import pkg from './package.json'
 
-const peerDeps = Object.keys(pkg.peerDependencies || {})
 const environment = process.env.NODE_ENV
 
 const isDevelopment = environment === 'development'
-
-const globals = {
-  react: 'React',
-  'react-dom': 'ReactDOM',
-  'styled-components': 'styled',
-}
 
 const extensions = ['.jsx', '.js', '.tsx', '.ts']
 
 export default [
   {
-    input: './src/index.ts',
-    external: peerDeps,
+    input: ['./src/index.ts'],
+    external: [
+      'react/jsx-runtime',
+      ...Object.keys({
+        ...pkg.peerDependencies,
+        ...pkg.dependencies,
+      }),
+    ],
     watch: {
       clearScreen: true,
       include: ['./src/**', './../tokens/**'],
@@ -47,17 +46,11 @@ export default [
     ],
     output: [
       {
-        file: pkg.publishConfig.browser,
-        name: pkg.name,
-        format: 'umd',
-        globals,
-      },
-      {
-        file: pkg.publishConfig.module,
-        name: pkg.name,
+        dir: 'dist/esm',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
         format: 'es',
         sourcemap: isDevelopment,
-        globals,
       },
       { file: pkg.publishConfig.main, format: 'cjs' },
     ],
