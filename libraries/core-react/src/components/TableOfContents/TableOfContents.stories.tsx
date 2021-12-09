fdb32abba97313fa9457cb2b4270de2593726c98
Laliq <marta.lalik@gmail.com>
2021-12-09 15:54:23 +0000

    ✨Add support for compact TableOfContent

--- libraries/core-react/src/components/TableOfContents/TableOfContents.stories.tsx
+++ libraries/core-react/src/components/TableOfContents/TableOfContents.stories.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react'
 import styled from 'styled-components'
-import { Icon, TableOfContents, Typography, TableOfContentsProps } from '../..'
+import {
+  Icon,
+  TableOfContents,
+  Typography,
+  TableOfContentsProps,
+  Density,
+  EdsProvider,
+} from '../..'
 import { Story, Meta } from '@storybook/react'
 
 import { subdirectory_arrow_right } from '@equinor/eds-icons'
@@ -177,5 +185,155 @@ export const Example: Story<TableOfContentsProps> = (args) => {
         </TableOfContents>
       </aside>
     </Wrapper>
+  )
+}
+
+export const Compact: Story<TableOfContentsProps> = (args) => {
+  const [density] = useState<Density>('comfortable')
+
+  return (
+    <EdsProvider density={density}>
+      <Wrapper>
+        <main>
+          <article>
+            <Typography variant="h1">Heading 1</Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="h2" id="sub-section-one">
+              Topic 1
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="h3">Heading 3</Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="h2" id="sub-section-two">
+              A very long topic to test implementation details
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="h3">Heading 3</Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="h2" id="sub-section-three">
+              Topic 3
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+            <Typography variant="h3">Heading 3</Typography>
+            <Typography variant="body_long">
+              Lorem ipsum dolor sit amet consecteur dit lot. Lorem ipsum dolor
+              sit amet consecteur dit lot. Lorem ipsum dolor sit amet consecteur
+              dit lot.
+            </Typography>
+          </article>
+        </main>
+        <aside>
+          <TableOfContents {...args}>
+            <TableOfContents.LinkItem>
+              <Typography variant="body_short" link href="#sub-section-one">
+                <Icon name="subdirectory_arrow_right" size={16} />
+                <span>Topic 1</span>
+              </Typography>
+            </TableOfContents.LinkItem>
+            <TableOfContents.LinkItem title="A very long topic to test proper implementation">
+              <Typography variant="body_short" link href="#sub-section-two">
+                <Icon name="subdirectory_arrow_right" size={16} />
+                <span>A very long topic to test proper implementation</span>
+              </Typography>
+            </TableOfContents.LinkItem>
+            <TableOfContents.LinkItem>
+              <Typography variant="body_short" link href="#sub-section-three">
+                <Icon name="subdirectory_arrow_right" size={16} />
+                <span>Topic 3</span>
+              </Typography>
+            </TableOfContents.LinkItem>
+          </TableOfContents>
+        </aside>
+      </Wrapper>
+    </EdsProvider>
   )
 }
--- libraries/core-react/src/components/TableOfContents/TableOfContents.tsx
+++ libraries/core-react/src/components/TableOfContents/TableOfContents.tsx
@@ -1,9 +1,11 @@
 import { forwardRef, HTMLAttributes } from 'react'
-import styled, { css } from 'styled-components'
+import styled, { css, ThemeProvider } from 'styled-components'
 import { List } from '../List'
 import { Typography } from '../Typography'
 import { typographyTemplate } from '../../utils'
-import { tableOfContents as tokens } from './TableOfContents.tokens'
+import { tableOfContents as tableOfContentToken } from './TableOfContents.tokens'
+import { useEds } from '../EdsProvider'
+import { useToken } from '../../hooks'
 
 export type TableOfContentsProps = {
   /** Sticky functionality */
@@ -12,38 +14,53 @@ export type TableOfContentsProps = {
   label?: string
 } & HTMLAttributes<HTMLElement>
 
-const StyledTableOfContents = styled.nav<TableOfContentsProps>`
-  margin-top: ${tokens.spacings.top};
-  margin-bottom: ${tokens.spacings.bottom};
+const StyledTableOfContents = styled.nav<TableOfContentsProps>(
+  ({ theme, sticky }) => {
+    return css`
+      margin-top: ${theme.spacings.top};
+      margin-bottom: ${theme.spacings.bottom};
 
-  ${({ sticky }) =>
-    sticky &&
-    css`
-      position: fixed;
-      top: ${tokens.entities.sticky.spacings.top};
-      right: ${tokens.entities.sticky.spacings.right};
-    `}
-`
+      ${sticky &&
+      css`
+        position: fixed;
+        top: ${theme.entities.sticky.spacings.top};
+        right: ${theme.entities.sticky.spacings.right};
+      `}
+    `
+  },
+)
 
 const TocList = styled(List)`
   margin: 0;
   padding: 0;
 `
 
-const TocLabel = styled(Typography)`
-  ${typographyTemplate(tokens.typography)}
-`
+const TocLabel = styled(Typography)(({ theme }) => {
+  return css`
+    ${typographyTemplate(theme.typography)}
+  `
+})
 
 const TableOfContents = forwardRef<HTMLElement, TableOfContentsProps>(
   function TableOfContents(
     { children, sticky = false, label = '', ...rest },
     ref,
   ) {
+    const { density } = useEds()
+    const token = useToken({ density }, tableOfContentToken)
+
     return (
-      <StyledTableOfContents ref={ref} label={label} sticky={sticky} {...rest}>
-        <TocLabel variant="overline">{label}</TocLabel>
-        <TocList>{children}</TocList>
-      </StyledTableOfContents>
+      <ThemeProvider theme={token}>
+        <StyledTableOfContents
+          ref={ref}
+          label={label}
+          sticky={sticky}
+          {...rest}
+        >
+          <TocLabel variant="overline">{label}</TocLabel>
+          <TocList>{children}</TocList>
+        </StyledTableOfContents>
+      </ThemeProvider>
     )
   },
 )
