94746b3e113fb467d3a81f6557be63a95b5fb0ec
mimarz <mimarz@gmail.com>
2021-12-15 15:06:10 +0100

    🚧 Removed koa & working on argsv parsing

--- apps/figma-broker/index.js
+++ apps/figma-broker/index.js
@@ -1,45 +1,36 @@
-#!/usr/bin/env NODE_NO_WARNINGS=1 node
-/* eslint-disable no-unused-vars */
+import { argv } from 'process'
 import dotenv from 'dotenv'
-import Koa from 'koa'
-import KoaRouter from '@koa/router'
-import KoaLogger from 'koa-logger'
-import KoaBody from 'koa-body'
-import R from 'ramda'
+import * as R from 'ramda'
 
 import { createTokens, createAssets } from './actions'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 9001
+console.log('argsv', JSON.stringify(argv))
 
-const app = new Koa()
-const router = new KoaRouter()
-const logger = new KoaLogger()
+// const runAction = R.curry(async (action, ctx) => {
+//   const result = await action(ctx)
+//   ctx.response.body = JSON.stringify(result)
+// })
 
-const runAction = R.curry(async (action, ctx) => {
-  const result = await action(ctx)
-  ctx.response.body = JSON.stringify(result)
-})
+// router
+//   .post('/create-tokens', KoaBody(), runAction(createTokens))
+//   .post('/create-assets', KoaBody(), runAction(createAssets))
 
-router
-  .post('/create-tokens', KoaBody(), runAction(createTokens))
-  .post('/create-assets', KoaBody(), runAction(createAssets))
+// app.use(logger).use(router.routes()).use(router.allowedMethods())
 
-app.use(logger).use(router.routes()).use(router.allowedMethods())
+// app.use(async (ctx, next) => {
+//   try {
+//     await next()
+//   } catch (err) {
+//     ctx.status = err.statusCode || err.status || 500
+//     ctx.body = {
+//       message: err.message,
+//     }
+//   }
+// })
 
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (err) {
-    ctx.status = err.statusCode || err.status || 500
-    ctx.body = {
-      message: err.message,
-    }
-  }
-})
-
-app.listen(PORT)
+// app.listen(PORT)
 
 // eslint-disable-next-line no-console
 console.info('Started Figma Broker 👨🏻‍💼💼🎉')
