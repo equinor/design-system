d7422f184c2df6f89f12408b3a77d75e4706ac0b
Laliq <marta.lalik@gmail.com>
2021-12-17 13:15:51 +0000

    Add styled

--- libraries/core-react/src/components/Group/Group.tsx
+++ libraries/core-react/src/components/Group/Group.tsx
@@ -1,7 +1,22 @@
 import { forwardRef } from 'react'
 import styled from 'styled-components'
+import { group as tokens } from './Group.tokens'
 
-const GroupBase = styled.div``
+const { border } = tokens
+
+const GroupBase = styled.div`
+  > * {
+    border-radius: 0;
+  }
+  > :first-child {
+    border-top-left-radius: ${border.type === 'border' && border.radius};
+    border-bottom-left-radius: ${border.type === 'border' && border.radius};
+  }
+  > :last-child {
+    border-top-right-radius: ${border.type === 'border' && border.radius};
+    border-bottom-right-radius: ${border.type === 'border' && border.radius};
+  }
+`
 
 export type GroupProps = React.HTMLAttributes<HTMLDivElement>
 
