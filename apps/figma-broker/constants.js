49ed0e44c41eae3a9bfe01be9181735958b8b348
Michael Marszalek <mmarsz@equinor.com>
2021-12-23 14:40:45 +0100

    🚚 Move static icons to assets (#1837)
    * 🚚 Moved static icons to assets
    
    * 📝 Updated README
    
    * 🚚 Updated figma assets dir

--- apps/figma-broker/constants.js
+++ apps/figma-broker/constants.js
@@ -1,6 +1,6 @@
 const COMMON_DIR = '../../common'
 const TOKENS_DIR = '../../packages/eds-tokens'
-const STATIC_DIR = '../../packages/static'
+const ASSETS_DIR = '../../assets'
 const ICONS_DIR = '../../packages/eds-icons'
 const STORYBOOK_DIR = '../../packages/eds-core-react/stories'
 
@@ -8,7 +8,7 @@ export const PATHS = {
   TOKENS: TOKENS_DIR,
   BASE_TOKENS: `${TOKENS_DIR}/base`,
   COMPONENTS_TOKENS: `${TOKENS_DIR}/components`,
-  ASSETS_ICONS: `${STATIC_DIR}/icons`,
+  ASSETS_ICONS: `${ASSETS_DIR}/icons`,
   SASS: `${COMMON_DIR}/public/sass`,
   CSS: `${COMMON_DIR}/public/css`,
   ICONS: `${STORYBOOK_DIR}/assets/icons`,
---
+++ assets/icons/README.md
@@ -0,0 +1,3 @@
+# Icons
+
+Collection of all the icons from EDS as plain svg and svg sprite files.
