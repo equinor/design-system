b05899d0de3b49f03dc1d9dda11117b1be8bc134
Victor Nystad <vnys@equinor.com>
2022-01-02 21:35:01 +0000

    Sort similarly to Zeroheight and Figma

--- packages/eds-core-react/.storybook/preview.js
+++ packages/eds-core-react/.storybook/preview.js
@@ -13,4 +13,22 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewMode: 'docs',
+  options: {
+    storySort: {
+      method: '',
+      order: [
+        'Introduction',
+        'Data Display',
+        'Feedback',
+        'Inputs',
+        'Navigation',
+        'Surfaces',
+        'Typography',
+        'Icons',
+        ['Introduction', 'Preview', 'Icon'],
+        '*',
+        'Playground',
+      ],
+    },
+  },
 }
