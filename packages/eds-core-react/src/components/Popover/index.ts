51920b35af7a197770dc0a97bbf980da205546b4
Laliq <marta.lalik@gmail.com>
2022-01-05 11:43:53 +0000

    Update index

--- packages/eds-core-react/src/components/Popover/index.ts
+++ packages/eds-core-react/src/components/Popover/index.ts
@@ -1,18 +1,27 @@
 import { Popover as BaseComponent, PopoverProps } from './Popover'
 import { PopoverTitle, PopoverTitleProps } from './PopoverTitle'
 import { PopoverContent, PopoverContentProps } from './PopoverContent'
+import { PopoverHeader, PopoverHeaderProps } from './PopoverHeader'
 
 type PopoverConformProps = typeof BaseComponent & {
   Title: typeof PopoverTitle
   Content: typeof PopoverContent
+  Header: typeof PopoverHeader
 }
 
 const Popover = BaseComponent as PopoverConformProps
 Popover.Title = PopoverTitle
 Popover.Content = PopoverContent
+Popover.Header = PopoverHeader
 
 Popover.Title.displayName = 'Popover.Title'
 Popover.Content.displayName = 'Popover.Content'
+Popover.Header.displayName = 'Popover.header'
 
 export { Popover }
-export type { PopoverProps, PopoverContentProps, PopoverTitleProps }
+export type {
+  PopoverProps,
+  PopoverContentProps,
+  PopoverTitleProps,
+  PopoverHeaderProps,
+}
