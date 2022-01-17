42d0afabb53d84ae47bdb4384ef57724994b638e
Laliq <marta.lalik@gmail.com>
2022-01-17 10:51:09 +0000

    ✨ Add DialogHeader component

---
+++ packages/eds-core-react/src/components/Dialog/DialogHeader.tsx
@@ -0,0 +1,38 @@
+import { forwardRef, HTMLAttributes } from 'react'
+import styled, { css } from 'styled-components'
+import { Divider } from '../Divider'
+
+export type DialogHeaderProps = HTMLAttributes<HTMLDivElement>
+
+const StyledDialogHeader = styled.div(({ theme }) => {
+  return css`
+    display: flex;
+    justify-content: space-betweene;
+    algin-items: center;
+    padding: ${theme.entities.children.spacings.top}
+      ${theme.entities.children.spacings.right} 0
+      ${theme.entities.children.spacings.left};
+  `
+})
+
+const StyledDivider = styled(Divider)(({ theme }) => {
+  return css`
+    margin-bottom: ${theme.entities.divider.spacings.bottom};
+  `
+})
+
+export const DialogHeader = forwardRef<HTMLDivElement, DialogHeaderProps>(
+  function DialogHeader({ children, ...rest }, ref) {
+    const props = {
+      ...rest,
+      ref,
+    }
+
+    return (
+      <div {...props}>
+        <StyledDialogHeader>{children}</StyledDialogHeader>
+        <StyledDivider variant="small" />
+      </div>
+    )
+  },
+)
--- packages/eds-core-react/src/components/Dialog/index.ts
+++ packages/eds-core-react/src/components/Dialog/index.ts
@@ -2,11 +2,13 @@ import { Dialog as BaseComponent, DialogProps } from './Dialog'
 import { Actions, DialogActionsProps } from './Actions'
 import { Title, DialogTitleProps } from './Title'
 import { CustomContent, DialogCustomContentProps } from './CustomContent'
+import { DialogHeader, DialogHeaderProps } from './DialogHeader'
 
 type CompoundDialogProps = typeof BaseComponent & {
   Actions: typeof Actions
   Title: typeof Title
   CustomContent: typeof CustomContent
+  Header: typeof DialogHeader
 }
 
 const Dialog = BaseComponent as CompoundDialogProps
@@ -14,10 +16,12 @@ const Dialog = BaseComponent as CompoundDialogProps
 Dialog.Actions = Actions
 Dialog.Title = Title
 Dialog.CustomContent = CustomContent
+Dialog.Header = DialogHeader
 
 Dialog.Actions.displayName = 'Dialog.Actions'
 Dialog.Title.displayName = 'Dialog.Title'
 Dialog.CustomContent.displayName = 'Dialog.CustomContent'
+Dialog.Header.displayName = 'Dialog.Header'
 
 export { Dialog }
 export type {
@@ -25,4 +29,5 @@ export type {
   DialogActionsProps,
   DialogTitleProps,
   DialogCustomContentProps,
+  DialogHeaderProps,
 }
