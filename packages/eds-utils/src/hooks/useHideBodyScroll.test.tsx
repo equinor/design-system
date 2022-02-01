c7d88fd389bf95df0d668b12ad340299f88c6cc9
oddvernes <oddbjorn.overnes@gmail.com>
2022-02-01 09:30:10 +0100

    ✅ useHideBodyScroll: Added another test

--- packages/eds-utils/src/hooks/useHideBodyScroll.test.tsx
+++ packages/eds-utils/src/hooks/useHideBodyScroll.test.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import { useHideBodyScroll } from './useHideBodyScroll'
 
 const TestComponent = ({ hideScroll }) => {
   useHideBodyScroll(hideScroll)
   return <div></div>
 }
+afterEach(cleanup)
 
 describe('useHideBodyScroll', () => {
   it('Should add "overflow: hidden" on document.body only while input is true', () => {
@@ -12,5 +13,12 @@ describe('useHideBodyScroll', () => {
     expect(document.body.style.overflow).toBe('hidden')
     rerender(<TestComponent hideScroll={false} />)
     expect(document.body.style.overflow).toBe('')
+  })
+  it('Should restore existing overflow style on document.body', () => {
+    document.body.style.overflow = 'clip'
+    const { rerender } = render(<TestComponent hideScroll={true} />)
+    expect(document.body.style.overflow).toBe('hidden')
+    rerender(<TestComponent hideScroll={false} />)
+    expect(document.body.style.overflow).toBe('clip')
   })
 })
