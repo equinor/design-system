dfef931952c4e5fb3846ee8cbe225bcf5c0f63a0
Laliq <marta.lalik@gmail.com>
2022-02-01 09:49:23 +0000

    🔧 Upade config file

--- packages/eds-core-react/rtl.setup.ts
+++ packages/eds-core-react/rtl.setup.ts
@@ -1,2 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect'
+import { toHaveNoViolations } from 'jest-axe'
+
+expect.extend(toHaveNoViolations)
