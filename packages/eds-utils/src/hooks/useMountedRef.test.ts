6d9d4aae0f06df1e8a8d26bdb7bd182a8c0b427f
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2022-02-01 13:39:57 +0100

    ✅ eds-utils tests (#1920)
    * ✅ useOutsideClick tests
    
    * ✅ useCombinedRefs tests
    
    * ✅ create useGlobalKeypress test
    
    * 🐛 useBodyScroll: Implement fix from old utils
    
    * ✅ useHideBodyScroll test
    
    * ✅ useId tests
    
    * ✅ useAutoResize test
    
    * ✅ useToken tests
    
    * ✅ usePopper test
    
    * ✅ Update useIsMounted test
    
    * ✅ useHideBodyScroll: Added another test
    
    * ✅ usePopper: added test for placement and offset

--- packages/eds-utils/src/hooks/useMountedRef.test.ts
+++ packages/eds-utils/src/hooks/useMountedRef.test.ts
@@ -1,7 +1,9 @@
 import { renderHook } from '@testing-library/react-hooks'
 import { useIsMounted } from './useMountedRef'
 
-test('should return true when rendered', () => {
-  const { result } = renderHook(() => useIsMounted())
-  expect(result.current).toBe(true)
+describe('useIsMounted', () => {
+  it('should return true when rendered', () => {
+    const { result } = renderHook(() => useIsMounted())
+    expect(result.current).toBe(true)
+  })
 })
