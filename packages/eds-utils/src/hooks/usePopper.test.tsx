6690f0985ee1b00e3f85af0fa56f671ae0bb2162
oddvernes <oddbjorn.overnes@gmail.com>
2022-02-01 09:56:12 +0100

    ✅ usePopper: added test for placement and offset

--- packages/eds-utils/src/hooks/usePopper.test.tsx
+++ packages/eds-utils/src/hooks/usePopper.test.tsx
@@ -1,13 +1,35 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-import { useState, useRef } from 'react'
-import { usePopper } from './usePopper'
+/* eslint-disable react/require-default-props */
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  cleanup,
+} from '@testing-library/react'
+import { useState, useRef, HTMLAttributes } from 'react'
+import { usePopper, Placement } from './usePopper'
 
-const TestComponent = ({ ...rest }) => {
+type MenuProps = {
+  placement?: Placement
+  offset?: number
+} & HTMLAttributes<HTMLDivElement>
+
+const TestComponent = ({
+  placement = 'auto',
+  offset = 10,
+  ...rest
+}: MenuProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement>(null)
   const anchorRef = useRef<HTMLButtonElement>(null)
   const [popperEl, setPopperEl] = useState<HTMLDivElement>(null)
-  const { styles, attributes } = usePopper(anchorEl, popperEl)
+  const { styles, attributes } = usePopper(
+    anchorEl,
+    popperEl,
+    null,
+    placement,
+    offset,
+  )
 
   const handleClick = () => {
     if (!isOpen) {
@@ -36,6 +58,7 @@ const TestComponent = ({ ...rest }) => {
     </div>
   )
 }
+afterEach(cleanup)
 
 describe('usePopper', () => {
   it('Should add popper attributes and styles after usePopper recieves anchor element', async () => {
@@ -50,6 +73,20 @@ describe('usePopper', () => {
     await waitFor(() => {
       expect(menu).toHaveAttribute('data-popper-placement', 'top')
       expect(content).toHaveAttribute('style')
+    })
+  })
+
+  it('Should forward placement and offset to popper', async () => {
+    render(<TestComponent placement="left" offset={20} />)
+    const button = screen.getByText('click')
+    const menu = screen.getByRole('menu')
+    const content = screen.getByText('content')
+
+    fireEvent.click(button)
+
+    await waitFor(() => {
+      expect(menu).toHaveAttribute('data-popper-placement', 'left')
+      expect(content.style.transform).toBe('translate(-20px, 0px)')
     })
   })
 })
