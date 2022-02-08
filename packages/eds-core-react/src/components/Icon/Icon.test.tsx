aaf8d30acce90f703b9c1864e81ac2abf10e18c3
Laliq <marta.lalik@gmail.com>
2022-02-08 13:38:20 +0000

    ✅ Add a11y test for Icon

--- packages/eds-core-react/src/components/Icon/Icon.test.tsx
+++ packages/eds-core-react/src/components/Icon/Icon.test.tsx
@@ -2,6 +2,7 @@
 import { render, cleanup, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+import { axe } from 'jest-axe'
 import styled from 'styled-components'
 import { save } from '@equinor/eds-icons'
 import { Icon } from '.'
@@ -20,6 +21,19 @@ describe('Icon', () => {
   it('Matches snapshot using data', () => {
     const { asFragment } = render(<Icon data={save} />)
     expect(asFragment()).toMatchSnapshot()
+  })
+  it('Should pass a11y test using data', async () => {
+    const { container } = render(<Icon data={save} />)
+    expect(await axe(container)).toHaveNoViolations()
+  })
+  it('Should pass a11y test using name', async () => {
+    const { container } = render(<Icon name="save" />)
+    expect(await axe(container)).toHaveNoViolations()
+  })
+  it('Should pass a11y test using title', async () => {
+    const title = 'Save me'
+    const { container } = render(<Icon name="save" title={title} />)
+    expect(await axe(container)).toHaveNoViolations()
   })
   it('Matches snapshot using name ', () => {
     const { asFragment } = render(<Icon name="save" />)
