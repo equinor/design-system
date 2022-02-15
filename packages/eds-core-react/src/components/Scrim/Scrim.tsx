85631f3eb05b54028c25bc606685812196204bde
oddvernes <oddbjorn.overnes@gmail.com>
2022-02-15 14:43:01 +0100

    💄 Scrim: Bump z-index to 1300

--- packages/eds-core-react/src/components/Scrim/Scrim.tsx
+++ packages/eds-core-react/src/components/Scrim/Scrim.tsx
@@ -10,7 +10,7 @@ const StyledScrim = styled.div`
   height: ${height};
   background: ${background};
   position: fixed;
-  z-index: 500;
+  z-index: 1300;
   top: 0;
   left: 0;
   align-items: center;
