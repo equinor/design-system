9155e06e4bba413c9879923c53f56da31f3bd219
Laliq <marta.lalik@gmail.com>
2022-03-08 14:53:44 +0000

    🔥 Remove headerLevel from Accordion

--- packages/eds-core-react/src/components/Accordion/Accordion.tsx
+++ packages/eds-core-react/src/components/Accordion/Accordion.tsx
@@ -15,7 +15,7 @@ const Accordion = forwardRef<
   HTMLDivElement,
   AccordionProps & HTMLAttributes<HTMLDivElement>
 >(function Accordion(
-  { headerLevel = 'h2', chevronPosition = 'left', children, id, ...props },
+  { chevronPosition = 'left', children, id, ...props },
   ref,
 ) {
   const accordionId = useId(id, 'accordion')
@@ -27,7 +27,6 @@ const Accordion = forwardRef<
     return cloneElement(child as ReactElement, {
       accordionId,
       index,
-      headerLevel,
       chevronPosition,
     })
   })
