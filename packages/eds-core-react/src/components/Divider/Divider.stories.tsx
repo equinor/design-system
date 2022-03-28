10bd46c495ffc100cce37054eedce64da1c64eb7
Laliq <marta.lalik@gmail.com>
2022-03-28 16:02:46 +0000

    Add fill-bleed divider example

--- packages/eds-core-react/src/components/Divider/Divider.stories.tsx
+++ packages/eds-core-react/src/components/Divider/Divider.stories.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-import { Divider, DividerProps } from '../..'
+import { Divider, DividerProps, Card, Typography, Icon } from '../..'
+// import { chevron_left, edit_text, more_vertical } from '@equinor/eds-icons'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { Stack as SBStack } from './../../../.storybook/components'
 import page from './Divider.docs.mdx'
@@ -19,19 +20,66 @@ export default {
 } as Meta
 
 const Stack = styled(SBStack)`
-  display: block;
+  /* display: block; */
   padding: 32px;
-  background-color: #999;
+  /* background-color: #999; */
+  background: #ebebeb;
 `
 
 export const Introduction: Story<DividerProps> = (args) => (
+  <>
+    <Divider {...args} />
+    <Divider {...args} />
+    <Divider {...args} />
+  </>
+)
+
+export const FullBleed: Story<DividerProps> = () => (
   <Stack>
-    <Divider {...args} />
-    <Divider {...args} />
-    <Divider {...args} />
+    <Card style={{ width: '50%' }}>
+      <Card.Header style={{ backgroundColor: '#DCDCDC' }}>
+        <Card.HeaderTitle>
+          <Typography variant="h4">NEWS</Typography>
+        </Card.HeaderTitle>
+      </Card.Header>
+      <Card.Content>
+        <Typography variant="caption">Today</Typography>
+        <br />
+        <Typography variant="overline">
+          Leading images are full width.
+        </Typography>
+        <Typography>
+          Use dividers occasionally, to create groupings rather than separate
+          individual items. Dividers should be noticeable in a layout, but not
+          clashing.
+        </Typography>
+      </Card.Content>
+      <Divider style={{ width: '100%' }} variant="small" />
+      <Card.Content>
+        <Typography variant="overline">
+          Leading images are full width.
+        </Typography>
+        <Typography>
+          Use dividers occasionally, to create groupings rather than separate
+          individual items.
+        </Typography>
+      </Card.Content>
+      <Divider style={{ width: '100%' }} variant="small" />
+      <Card.Content>
+        <Typography variant="overline">
+          Leading images are full width.
+        </Typography>
+        <Typography>
+          Use dividers occasionally, to create groupings rather than separate
+          individual items. Dividers should only be used if elements can't be
+          split using white space.
+        </Typography>
+      </Card.Content>
+    </Card>
   </Stack>
 )
 
+/* Is it needed? */
 /* export const Color: Story<DividerProps> = () => (
   <Stack>
     <Divider />
