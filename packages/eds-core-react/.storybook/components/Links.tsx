bcf3563de9ca08ebd2f351bdc06f86527fe414db
mimarz <mimarz@gmail.com>
2022-03-29 12:58:30 +0200

    ✨ Added npm & source url links

--- packages/eds-core-react/.storybook/components/Links.tsx
+++ packages/eds-core-react/.storybook/components/Links.tsx
@@ -69,13 +69,45 @@ const W3cIcon = () => (
   </svg>
 )
 
+const NpmIcon = () => (
+  <svg
+    preserveAspectRatio="xMidYMid"
+    viewBox="0 0 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+    width="1rem"
+    height="1rem"
+  >
+    <path d="M0 256V0h256v256z" fill="#C12127"></path>
+    <path d="M48 48h160v160h-32V80h-48v128H48z" fill="#FFF"></path>
+  </svg>
+)
+
+const GithubIcon = () => (
+  <svg
+    width="1rem"
+    height="1rem"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10.9 2.1c-4.6.5-8.3 4.2-8.8 8.7-.5 4.7 2.2 8.9 6.3 10.5.3.1.6-.1.6-.5v-1.6s-.4.1-.9.1c-1.4 0-2-1.2-2.1-1.9-.1-.4-.3-.7-.6-1-.3-.1-.4-.1-.4-.2 0-.2.3-.2.4-.2.6 0 1.1.7 1.3 1 .5.8 1.1 1 1.4 1 .4 0 .7-.1.9-.2.1-.7.4-1.4 1-1.8-2.3-.5-4-1.8-4-4 0-1.1.5-2.2 1.2-3-.1-.2-.2-.7-.2-1.4 0-.4 0-1 .3-1.6 0 0 1.4 0 2.8 1.3.5-.2 1.2-.3 1.9-.3s1.4.1 2 .3C15.3 6 16.8 6 16.8 6c.2.6.2 1.2.2 1.6 0 .8-.1 1.2-.2 1.4.7.8 1.2 1.8 1.2 3 0 2.2-1.7 3.5-4 4 .6.5 1 1.4 1 2.3v2.6c0 .3.3.6.7.5 3.7-1.5 6.3-5.1 6.3-9.3 0-6-5.1-10.7-11.1-10z" />
+  </svg>
+)
+
 type LinksProps = {
   figmaUrl?: string
   ariaUrl?: string
   documentationUrl?: string
+  npmUrl?: string
+  sourceUrl?: string
 }
 
-export const Links = ({ ariaUrl, figmaUrl, documentationUrl }: LinksProps) => (
+export const Links = ({
+  ariaUrl,
+  figmaUrl,
+  documentationUrl,
+  npmUrl,
+  sourceUrl,
+}: LinksProps) => (
   <Stack style={{ justifyContent: 'flex-start' }}>
     {ariaUrl && (
       <Button variant="ghost" as="a" href={ariaUrl}>
@@ -92,6 +124,18 @@ export const Links = ({ ariaUrl, figmaUrl, documentationUrl }: LinksProps) => (
     {documentationUrl && (
       <Button variant="ghost" as="a" href={documentationUrl}>
         📝 &nbsp;Documentation
+      </Button>
+    )}
+    {npmUrl && (
+      <Button variant="ghost" as="a" href={npmUrl}>
+        <NpmIcon />
+        <span>Package</span>
+      </Button>
+    )}
+    {sourceUrl && (
+      <Button variant="ghost" as="a" href={sourceUrl}>
+        <GithubIcon />
+        <span>Source</span>
       </Button>
     )}
   </Stack>
