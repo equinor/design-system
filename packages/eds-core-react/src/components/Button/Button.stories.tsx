424fa7b668ccee8fbc525ec75caabdd5616ad94d
mimarz <mimarz@gmail.com>
2022-03-29 14:05:06 +0200

    📝 Updated docs

--- packages/eds-core-react/src/components/Button/Button.stories.tsx
+++ packages/eds-core-react/src/components/Button/Button.stories.tsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react'
-import { Button, Icon, ButtonProps, EdsProvider, Density } from '../..'
+import {
+  Button,
+  Icon,
+  ButtonProps,
+  EdsProvider,
+  Density,
+  Progress,
+} from '../..'
 import styled from 'styled-components'
-import { Meta, Story } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
+import { Story, ComponentMeta } from '@storybook/react'
 import { menu } from '@equinor/eds-icons'
 import { Stack as SBStack } from './../../../.storybook/components'
 // import { Group } from '../Group'
@@ -38,7 +44,7 @@ export default {
       page,
     },
   },
-} as Meta
+} as ComponentMeta<typeof Button>
 
 export const Introduction: Story<ButtonProps> = (args) => (
   <Button {...args}>You can control me</Button>
@@ -55,6 +61,84 @@ export const Basic: Story<ButtonProps> = () => (
     <Button variant="ghost">Ghost</Button>
     <Button variant="ghost_icon">
       <Icon name="save" title="save action"></Icon>
+    </Button>
+  </Stack>
+)
+
+export const GhostIcon: Story<ButtonProps> = () => (
+  <Stack>
+    <Button variant="ghost_icon">
+      <Icon name="save" title="save action"></Icon>
+    </Button>
+    <Button variant="ghost_icon" color="secondary">
+      <Icon name="save" title="save"></Icon>
+    </Button>
+    <Button variant="ghost_icon" color="danger">
+      <Icon name="save" title="save"></Icon>
+    </Button>
+    <Button variant="ghost_icon" disabled>
+      <Icon name="save" title="save"></Icon>
+    </Button>
+  </Stack>
+)
+
+export const Color: Story<ButtonProps> = () => (
+  <Stack>
+    <Button color="primary">Primary</Button>
+    <Button color="secondary">Secondary</Button>
+    <Button color="danger">Danger</Button>
+  </Stack>
+)
+
+export const Compact: Story<ButtonProps> = () => {
+  const [density, setDensity] = useState<Density>('comfortable')
+
+  useEffect(() => {
+    // Simulate user change
+    setDensity('compact')
+  }, [density])
+
+  return (
+    <EdsProvider density={density}>
+      <Stack>
+        <Button>Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="ghost_icon">
+          <Icon data={menu} title="Ghost icon menu"></Icon>
+        </Button>
+      </Stack>
+    </EdsProvider>
+  )
+}
+
+export const FileUpload: Story<ButtonProps> = () => (
+  <Stack>
+    <label htmlFor="file-upload">
+      <input
+        type="file"
+        id="file-upload"
+        style={{ display: 'none' }}
+        multiple
+      />
+      <Button as="span">Upload file</Button>
+    </label>
+  </Stack>
+)
+
+export const ProgressButton: Story<ButtonProps> = () => (
+  <Stack>
+    <Button>
+      <Progress.Dots />
+    </Button>
+    <Button variant="ghost_icon">
+      <Progress.Dots color="primary" />
+    </Button>
+    <Button>
+      <Progress.Circular size={16} color="neutral" />
+    </Button>
+    <Button variant="ghost_icon">
+      <Progress.Circular size={16} color="primary" />
     </Button>
   </Stack>
 )
@@ -100,192 +184,6 @@ export const All: Story<ButtonProps> = () => (
   </Stack>
 )
 
-export const Contained: Story<ButtonProps> = () => (
-  <Stack>
-    <Button>Primary</Button>
-    <Button color="secondary">Secondary</Button>
-    <Button color="danger">Danger</Button>
-    <Button disabled>Disabled</Button>
-    <Button>
-      <Icon name="save" title="save"></Icon>Primary
-    </Button>
-    <Button color="secondary">
-      <Icon name="save" title="save"></Icon>Secondary
-    </Button>
-    <Button color="danger">
-      <Icon name="save" title="save"></Icon>Danger
-    </Button>
-    <Button disabled>
-      <Icon name="save" title="save"></Icon>Disabled
-    </Button>
-    <Button>
-      Primary <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button color="secondary">
-      Secondary
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button color="danger">
-      Danger
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button disabled>
-      Disabled
-      <Icon name="save" title="save"></Icon>
-    </Button>
-  </Stack>
-)
-
-export const Outlined: Story<ButtonProps> = () => (
-  <Stack>
-    <Button variant="outlined">Primary</Button>
-    <Button variant="outlined" color="secondary">
-      Secondary
-    </Button>
-    <Button variant="outlined" color="danger">
-      Danger
-    </Button>
-    <Button variant="outlined" disabled>
-      Disabled
-    </Button>
-    <Button variant="outlined">
-      <Icon name="save" title="save"></Icon>
-      Primary
-    </Button>
-    <Button variant="outlined" color="secondary">
-      <Icon name="save" title="save"></Icon>
-      Secondary
-    </Button>
-    <Button variant="outlined" color="danger">
-      <Icon name="save" title="save"></Icon>
-      Danger
-    </Button>
-    <Button variant="outlined" disabled>
-      <Icon name="save" title="save"></Icon>
-      Disabled
-    </Button>
-    <Button variant="outlined">
-      Primary
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="outlined" color="secondary">
-      Secondary
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="outlined" color="danger">
-      Danger
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="outlined" disabled>
-      Disabled
-      <Icon name="save" title="save"></Icon>
-    </Button>
-  </Stack>
-)
-
-export const Ghost: Story<ButtonProps> = () => (
-  <Stack>
-    <Button variant="ghost">Primary</Button>
-    <Button variant="ghost" color="secondary">
-      Secondary
-    </Button>
-    <Button variant="ghost" color="danger">
-      Danger
-    </Button>
-    <Button variant="ghost" disabled>
-      Disabled
-    </Button>
-    <Button variant="ghost">
-      <Icon name="save" title="save"></Icon>
-      Primary
-    </Button>
-    <Button variant="ghost" color="secondary">
-      <Icon name="save" title="save"></Icon>
-      Secondary
-    </Button>
-    <Button variant="ghost" color="danger">
-      <Icon name="save" title="save"></Icon>
-      Danger
-    </Button>
-    <Button variant="ghost" disabled>
-      <Icon name="save" title="save"></Icon>
-      Disabled
-    </Button>
-    <Button variant="ghost">
-      Primary
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="ghost" color="secondary">
-      Secondary
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="ghost" color="danger">
-      Danger
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="ghost" disabled>
-      Disabled
-      <Icon name="save" title="save"></Icon>
-    </Button>
-  </Stack>
-)
-
-export const GhostIcon: Story<ButtonProps> = () => (
-  <Stack>
-    <Button variant="ghost_icon">
-      <Icon name="save" title="save action"></Icon>
-    </Button>
-    <Button variant="ghost_icon" color="secondary">
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="ghost_icon" color="danger">
-      <Icon name="save" title="save"></Icon>
-    </Button>
-    <Button variant="ghost_icon" disabled>
-      <Icon name="save" title="save"></Icon>
-    </Button>
-  </Stack>
-)
-
-export const Form: Story<ButtonProps> = () => {
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault() // to prevent navigation from storybook
-    action('onSubmit')(e)
-  }
-
-  return (
-    <Stack>
-      <form onSubmit={handleSubmit}>
-        <Button type="submit">Submit form</Button>
-      </form>
-    </Stack>
-  )
-}
-
-export const FileUpload: Story<ButtonProps> = () => (
-  <Stack>
-    <input type="file" id="file-upload" style={{ display: 'none' }} multiple />
-    <label htmlFor="file-upload">
-      <Button as="span">Upload</Button>
-    </label>
-  </Stack>
-)
-
-FileUpload.parameters = {
-  docs: {
-    description: {
-      story:
-        'Please note this demo only works in Storybook Canvas (isolated example only)',
-    },
-  },
-}
-
-export const Link: Story<ButtonProps> = () => (
-  <Stack>
-    <Button href="#">Link</Button>
-  </Stack>
-)
-
 export const FullWidth: Story<ButtonProps> = () => (
   <FullWidthStack>
     <Button fullWidth>Primary</Button>
@@ -327,36 +225,6 @@ export const FullWidth: Story<ButtonProps> = () => (
     </Button>
   </FullWidthStack>
 )
-
-export const Compact: Story<ButtonProps> = () => {
-  const [density, setDensity] = useState<Density>('comfortable')
-
-  useEffect(() => {
-    // Simulate user change
-    setDensity('compact')
-  }, [density])
-
-  return (
-    <EdsProvider density={density}>
-      <Stack>
-        <Button>Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="ghost_icon">
-          <Icon data={menu} title="Ghost icon menu"></Icon>
-        </Button>
-      </Stack>
-    </EdsProvider>
-  )
-}
-
-Compact.parameters = {
-  docs: {
-    description: {
-      story: 'Compact `Button` using `EdsProvider`',
-    },
-  },
-}
 
 // export const ButtonGroup: Story<ButtonProps> = () => (
 //   <Stack style={{ gridGap: 0 }}>
