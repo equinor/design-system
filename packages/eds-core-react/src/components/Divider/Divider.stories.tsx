fe2389c4bb9afd3baa30e74de9254eaf087052f6
Laliq <marta.lalik@gmail.com>
2022-03-31 13:14:17 +0200

    📝  Divider documentation (#2093)
    * 📝  Add docs file
    
    * 📝  Adding content to docs file
    
    * 📝  Add usage description & set up the layout
    
    * Layout change
    
    * Add fill-bleed divider example
    
    * Add descriptiom to full-bleed divider example & update general layout
    
    * Add Inset divider example
    
    * Merge Links component
    
    * Ada Middle divider example
    
    * Add Links

--- packages/eds-core-react/src/components/Divider/Divider.stories.tsx
+++ packages/eds-core-react/src/components/Divider/Divider.stories.tsx
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
-import { Divider, DividerProps } from '../..'
+import {
+  Avatar,
+  Button,
+  Card,
+  Chip,
+  Divider,
+  DividerProps,
+  Typography,
+} from '../..'
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { Stack as SBStack } from './../../../.storybook/components'
+import page from './Divider.docs.mdx'
 
 export default {
   title: 'Data Display/Divider',
@@ -11,33 +21,135 @@ export default {
   },
   parameters: {
     docs: {
-      description: {
-        component: `A divider is a thin line that separates content into clear groups.
-        `,
-      },
+      page,
     },
   },
 } as Meta
 
-const Wrapper = styled.div`
+const Stack = styled(SBStack)`
   padding: 32px;
-  background-color: #999;
+  background: #ebebeb;
 `
 
-export const Default: Story<DividerProps> = (args) => <Divider {...args} />
-
-export const Small: Story<DividerProps> = () => (
-  <Wrapper>
-    <Divider color="lighter" variant="small" />
-    <Divider color="light" variant="small" />
-    <Divider variant="small" />
-  </Wrapper>
+export const Introduction: Story<DividerProps> = (args) => (
+  <>
+    <Divider {...args} />
+    <Divider {...args} />
+    <Divider {...args} />
+  </>
 )
 
-export const Medium: Story<DividerProps> = () => (
-  <Wrapper>
-    <Divider color="lighter" />
-    <Divider color="light" />
-    <Divider />
-  </Wrapper>
+export const FullBleed: Story<DividerProps> = () => (
+  <Stack>
+    <Card style={{ width: '50%' }}>
+      <Card.Header>
+        <Card.HeaderTitle>
+          <Typography variant="h4">NEWS</Typography>
+        </Card.HeaderTitle>
+      </Card.Header>
+      <Card.Content>
+        <Typography variant="caption">Today</Typography>
+        <br />
+        <Typography variant="overline">News title</Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Typography>
+      </Card.Content>
+      <Divider style={{ width: '100%' }} variant="small" />
+      <Card.Content>
+        <Typography variant="overline">News title</Typography>
+        <Typography>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+        </Typography>
+      </Card.Content>
+      <Divider style={{ width: '100%' }} variant="small" />
+      <Card.Content>
+        <Typography variant="overline">News title</Typography>
+        <Typography>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </Typography>
+      </Card.Content>
+    </Card>
+  </Stack>
+)
+
+export const Inset: Story<DividerProps> = () => (
+  <Stack>
+    <Card style={{ width: '50%' }}>
+      <Card.Header>
+        <Card.HeaderTitle>
+          <Typography variant="h4">ALL</Typography>
+        </Card.HeaderTitle>
+      </Card.Header>
+      <Card.Header>
+        <Avatar alt="Kitten" src="https://i.imgur.com/UM3mrju.jpg" size={40} />
+        <Card.HeaderTitle>
+          <Typography variant="h6">Title goes here</Typography>
+          <Typography variant="body_short">
+            Ut enim ad minim veniam, quis nostrud exercitation.
+          </Typography>
+          <Divider style={{ width: '100%' }} variant="small" />
+        </Card.HeaderTitle>
+      </Card.Header>
+      <Card.Header>
+        <Avatar alt="Kitten" src="https://i.imgur.com/UM3mrju.jpg" size={40} />
+        <Card.HeaderTitle>
+          <Typography variant="h6">Title goes here</Typography>
+          <Typography variant="body_short">
+            Ut enim ad minim veniam, quis nostrud exercitation.
+          </Typography>
+          <Divider style={{ width: '100%' }} variant="small" />
+        </Card.HeaderTitle>
+      </Card.Header>
+      <Card.Header>
+        <Avatar alt="Kitten" src="https://i.imgur.com/UM3mrju.jpg" size={40} />
+        <Card.HeaderTitle>
+          <Typography variant="h6">Title goes here</Typography>
+          <Typography variant="body_short">
+            Ut enim ad minim veniam, quis nostrud exercitation.
+          </Typography>
+        </Card.HeaderTitle>
+      </Card.Header>
+    </Card>
+  </Stack>
+)
+
+export const Middle: Story<DividerProps> = () => (
+  <Stack>
+    <Card style={{ width: '50%' }}>
+      <Card.Header>
+        <Card.HeaderTitle>
+          <Typography variant="h4">TICKET</Typography>
+        </Card.HeaderTitle>
+        <Typography variant="h6">20.02.2020</Typography>
+      </Card.Header>
+      <Card.Content>
+        <Typography variant="overline">Description</Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Typography>
+        <Divider style={{ width: '100%' }} />
+      </Card.Content>
+      <Card.Content>
+        <Typography>Choose option</Typography>
+      </Card.Content>
+      <Card.Actions>
+        <Chip>active</Chip>
+        <Chip variant="active">pause</Chip>
+        <Chip>disable</Chip>
+        <Chip variant="error">stop</Chip>
+      </Card.Actions>
+      <Card.Actions>
+        <Button style={{ marginTop: '16px' }} variant="outlined">
+          SUBMIT TICKET
+        </Button>
+      </Card.Actions>
+    </Card>
+  </Stack>
 )
