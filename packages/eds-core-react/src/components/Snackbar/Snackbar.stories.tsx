26832b9e2955d421fd27a27623eae0760ddc978b
Laliq <marta.lalik@gmail.com>
2022-04-08 07:47:35 +0200

    📝  Snackbar documentation (#2143)
    * 📝  Create docs file
    
    * 📝  Add Introduction
    
    * 📝  Add Usage
    
    * 📝  Add Examples & update links

--- packages/eds-core-react/src/components/Snackbar/Snackbar.stories.tsx
+++ packages/eds-core-react/src/components/Snackbar/Snackbar.stories.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { Snackbar, SnackbarProps, Button, EdsProvider, Density } from '../..'
-import { Story, Meta } from '@storybook/react'
+import { Story, ComponentMeta } from '@storybook/react'
+import { Stack } from './../../../.storybook/components'
+import page from './Snackbar.docs.mdx'
 
 export default {
   title: 'Feedback/Snackbar',
@@ -8,26 +10,23 @@ export default {
   subcomponents: { Action: Snackbar.Action },
   parameters: {
     docs: {
-      description: {
-        component: `Snackbars provide brief temporary non-interrupting notifications at the bottom of the screen.
-        `,
-      },
+      page,
     },
   },
-} as Meta
+} as ComponentMeta<typeof Snackbar>
 
-export const Default: Story<SnackbarProps> = (args) => {
+export const Introduction: Story<SnackbarProps> = (args) => {
   const { open } = args
   const [visible, setVisible] = useState(open)
   return (
-    <>
+    <Stack>
       <Button type="button" onClick={() => setVisible(true)}>
         Show a simple snackbar with default options
       </Button>
       <Snackbar {...args} open={visible} onClose={() => setVisible(false)}>
         Play with me
       </Snackbar>
-    </>
+    </Stack>
   )
 }
 
@@ -35,7 +34,7 @@ export const Simple: Story<SnackbarProps> = () => {
   const [open, setOpen] = useState(false)
 
   return (
-    <>
+    <Stack>
       <Button type="button" onClick={() => setOpen(true)}>
         Show a simple snackbar for 5 seconds
       </Button>
@@ -46,14 +45,14 @@ export const Simple: Story<SnackbarProps> = () => {
       >
         Message goes here
       </Snackbar>
-    </>
+    </Stack>
   )
 }
 
 export const WithAction: Story<SnackbarProps> = () => {
   const [withActionOpen, setWithActionOpen] = useState(false)
   return (
-    <>
+    <Stack>
       <Button type="button" onClick={() => setWithActionOpen(true)}>
         Show a snackbar with action for the default 7 seconds
       </Button>
@@ -63,10 +62,9 @@ export const WithAction: Story<SnackbarProps> = () => {
           <Button variant="ghost">Undo</Button>
         </Snackbar.Action>
       </Snackbar>
-    </>
+    </Stack>
   )
 }
-
 WithAction.storyName = 'With action'
 
 export const Compact: Story<SnackbarProps> = () => {
@@ -79,25 +77,19 @@ export const Compact: Story<SnackbarProps> = () => {
   }, [density])
 
   return (
-    <EdsProvider density={density}>
-      <Button type="button" onClick={() => setOpen(true)}>
-        Show a simple compact snackbar
-      </Button>
-      <Snackbar
-        open={open}
-        onClose={() => setOpen(false)}
-        autoHideDuration={5000}
-      >
-        Message goes here
-      </Snackbar>
-    </EdsProvider>
+    <Stack>
+      <EdsProvider density={density}>
+        <Button type="button" onClick={() => setOpen(true)}>
+          Show a simple compact snackbar
+        </Button>
+        <Snackbar
+          open={open}
+          onClose={() => setOpen(false)}
+          autoHideDuration={5000}
+        >
+          Message goes here
+        </Snackbar>
+      </EdsProvider>
+    </Stack>
   )
-}
-
-Compact.parameters = {
-  docs: {
-    description: {
-      story: 'Compact `Snackbar` using `EdsProvider` ',
-    },
-  },
 }
