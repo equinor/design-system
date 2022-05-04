26ed1e23a07d1c1efcdd709e51fe5bc838fd03f1
oddvernes <oddbjorn.overnes@gmail.com>
2022-05-04 12:54:33 +0200

    📝♿️ Progress.Linear a11y story and updated Determinate story

--- packages/eds-core-react/src/components/Progress/Linear/Linear.stories.tsx
+++ packages/eds-core-react/src/components/Progress/Linear/Linear.stories.tsx
@@ -1,4 +1,5 @@
-import { Progress, LinearProgressProps } from '../../..'
+import { Progress, LinearProgressProps, Button } from '../../..'
+import { useState, useEffect, useRef } from 'react'
 import { ComponentMeta, Story } from '@storybook/react'
 import { useMockProgress } from '../../../stories'
 import styled from 'styled-components'
@@ -41,15 +42,86 @@ export const Indeterminate: Story<LinearProgressProps> = () => (
 )
 
 export const Determinate: Story<LinearProgressProps> = () => {
-  const progress = useMockProgress(0)
+  const [progress, setProgress] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  const timer = useRef<ReturnType<typeof setTimeout>>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    return () => {
+      if (timer.current) clearInterval(timer.current)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!isLoading && buttonRef.current && timer.current) {
+      buttonRef.current.focus()
+    }
+  }, [isLoading])
+
+  const resetProgress = () => {
+    setProgress(0)
+    setIsLoading(true)
+    timer.current = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === null) return null
+        if (oldProgress === 100) {
+          setIsLoading(false)
+          return 100
+        }
+        const diff = Math.random() * 10
+        return Math.min(oldProgress + diff, 100)
+      })
+    }, 1500)
+  }
 
   return (
-    <Stack>
-      <Progress.Linear
-        variant="determinate"
-        value={progress}
-        aria-label="Progress bar label"
-      />
+    <Stack aria-busy={isLoading} aria-live="polite">
+      {isLoading ? (
+        <Progress.Linear
+          variant="determinate"
+          aria-label="loading determinate linear progress test"
+          value={progress}
+        />
+      ) : (
+        <Button ref={buttonRef} onClick={resetProgress}>
+          Trigger progress
+        </Button>
+      )}
+    </Stack>
+  )
+}
+
+export const Accessibility: Story<LinearProgressProps> = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const timer = useRef<ReturnType<typeof setTimeout>>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    return () => {
+      if (timer.current) clearTimeout(timer.current)
+    }
+  }, [])
+
+  const resetProgress = () => {
+    setIsLoading(true)
+    timer.current = setTimeout(() => {
+      setIsLoading(false)
+    }, 6000)
+  }
+  return (
+    <Stack aria-busy={isLoading} aria-live="polite">
+      {isLoading ? (
+        <Progress.Linear aria-label="Loading linear accessibility test" />
+      ) : (
+        <Button
+          ref={buttonRef}
+          onClick={resetProgress}
+          aria-disabled={isLoading}
+        >
+          Click to load
+        </Button>
+      )}
     </Stack>
   )
 }
