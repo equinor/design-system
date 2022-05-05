367398356ff9a1a1d4c6f76d481c51a75de6d4c4
Laliq <marta.lalik@gmail.com>
2022-05-05 06:21:39 +0000

    Fixes & add Scrim

--- packages/eds-core-react/src/components/SideSheet/SideSheet.stories.tsx
+++ packages/eds-core-react/src/components/SideSheet/SideSheet.stories.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   Divider,
   Icon,
+  Scrim,
   Search,
   Slider,
   Table,
@@ -65,6 +66,17 @@ const Wrapper = styled.div`
   padding-top: 16px;
 `
 
+const SheetWrapper = styled.div`
+  overflow-y: auto;
+  max-height: 80%;
+`
+
+const UnstyledList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`
+
 export const Introduction: Story<SideSheetProps> = (args) => {
   const [toggle, setToggle] = useState(true)
   return (
@@ -81,7 +93,7 @@ export const Introduction: Story<SideSheetProps> = (args) => {
 
 export const Placement: Story<SideSheetProps> = () => {
   const cellValues = toCellValues(data, columns)
-  const [toggle, setToggle] = useState(false)
+  const [open, setOpen] = useState(false)
   const [value, updateValue] = useState([1, 50])
   const changeHandler = (
     event: FormEvent<HTMLDivElement>,
@@ -89,12 +101,6 @@ export const Placement: Story<SideSheetProps> = () => {
   ) => {
     updateValue(value as number[])
   }
-
-  const UnstyledList = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  `
 
   return (
     <Container>
@@ -111,7 +117,7 @@ export const Placement: Story<SideSheetProps> = () => {
           <Icons>
             <Icon name="account_circle" size={16} title="user" />
             <Icon name="notifications" size={16} />
-            <Button variant="ghost_icon" onClick={() => setToggle(!toggle)}>
+            <Button variant="ghost_icon" onClick={() => setOpen(!open)}>
               <Icon name="filter_list" size={16} />
             </Button>
           </Icons>
@@ -135,56 +141,57 @@ export const Placement: Story<SideSheetProps> = () => {
           ))}
         </Table.Body>
       </Table>
-      <SideSheet open={toggle} onClose={() => setToggle(!toggle)}>
-        <div style={{ padding: '6px' }}>
-          <Typography variant="h4">Filters</Typography>
-          <br />
-          <Typography variant="h6" color="disabled">
-            Labels
-          </Typography>
-          <UnstyledList>
-            <li>
-              <Checkbox
-                label="Fruits"
-                name="multiple"
-                value="first"
-                defaultChecked
-              />
-            </li>
-            <li>
-              <Checkbox label="Vegetables" name="multiple" value="second" />
-            </li>
-            <li>
-              <Checkbox label="Favourites" name="multiple" value="third" />
-            </li>
-          </UnstyledList>
-          <br />
-          <Typography id="range-slider-label" variant="h6" color="disabled">
-            Price range
-          </Typography>
-          <Slider
-            value={value}
-            onChange={changeHandler}
-            ariaLabelledby="range-slider-label"
-          />
-          <p style={{ marginTop: '1.5rem' }}>
-            <small>$: {value.join(', ')}</small>
-          </p>
-        </div>
-      </SideSheet>
+      <Scrim
+        open={open}
+        onClose={() => setOpen(!open)}
+        isDismissable
+        style={{ position: 'absolute' }}
+      >
+        <SideSheet open={open} onClose={() => setOpen(!open)}>
+          <div>
+            <Typography variant="h4">Filters</Typography>
+            <br />
+            <Typography variant="h6" color="disabled">
+              Labels
+            </Typography>
+            <UnstyledList>
+              <li>
+                <Checkbox
+                  label="Fruits"
+                  name="multiple"
+                  value="first"
+                  defaultChecked
+                />
+              </li>
+              <li>
+                <Checkbox label="Vegetables" name="multiple" value="second" />
+              </li>
+              <li>
+                <Checkbox label="Favourites" name="multiple" value="third" />
+              </li>
+            </UnstyledList>
+            <br />
+            <Typography id="range-slider-label" variant="h6" color="disabled">
+              Price range
+            </Typography>
+            <Slider
+              value={value}
+              onChange={changeHandler}
+              ariaLabelledby="range-slider-label"
+            />
+            <p style={{ marginTop: '1.5rem' }}>
+              <small>$: {value.join(', ')}</small>
+            </p>
+          </div>
+        </SideSheet>
+      </Scrim>
     </Container>
   )
 }
 
-export const Scrolling: Story<SideSheetProps> = () => {
+export const Scrollable: Story<SideSheetProps> = () => {
   const cellValues = toCellValues(data, columns)
-  const [toggle, setToggle] = useState(false)
-
-  const UnstyledList = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  `
+  const [open, setOpen] = useState(false)
 
   return (
     <Container>
@@ -220,7 +227,7 @@ export const Scrolling: Story<SideSheetProps> = () => {
                 <Table.Cell key={cellValue}>{cellValue}</Table.Cell>
               ))}
               <Table.Cell>
-                <Button variant="ghost_icon" onClick={() => setToggle(!toggle)}>
+                <Button variant="ghost_icon" onClick={() => setOpen(!open)}>
                   <Icon name="details" size={16} title="details" />
                 </Button>
               </Table.Cell>
@@ -228,50 +235,64 @@ export const Scrolling: Story<SideSheetProps> = () => {
           ))}
         </Table.Body>
       </Table>
-      <SideSheet
-        open={toggle}
-        onClose={() => setToggle(!toggle)}
-        style={{ overflowY: 'hidden' }}
+      <Scrim
+        open={open}
+        onClose={() => setOpen(!open)}
+        style={{ position: 'absolute' }}
       >
-        <div style={{ padding: '6px', overflowY: 'auto', maxHeight: '80%' }}>
-          <Typography variant="h4">Details</Typography>
-          <TextField
-            id="storybook-details"
-            placeholder="Add description"
-            inputIcon={<Icon name="edit" title="Add description" />}
-            style={{ paddingTop: '16px' }}
-          ></TextField>
-          <Divider />
-          <Typography variant="h6" color="disabled">
-            Information
-          </Typography>
-          <Wrapper>
-            <Icon name="place" title="place" color="#007079" />
-            <Typography>place</Typography>
-            <Icon name="calendar_date_range" title="calendar" color="#007079" />
-            <Typography>date</Typography>
-            <Icon name="label" title="label" color="#007079" />
-            <Typography>labels</Typography>
-            <Icon name="file_description" title="description" color="#007079" />
-            <Typography>description</Typography>
-          </Wrapper>
-          <Divider />
-          <Typography variant="h6" color="disabled">
-            Add labels
-          </Typography>
-          <UnstyledList>
-            <li>
-              <Checkbox label="Fruits" name="multiple" />
-            </li>
-            <li>
-              <Checkbox label="Vegetables" name="multiple" />
-            </li>
-            <li>
-              <Checkbox label="Favourites" name="multiple" />
-            </li>
-          </UnstyledList>
-        </div>
-      </SideSheet>
+        <SideSheet
+          open={open}
+          onClose={() => setOpen(!open)}
+          style={{ overflowY: 'hidden' }}
+        >
+          <SheetWrapper>
+            <Typography variant="h4">Details</Typography>
+            <TextField
+              id="storybook-details"
+              placeholder="Add description"
+              inputIcon={<Icon name="edit" title="Add description" />}
+              style={{ paddingTop: '16px' }}
+            />
+            <Divider />
+            <Typography variant="h6" color="disabled">
+              Information
+            </Typography>
+            <Wrapper>
+              <Icon name="place" title="place" color="#007079" />
+              <Typography>place</Typography>
+              <Icon
+                name="calendar_date_range"
+                title="calendar"
+                color="#007079"
+              />
+              <Typography>date</Typography>
+              <Icon name="label" title="label" color="#007079" />
+              <Typography>labels</Typography>
+              <Icon
+                name="file_description"
+                title="description"
+                color="#007079"
+              />
+              <Typography>description</Typography>
+            </Wrapper>
+            <Divider />
+            <Typography variant="h6" color="disabled">
+              Add labels
+            </Typography>
+            <UnstyledList>
+              <li>
+                <Checkbox label="Fruits" name="multiple" />
+              </li>
+              <li>
+                <Checkbox label="Vegetables" name="multiple" />
+              </li>
+              <li>
+                <Checkbox label="Favourites" name="multiple" />
+              </li>
+            </UnstyledList>
+          </SheetWrapper>
+        </SideSheet>
+      </Scrim>
     </Container>
   )
 }
