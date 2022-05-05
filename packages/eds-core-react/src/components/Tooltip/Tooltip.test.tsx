946fdfad501cfb4cba7d39832d882f932343ae1c
mimarz <mimarz@gmail.com>
2022-05-05 16:11:17 +0200

    ✅ uncomment testing tests

--- packages/eds-core-react/src/components/Tooltip/Tooltip.test.tsx
+++ packages/eds-core-react/src/components/Tooltip/Tooltip.test.tsx
@@ -17,8 +17,6 @@ const StyledTooltip = styled(Tooltip)`
   background: red;
 `
 
-const enterDelayDefault = 100
-
 afterEach(cleanup)
 
 describe('Tooltip', () => {
@@ -52,97 +50,97 @@ describe('Tooltip', () => {
 
     expect(results).toHaveNoViolations()
   })
-  // it('Should pass a11y test with id & placement', async () => {
-  //   const { container } = render(
-  //     <Tooltip title="Tooltip" id="a11y-tooltip" placement="top" enterDelay={0}>
-  //       <span>Test</span>
-  //     </Tooltip>,
-  //   )
+  it('Should pass a11y test with id & placement', async () => {
+    const { container } = render(
+      <Tooltip title="Tooltip" id="a11y-tooltip" placement="top" enterDelay={0}>
+        <span>Test</span>
+      </Tooltip>,
+    )
 
-  //   const content = screen.getByText('Test')
-  //   fireEvent.mouseOver(content)
+    const content = screen.getByText('Test')
+    fireEvent.mouseOver(content)
 
-  //   const results = await axe(container)
+    const results = await axe(container)
 
-  //   expect(results).toHaveNoViolations()
-  // })
-  // it('can extend the css for the component', async () => {
-  //   render(
-  //     <StyledTooltip title="Tooltip">
-  //       <span>Test</span>
-  //     </StyledTooltip>,
-  //   )
+    expect(results).toHaveNoViolations()
+  })
+  it('can extend the css for the component', async () => {
+    render(
+      <StyledTooltip title="Tooltip">
+        <span>Test</span>
+      </StyledTooltip>,
+    )
 
-  //   const content = screen.getByText('Test')
+    const content = screen.getByText('Test')
 
-  //   fireEvent.mouseOver(content)
+    fireEvent.mouseOver(content)
 
-  //   const tooltip = await screen.findByRole('tooltip')
-  //   expect(tooltip).toHaveStyleRule('background', 'red')
-  // })
-  // it('is visible when content is being hovered', async () => {
-  //   render(
-  //     <Tooltip title="Tooltip" placement="right-start">
-  //       <div>Test</div>
-  //     </Tooltip>,
-  //   )
+    const tooltip = await screen.findByRole('tooltip')
+    expect(tooltip).toHaveStyleRule('background', 'red')
+  })
+  it('is visible when content is being hovered', async () => {
+    render(
+      <Tooltip title="Tooltip" placement="right-start">
+        <div>Test</div>
+      </Tooltip>,
+    )
 
-  //   const content = screen.getByText('Test')
+    const content = screen.getByText('Test')
 
-  //   fireEvent.mouseOver(content)
+    fireEvent.mouseOver(content)
 
-  //   const tooltip = await screen.findByRole('tooltip')
-  //   expect(tooltip).toHaveAttribute('data-popper-placement', 'right-start')
-  // })
-  // it('renders with a correct title', async () => {
-  //   render(
-  //     <Tooltip title="Tooltip" placement="right-start">
-  //       <span>Test</span>
-  //     </Tooltip>,
-  //   )
+    const tooltip = await screen.findByRole('tooltip')
+    expect(tooltip).toHaveAttribute('data-popper-placement', 'right-start')
+  })
+  it('renders with a correct title', async () => {
+    render(
+      <Tooltip title="Tooltip" placement="right-start">
+        <span>Test</span>
+      </Tooltip>,
+    )
 
-  //   const content = screen.getByText('Test')
+    const content = screen.getByText('Test')
 
-  //   fireEvent.mouseOver(content)
+    fireEvent.mouseOver(content)
 
-  //   const tooltip = await screen.findByRole('tooltip')
+    const tooltip = await screen.findByRole('tooltip')
 
-  //   expect(content).toBeDefined()
-  //   expect(tooltip).toBeDefined()
-  // })
-  // it('renders on focus', async () => {
-  //   render(
-  //     <Tooltip title="Tooltip" placement="right-start">
-  //       <span>Test</span>
-  //     </Tooltip>,
-  //   )
+    expect(content).toBeDefined()
+    expect(tooltip).toBeDefined()
+  })
+  it('renders on focus', async () => {
+    render(
+      <Tooltip title="Tooltip" placement="right-start">
+        <span>Test</span>
+      </Tooltip>,
+    )
 
-  //   const content = screen.getByText('Test')
+    const content = screen.getByText('Test')
 
-  //   fireEvent.focusIn(content)
-  //   expect(await screen.findByRole('tooltip')).toBeDefined()
-  // })
-  // it('shows after correct delay', async () => {
-  //   render(
-  //     <Tooltip title="Tooltip" placement="right-start" enterDelay={500}>
-  //       <span>Test</span>
-  //     </Tooltip>,
-  //   )
+    fireEvent.focusIn(content)
+    expect(await screen.findByRole('tooltip')).toBeDefined()
+  })
+  it('shows after correct delay', async () => {
+    render(
+      <Tooltip title="Tooltip" placement="right-start" enterDelay={500}>
+        <span>Test</span>
+      </Tooltip>,
+    )
 
-  //   const content = screen.getByText('Test')
+    const content = screen.getByText('Test')
 
-  //   fireEvent.mouseOver(content)
+    fireEvent.mouseOver(content)
 
-  //   await act(() => new Promise((r) => setTimeout(r, 200)))
-  //   expect(screen.queryByText('Tooltip')).not.toBeInTheDocument()
+    await act(() => new Promise((r) => setTimeout(r, 200)))
+    expect(screen.queryByText('Tooltip')).not.toBeInTheDocument()
 
-  //   await act(() => new Promise((r) => setTimeout(r, 300)))
+    await act(() => new Promise((r) => setTimeout(r, 300)))
 
-  //   const tooltip = await screen.findByRole('tooltip')
+    const tooltip = await screen.findByRole('tooltip')
 
-  //   expect(content).toBeDefined()
-  //   expect(tooltip).toBeDefined()
-  // })
+    expect(content).toBeDefined()
+    expect(tooltip).toBeDefined()
+  })
   it('child onFocus is called when focusd', () => {
     const handler = jest.fn()
     render(
