76cd8ec9ba308d34e1e1f81c21cce3e6a87b805b
oddvernes <oddbjorn.overnes@gmail.com>
2022-05-31 14:33:43 +0200

    remove footer padding

--- packages/eds-lab-react/src/components/SideBar/SideBarFooter.tsx
+++ packages/eds-lab-react/src/components/SideBar/SideBarFooter.tsx
@@ -7,12 +7,6 @@ const StyledSideBarFooter = styled.div(({ theme }) => {
   return css`
     grid-area: footer;
     padding: 0 ${theme.spacings.right} 0 ${theme.spacings.left};
-    :last-child {
-      padding-bottom: ${theme.spacings.bottom};
-    }
-    :first-child {
-      padding-top: ${theme.spacings.top};
-    }
   `
 })
 
