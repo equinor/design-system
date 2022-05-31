c1fc22aabc797699a806e235def19b2dafa94e4a
Laliq <105340174+martalalik@users.noreply.github.com>
2022-05-31 15:53:53 +0200

    📝 Avatar documentation  (#2251)
    * 📝 Add docs file
    
    * 📝 Add Introduction
    
    * 📝 Add Usage
    
    * 📝  Add Examples
    
    * 📝  Update Examples description

--- packages/eds-core-react/src/components/Avatar/Avatar.stories.tsx
+++ packages/eds-core-react/src/components/Avatar/Avatar.stories.tsx
@@ -1,47 +1,155 @@
-import styled from 'styled-components'
-import { Story, Meta } from '@storybook/react'
-import { Avatar, AvatarProps, Typography } from '../..'
-
-const Container = styled.div`
-  margin: 32px;
-  display: grid;
-  grid-gap: 16px;
-`
-const Wrapper = styled.div`
-  display: grid;
-  grid-gap: 32px;
-  grid-template-columns: repeat(5, fit-content(100%));
-`
+import { Story, ComponentMeta } from '@storybook/react'
+import {
+  Avatar,
+  AvatarProps,
+  Button,
+  Card,
+  Divider,
+  Icon,
+  Typography,
+} from '../..'
+import { Stack } from './../../../.storybook/components'
+import page from './Avatar.docs.mdx'
 
 export default {
   title: 'Data Display/Avatar',
   component: Avatar,
   parameters: {
     docs: {
-      description: {
-        component: `An avatar is a small (user) picture. The avatar does not exist in Figma as a separate component.`,
-      },
+      page,
     },
   },
-} as Meta
+} as ComponentMeta<typeof Avatar>
 
-export const Default: Story<AvatarProps> = (args) => (
-  <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} {...args} />
+const iconSize = 24
+
+export const Introduction: Story<AvatarProps> = (args) => (
+  <Stack>
+    <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} {...args} />
+  </Stack>
 )
 
-export const Examples: Story<AvatarProps> = () => (
-  <Container>
-    <Typography variant="h2">Sizes</Typography>
-    <Wrapper>
-      <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={16} alt="avatar" />
-      <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={24} alt="avatar" />
-      <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={32} alt="avatar" />
-      <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={40} alt="avatar" />
-      <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={48} alt="avatar" />
-    </Wrapper>
-    <Typography variant="h2">Disabled</Typography>
-    <Wrapper>
-      <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} disabled alt="avatar" />
-    </Wrapper>
-  </Container>
+export const Size: Story<AvatarProps> = () => (
+  <Stack>
+    <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={16} alt="avatar" />
+    <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={24} alt="avatar" />
+    <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={32} alt="avatar" />
+    <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={40} alt="avatar" />
+    <Avatar src={'https://i.imgur.com/UM3mrju.jpg'} size={48} alt="avatar" />
+  </Stack>
+)
+
+export const Disabled: Story<AvatarProps> = () => (
+  <Stack>
+    <Avatar
+      src={'https://i.imgur.com/UM3mrju.jpg'}
+      disabled
+      size={48}
+      alt="avatar"
+    />
+  </Stack>
+)
+
+export const Context: Story<AvatarProps> = () => (
+  <>
+    <Stack style={{ background: '#ebebeb', padding: '32px' }}>
+      <Card style={{ width: '50%' }}>
+        <Card.Header>
+          <Card.HeaderTitle>
+            <Typography variant="h4">ALL</Typography>
+          </Card.HeaderTitle>
+        </Card.Header>
+        <Card.Header>
+          <Avatar
+            alt="Kitten"
+            src="https://i.imgur.com/UM3mrju.jpg"
+            size={40}
+          />
+          <Card.HeaderTitle>
+            <Typography variant="h5">Title goes here</Typography>
+            <Typography variant="body_short">
+              Ut enim ad minim veniam, quis nostrud exercitation.
+            </Typography>
+            <Divider style={{ width: '100%' }} variant="small" />
+          </Card.HeaderTitle>
+        </Card.Header>
+        <Card.Header>
+          <Avatar
+            alt="Kitten"
+            src="https://i.imgur.com/UM3mrju.jpg"
+            size={40}
+          />
+          <Card.HeaderTitle>
+            <Typography variant="h5">Title goes here</Typography>
+            <Typography variant="body_short">
+              Ut enim ad minim veniam, quis nostrud exercitation.
+            </Typography>
+            <Divider style={{ width: '100%' }} variant="small" />
+          </Card.HeaderTitle>
+        </Card.Header>
+        <Card.Header>
+          <Avatar
+            alt="Kitten"
+            src="https://i.imgur.com/UM3mrju.jpg"
+            size={40}
+          />
+          <Card.HeaderTitle>
+            <Typography variant="h5">Title goes here</Typography>
+            <Typography variant="body_short">
+              Ut enim ad minim veniam, quis nostrud exercitation.
+            </Typography>
+          </Card.HeaderTitle>
+        </Card.Header>
+      </Card>
+    </Stack>
+
+    <Stack style={{ background: '#ebebeb', padding: '32px' }}>
+      <Card variant="info">
+        <Card.Header>
+          <Card.HeaderTitle>
+            <Typography variant="h5">Title goes here</Typography>
+            <Typography variant="body_short">Body short</Typography>
+          </Card.HeaderTitle>
+          <Avatar
+            alt="Kitten"
+            src="https://i.imgur.com/UM3mrju.jpg"
+            size={40}
+          />
+        </Card.Header>
+      </Card>
+      <Card variant="warning">
+        <Card.Header>
+          <Avatar
+            alt="Kitten"
+            src="https://i.imgur.com/UM3mrju.jpg"
+            size={40}
+          />
+          <Card.HeaderTitle>
+            <Typography variant="h5">Title goes here</Typography>
+            <Typography variant="caption">Caption</Typography>
+          </Card.HeaderTitle>
+        </Card.Header>
+      </Card>
+      <Card variant="danger">
+        <Card.Header>
+          <Avatar
+            alt="Kitten"
+            src="https://i.imgur.com/UM3mrju.jpg"
+            size={40}
+          />
+          <Card.HeaderTitle>
+            <Typography variant="h5">Title goes here</Typography>
+            <Typography variant="caption">Caption</Typography>
+          </Card.HeaderTitle>
+          <Button variant="ghost_icon">
+            <Icon
+              name="more_vertical"
+              title="more action"
+              size={iconSize}
+            ></Icon>
+          </Button>
+        </Card.Header>
+      </Card>
+    </Stack>
+  </>
 )
