fd69e65c1b38ec2a7d8f494f7227e281d5f0c40e
Laliq <marta.lalik@gmail.com>
2022-06-03 08:34:59 +0200

    📝 Slider documentation (#2161)
    * 📝 Add docs file
    
    * 📝 Add Introduction
    
    * 📝 Add Usage
    
    * 📝 Add Examples
    
    * 🏷️  Type fix & add aria-labelledby
    
    * 📝 Change ARIA link
    
    * 📝  Update Aria link
    
    * Add Aria
    
    Co-authored-by: martalalik <lala.czaki@gmail.com>

--- packages/eds-core-react/src/components/Slider/Slider.stories.tsx
+++ packages/eds-core-react/src/components/Slider/Slider.stories.tsx
@@ -1,33 +1,71 @@
-import { useState, FormEvent } from 'react'
+import { useState, ChangeEvent } from 'react'
 import { Slider, SliderProps } from '../..'
-import { Story, Meta } from '@storybook/react'
+import { Story, ComponentMeta } from '@storybook/react'
+import page from './Slider.docs.mdx'
 
 export default {
   title: 'Inputs/Slider',
   component: Slider,
   parameters: {
     docs: {
-      description: {
-        component: `A slider is a flexible input that allows users to adjust
-        predefined values.
-        `,
-      },
+      page,
     },
   },
-} as Meta
+} as ComponentMeta<typeof Slider>
 
-export const Default: Story<SliderProps> = (args) => {
+export const Introduction: Story<SliderProps> = (args) => {
   return (
     <div style={{ margin: '3rem' }}>
-      <Slider {...args} />
+      <Slider aria-label="simple-slider" {...args} />
     </div>
   )
 }
 
+export const SimpleSlider: Story<SliderProps> = () => (
+  <>
+    <span id="simple-slider">Slide me</span>
+    <Slider value={4} min={0} max={10} aria-labelledby="simple-slider" />
+  </>
+)
+SimpleSlider.storyName = 'Simple slider'
+SimpleSlider.decorators = [
+  (Story) => (
+    <div style={{ margin: '3rem' }}>
+      <Story />
+    </div>
+  ),
+]
+
+export const SimpleSliderWithSteps: Story<SliderProps> = () => {
+  return (
+    <>
+      <span id="even-simpler-slider">
+        Simple slider, no dots, no min or max values, steps of 10
+      </span>
+      <Slider
+        aria-labelledby="even-simpler-slider"
+        value={50}
+        step={10}
+        minMaxDots={false}
+        minMaxValues={false}
+      />
+    </>
+  )
+}
+
+SimpleSliderWithSteps.storyName = 'Simple slider with steps'
+SimpleSliderWithSteps.decorators = [
+  (Story) => (
+    <div style={{ margin: '3rem' }}>
+      <Story />
+    </div>
+  ),
+]
+
 export const RangeSlider: Story<SliderProps> = () => {
   const [value, updateValue] = useState([30, 70])
   const changeHandler = (
-    event: FormEvent<HTMLDivElement>,
+    event: ChangeEvent<HTMLInputElement>,
     value: number[] | number,
   ) => {
     updateValue(value as number[])
@@ -39,7 +77,7 @@ export const RangeSlider: Story<SliderProps> = () => {
       <Slider
         value={value}
         onChange={changeHandler}
-        ariaLabelledby="range-slider-label"
+        aria-labelledby="range-slider-label"
       />
       <p style={{ marginTop: '1.5rem' }}>
         <small>Output from slider is {value.join(', ')}</small>
@@ -68,14 +106,14 @@ export const RangeSliderWithCommittedStep: Story<SliderProps> = () => {
       <Slider
         value={value}
         onChange={(
-          event: FormEvent<HTMLDivElement>,
+          event: ChangeEvent<HTMLInputElement>,
           value: number[] | number,
         ) => {
           updateValue(value as number[])
         }}
         min={0}
         max={500}
-        ariaLabelledby="range-slider-label-with-committed"
+        aria-labelledby="range-slider-label-with-committed"
         onChangeCommitted={(event, value) => {
           updateValueCommited(value as number[])
         }}
@@ -102,7 +140,7 @@ export const RangeSliderWithInterval: Story<SliderProps> = () => (
   <>
     <span id="large-step-range-slider">Range slider with steps of 5</span>
     <Slider
-      ariaLabelledby="large-step-range-slider"
+      aria-labelledby="large-step-range-slider"
       step={5}
       min={30}
       minMaxDots={false}
@@ -139,7 +177,7 @@ export const RangeSliderWithDates: Story<SliderProps> = () => {
       <Slider
         min={getUnixTime('2020-01-01')}
         max={getUnixTime('2020-01-31')}
-        ariaLabelledby="date-range-slider"
+        aria-labelledby="date-range-slider"
         step={60 * 60 * 24 * 1000}
         value={[getUnixTime('2020-01-01'), getUnixTime('2020-01-31')]}
         outputFunction={outputFunction}
@@ -157,47 +195,6 @@ RangeSliderWithDates.decorators = [
   ),
 ]
 
-export const SimpleSlider: Story<SliderProps> = () => (
-  <>
-    <span id="simple-slider">Slide me</span>
-    <Slider value={4} min={0} max={10} ariaLabelledby="simple-slider" />
-  </>
-)
-SimpleSlider.storyName = 'Simple slider'
-SimpleSlider.decorators = [
-  (Story) => (
-    <div style={{ margin: '3rem' }}>
-      <Story />
-    </div>
-  ),
-]
-
-export const SimpleSliderWithSteps: Story<SliderProps> = () => {
-  return (
-    <>
-      <span id="even-simpler-slider">
-        Simple slider, no dots, no min or max values, steps of 10
-      </span>
-      <Slider
-        ariaLabelledby="even-simpler-slider"
-        value={50}
-        step={10}
-        minMaxDots={false}
-        minMaxValues={false}
-      />
-    </>
-  )
-}
-
-SimpleSliderWithSteps.storyName = 'Simple slider with steps'
-SimpleSliderWithSteps.decorators = [
-  (Story) => (
-    <div style={{ margin: '3rem' }}>
-      <Story />
-    </div>
-  ),
-]
-
 export const Disabled: Story<SliderProps> = () => (
   <>
     <label htmlFor="disabled-slider">Disabled slider</label>
@@ -205,7 +202,7 @@ export const Disabled: Story<SliderProps> = () => (
       id="disabled-slider"
       value={50}
       disabled
-      ariaLabelledby="disabled-slider"
+      aria-labelledby="disabled-slider"
     />
   </>
 )
