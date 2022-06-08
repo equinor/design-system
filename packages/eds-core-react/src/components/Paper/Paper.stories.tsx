b131f1ecca56deb64edc3881c5c217bad837bd02
martalalik <lala.czaki@gmail.com>
2022-06-08 15:03:47 +0200

    📝 Add elevation overview

--- packages/eds-core-react/src/components/Paper/Paper.stories.tsx
+++ packages/eds-core-react/src/components/Paper/Paper.stories.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Paper, PaperProps } from '../..'
+import { Paper, PaperProps, Typography } from '../..'
 import { Story, ComponentMeta } from '@storybook/react'
 import { Stack as SBStack } from './../../../.storybook/components'
 import page from './Paper.docs.mdx'
@@ -23,6 +23,11 @@ const Wrapper = styled(Paper)`
   height: 150px;
   width: 150px;
 `
+const WrapperOverview = styled(Paper)`
+  height: 100px;
+  width: 50px;
+  padding: 10px;
+`
 
 export const Introduction: Story<PaperProps> = (args) => (
   <Stack>
@@ -31,3 +36,39 @@ export const Introduction: Story<PaperProps> = (args) => (
     </Wrapper>
   </Stack>
 )
+
+export const ElevationOverview: Story<PaperProps> = () => (
+  <Stack>
+    <WrapperOverview elevation="none">
+      <Typography variant="overline">none</Typography>
+      <Typography variant="body_short_bold">0.0px 0.0px 1.0px 0px</Typography>
+      <Paper elevation="none" />
+    </WrapperOverview>
+    <WrapperOverview elevation="raised">
+      <Typography variant="overline">raised</Typography>
+      <Typography variant="body_short_bold">0.0px 1.0px 5.0px 0px</Typography>
+      <Paper elevation="raised" />
+    </WrapperOverview>
+    <WrapperOverview elevation="overlay">
+      <Typography variant="overline">overlay</Typography>
+      <Typography variant="body_short_bold">0.0px 1.0px 10.0px 0px</Typography>
+      <Paper elevation="overlay" />
+    </WrapperOverview>
+    <WrapperOverview elevation="sticky">
+      <Typography variant="overline">sticky</Typography>
+      <Typography variant="body_short_bold">0.0px 4.0px 5.0px 0px</Typography>
+      <Paper elevation="sticky" />
+    </WrapperOverview>
+    <WrapperOverview elevation="temporary_nav">
+      <Typography variant="overline">temporary_nav</Typography>
+      <Typography variant="body_short_bold">0.0px 7.0px 8.0px 0px</Typography>
+      <Paper elevation="temporary_nav" />
+    </WrapperOverview>
+    <WrapperOverview elevation="above_scrim">
+      <Typography variant="overline">above_scrim</Typography>
+      <Typography variant="body_short_bold">0.0px 11.0px 15.0px 0px</Typography>
+      <Paper elevation="above_scrim" />
+    </WrapperOverview>
+  </Stack>
+)
+ElevationOverview.storyName = 'Elevation overview'
