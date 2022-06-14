789eb435370a3dc5bd7f7a5076b8acbacfe37d1c
martalalik <lala.czaki@gmail.com>
2022-06-14 16:33:03 +0200

    📝 Update elevation overview

--- packages/eds-core-react/src/components/Paper/Paper.stories.tsx
+++ packages/eds-core-react/src/components/Paper/Paper.stories.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Divider, Paper, PaperProps, Typography } from '../..'
+import { Paper, PaperProps, Typography } from '../..'
 import { Story, ComponentMeta } from '@storybook/react'
 import { Stack as SBStack } from './../../../.storybook/components'
 import page from './Paper.docs.mdx'
@@ -24,8 +24,8 @@ const Wrapper = styled(Paper)`
   width: 150px;
 `
 const WrapperOverview = styled(Paper)`
-  height: 150px;
-  width: 50px;
+  height: 100px;
+  width: 110px;
   padding: 10px;
 `
 
@@ -41,58 +41,26 @@ export const ElevationOverview: Story<PaperProps> = () => (
   <Stack>
     <WrapperOverview elevation="none">
       <Typography variant="overline">none</Typography>
-      <Divider />
-      <Typography variant="caption">0.0px 0.0px 1.0px 0px</Typography>
       <Paper elevation="none" />
     </WrapperOverview>
     <WrapperOverview elevation="raised">
       <Typography variant="overline">raised</Typography>
-      <Divider />
-      <Typography variant="caption">0.0px 1.0px 5.0px 0px</Typography>
-      <Typography variant="caption" style={{ opacity: '0.8' }}>
-        0.0px 3.0px 4.0px 0px
-      </Typography>
-      <Typography variant="caption">0.0px 2.0px 4.0px 0px</Typography>
       <Paper elevation="raised" />
     </WrapperOverview>
     <WrapperOverview elevation="overlay">
       <Typography variant="overline">overlay</Typography>
-      <Divider />
-      <Typography variant="caption">0.0px 1.0px 10.0px 0px</Typography>
-      <Typography variant="caption" style={{ opacity: '0.8' }}>
-        0.0px 4.0px 5.0px 0px
-      </Typography>
-      <Typography variant="caption">0.0px 2.0px 4.0px 0px</Typography>
       <Paper elevation="overlay" />
     </WrapperOverview>
     <WrapperOverview elevation="sticky">
       <Typography variant="overline">sticky</Typography>
-      <Divider />
-      <Typography variant="caption">0.0px 4.0px 5.0px 0px</Typography>
-      <Typography variant="caption" style={{ opacity: '0.8' }}>
-        0.0px 3.0px 14.0px 0px
-      </Typography>
-      <Typography variant="caption">0.0px 8.0px 10.0px 0px</Typography>
       <Paper elevation="sticky" />
     </WrapperOverview>
     <WrapperOverview elevation="temporary_nav">
       <Typography variant="overline">temporary_nav</Typography>
-      <Divider />
-      <Typography variant="caption">0.0px 7.0px 8.0px 0px</Typography>
-      <Typography variant="caption" style={{ opacity: '0.8' }}>
-        0.0px 5.0px 22.0px 0px
-      </Typography>
-      <Typography variant="caption">0.0px 12.0px 17.0px 0px</Typography>
       <Paper elevation="temporary_nav" />
     </WrapperOverview>
     <WrapperOverview elevation="above_scrim">
       <Typography variant="overline">above_scrim</Typography>
-      <Divider />
-      <Typography variant="caption">0.0px 11.0px 15.0px 0px</Typography>
-      <Typography variant="caption" style={{ opacity: '0.8' }}>
-        0.0px 9.0px 46.0px 0px
-      </Typography>
-      <Typography variant="caption">0.0px 24.0px 38.0px 0px</Typography>
       <Paper elevation="above_scrim" />
     </WrapperOverview>
   </Stack>
