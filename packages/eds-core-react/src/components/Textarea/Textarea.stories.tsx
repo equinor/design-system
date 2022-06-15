cdda43b72fdf0767db208dd207e5cdb7a5568a01
Laliq <105340174+martalalik@users.noreply.github.com>
2022-06-15 15:48:33 +0200

    📝  Textarea documentation (#2293)
    * 📝  Add docs files
    
    * Remove styled wrapper from Input
    
    * 📝  Add Introduction
    
    * 📝  Add Usage
    
    * 📝 Update description

---
+++ packages/eds-core-react/src/components/Textarea/Textarea.stories.tsx
@@ -0,0 +1,18 @@
+import { Story } from '@storybook/react/types-6-0'
+import { ComponentMeta } from '@storybook/react'
+import { Textarea, TextareaProps } from '../Textarea'
+import page from './Textarea.docs.mdx'
+
+export default {
+  title: 'Inputs/Textarea',
+  component: Textarea,
+  parameters: {
+    docs: {
+      page,
+    },
+  },
+} as ComponentMeta<typeof Textarea>
+
+export const Introduction: Story<TextareaProps> = (args) => (
+  <Textarea {...args} />
+)
