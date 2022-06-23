7822a0533917aad29cc0adb2932e7ca03a2e485c
martalalik <lala.czaki@gmail.com>
2022-06-23 11:08:06 +0200

    🎨 List: add decorators

--- packages/eds-core-react/src/components/List/Lists.stories.tsx
+++ packages/eds-core-react/src/components/List/Lists.stories.tsx
@@ -1,5 +1,6 @@
 import { List, ListProps } from '../..'
 import { ComponentMeta, Story } from '@storybook/react'
+import { Stack } from './../../../.storybook/components'
 import page from './List.docs.mdx'
 
 const start = '15'
@@ -7,28 +8,42 @@ const start = '15'
 export default {
   title: 'Data Display/Lists',
   component: List,
+  subcomponents: { Item: List.Item },
   parameters: {
     docs: {
       page,
+      source: {
+        excludeDecorators: true,
+      },
     },
   },
-  subcomponents: { Item: List.Item },
+  decorators: [
+    (Story) => {
+      return (
+        <Stack>
+          <Story />
+        </Stack>
+      )
+    },
+  ],
 } as ComponentMeta<typeof List>
 
-export const Introduction: Story<ListProps> = (args) => (
-  <List {...args}>
-    <List.Item>List item</List.Item>
-    <List.Item>List item</List.Item>
-    <List.Item>
-      List item
-      <List {...args}>
-        <List.Item>List item</List.Item>
-        <List.Item>List item</List.Item>
-        <List.Item>List item</List.Item>
-      </List>
-    </List.Item>
-  </List>
-)
+export const Introduction: Story<ListProps> = (args) => {
+  return (
+    <List {...args}>
+      <List.Item>List item</List.Item>
+      <List.Item>List item</List.Item>
+      <List.Item>
+        List item
+        <List {...args}>
+          <List.Item>List item</List.Item>
+          <List.Item>List item</List.Item>
+          <List.Item>List item</List.Item>
+        </List>
+      </List.Item>
+    </List>
+  )
+}
 
 export const Unordered: Story<ListProps> = () => (
   <List variant="bullet">
