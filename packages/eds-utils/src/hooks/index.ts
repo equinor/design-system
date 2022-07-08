716c17b6c0c7f5049f77f92ac1df74ff63a8ddc0
oddvernes <oddbjorn.overnes@gmail.com>
2022-07-08 15:53:37 +0200

    remove export

--- packages/eds-utils/src/hooks/index.ts
+++ packages/eds-utils/src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useOutsideClick } from './useOutsideClick'
-export { useCombinedRefs } from './useCombinedRefs'
 export * from './usePopper'
 export { useGlobalKeyPress } from './useGlobalKeyPress'
 export { useId } from './useId'
