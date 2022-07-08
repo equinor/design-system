38467c303daf729c792a8f9f7e307067f4efaa2d
oddvernes <oddbjorn.overnes@gmail.com>
2022-07-08 15:38:09 +0200

    ✅Added mergeRef tests

---
+++ packages/eds-utils/src/utils/mergeRefs.test.tsx
@@ -0,0 +1,26 @@
+import { render, cleanup, screen } from '@testing-library/react'
+import { forwardRef, createRef } from 'react'
+import { mergeRefs } from './mergeRefs'
+
+const Component = forwardRef<HTMLDivElement>((props, ref) => {
+  const targetRef = mergeRefs<HTMLDivElement>(ref)
+  return <div {...props} ref={targetRef} />
+})
+Component.displayName = 'Component'
+afterEach(cleanup)
+
+describe('useCombinedRefs', () => {
+  it('calls function ref with correct payload', () => {
+    const callback = jest.fn()
+    render(<Component data-testid="component" ref={callback} />)
+
+    expect(callback).toHaveBeenCalledWith(screen.getByTestId('component'))
+  })
+
+  it('assigns ref object with correct payload', () => {
+    const elementRef = createRef<HTMLDivElement>()
+    render(<Component data-testid="component" ref={elementRef} />)
+
+    expect(elementRef.current).toBe(screen.getByTestId('component'))
+  })
+})
