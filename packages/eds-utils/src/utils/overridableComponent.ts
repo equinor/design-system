6a7d4fe7495c859c177fc2aefce4ddf6b4848de1
oddvernes <oddbjorn.overnes@gmail.com>
2022-08-11 13:47:21 +0200

    ✨ added OverridableComponent to utils

---
+++ packages/eds-utils/src/utils/overridableComponent.ts
@@ -0,0 +1,10 @@
+export type OverridableComponent<Component, Element extends HTMLElement> = {
+  (props: Component & React.RefAttributes<Element>): ReturnType<React.FC>
+
+  <As extends React.ElementType>(
+    props: {
+      as: As
+    } & Component &
+      Omit<React.ComponentPropsWithRef<As>, keyof Component>,
+  ): ReturnType<React.FC>
+}
