40e78dac28e8a61f5ad185fa4490fa5d9adfdfab
martalalik <lala.czaki@gmail.com>
2022-08-22 12:48:17 +0200

    🚧  Add SplitButton

--- packages/eds-core-react/src/components/Group/Group.stories.tsx
+++ packages/eds-core-react/src/components/Group/Group.stories.tsx
@@ -1,5 +1,14 @@
 import { useState, useEffect } from 'react'
-import { Button, Density, EdsProvider, Group, GroupProps } from '../..'
+import { action } from '@storybook/addon-actions'
+import {
+  Button,
+  Density,
+  EdsProvider,
+  Group,
+  GroupProps,
+  Icon,
+  Menu,
+} from '../..'
 import { Story, ComponentMeta } from '@storybook/react'
 import { Stack } from './../../../.storybook/components'
 import page from './Group.docs.mdx'
@@ -43,6 +52,64 @@ export const Vertical: Story<GroupProps> = () => (
     <Button variant="outlined">Outlined</Button>
   </Group>
 )
+
+export const Split: Story<GroupProps> = () => {
+  const options = ['Create task', 'Update task', 'Delete task']
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement>(null)
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const handleMenuItemClick = (event: React.MouseEvent, index: number) => {
+    action('click')(event)
+    event.stopPropagation()
+    setSelectedIndex(index)
+  }
+
+  const openMenu = () => {
+    setIsOpen(true)
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <Group aria-label="split button">
+      <Button ref={setAnchorEl} variant="outlined">
+        {options[selectedIndex]}
+      </Button>
+      <Button
+        variant="contained_icon"
+        aria-label="select task action"
+        aria-haspopup="true"
+        aria-controls="menu-default"
+        id="anchor-split"
+        onClick={() => (isOpen ? closeMenu() : openMenu())}
+      >
+        <Icon name="arrow_drop_down" title="arrow_down"></Icon>
+      </Button>
+      <Menu
+        open={isOpen}
+        id="menu-split"
+        aria-labelledby="anchor-split"
+        onClose={closeMenu}
+        anchorEl={anchorEl}
+      >
+        {options.map((option, index) => (
+          <Menu.Item
+            key={option}
+            disabled={index === 2}
+            onClick={(event: React.MouseEvent) =>
+              handleMenuItemClick(event, index)
+            }
+          >
+            {option}
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Group>
+  )
+}
 
 export const Compact: Story<GroupProps> = () => {
   const [density, setDensity] = useState<Density>('comfortable')
