edb4de3d9d26b004e3b6b307c12ecfb168e37a09
Laliq <105340174+martalalik@users.noreply.github.com>
2022-08-24 15:02:23 +0200

    📝   Add ButtonGroup to Storybook (#2420)
    * 📝 Update Usage
    
    * 📝 Add ButtonGroup
    
    * ✏️ Fix typo
    
    * 📝 Add Introduction
    
    * ✨ Add vertical prop & story
    
    * 🚧  Add SplitButton
    
    * ✨ Add role to Group
    
    * 💄 Fix menu btn position
    
    * 💄 Update group style
    
    * ♿️  Add aria-label & a11y description
    
    * ♿️ Update a11y description
    
    * 📝 Update component name from Group to ButtonGroup
    
    * 💄  Change display
    
    * 💄  Update display to flex-inline
    
    * 🎨  Move ButtonGroup under Button
    
    * ♿️  Add aria-label
    
    * ✏️ Fix typo

--- packages/eds-core-react/src/components/Button/Button.stories.tsx
+++ packages/eds-core-react/src/components/Button/Button.stories.tsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
+import { action } from '@storybook/addon-actions'
 import {
   Button,
-  Icon,
   ButtonProps,
+  ButtonGroupProps,
+  Icon,
   EdsProvider,
   Density,
+  Menu,
   Progress,
 } from '../..'
 import { Story, ComponentMeta } from '@storybook/react'
 import { menu, add, save } from '@equinor/eds-icons'
 import { Stack } from './../../../.storybook/components'
-// import { Group } from '../Group'
 import page from './Button.docs.mdx'
 
 export default {
@@ -254,30 +256,30 @@ export const FullWidth: Story<ButtonProps> = () => (
     <Button disabled fullWidth>
       Disabled
     </Button>
-    <Button fullWidth>
+    <Button fullWidth aria-label="save action">
       <Icon data={save}></Icon>Primary
     </Button>
-    <Button color="secondary" fullWidth>
+    <Button color="secondary" fullWidth aria-label="save action">
       <Icon data={save}></Icon>Secondary
     </Button>
-    <Button color="danger" fullWidth>
+    <Button color="danger" fullWidth aria-label="save action">
       <Icon data={save}></Icon>Danger
     </Button>
-    <Button disabled fullWidth>
+    <Button disabled fullWidth aria-label="save action">
       <Icon data={save}></Icon>Disabled
     </Button>
-    <Button fullWidth>
+    <Button fullWidth aria-label="save action">
       Primary <Icon data={save}></Icon>
     </Button>
-    <Button color="secondary" fullWidth>
+    <Button color="secondary" fullWidth aria-label="save action">
       Secondary
       <Icon data={save}></Icon>
     </Button>
-    <Button color="danger" fullWidth>
+    <Button color="danger" fullWidth aria-label="save action">
       Danger
       <Icon data={save}></Icon>
     </Button>
-    <Button disabled fullWidth>
+    <Button disabled fullWidth aria-label="save action">
       Disabled
       <Icon data={save}></Icon>
     </Button>
@@ -305,9 +307,15 @@ export const Compact: Story<ButtonProps> = () => {
       <Button>Contained</Button>
       <Button variant="outlined">Outlined</Button>
       <Button variant="ghost">Ghost</Button>
-      <Button variant="ghost_icon">
+      <Button variant="ghost_icon" aria-label="menu action">
         <Icon data={menu} title="Ghost icon menu"></Icon>
       </Button>
+      <Button.Group aria-label="button group compact">
+        <Button>Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <Button variant="outlined">Outlined</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Button.Group>
     </EdsProvider>
   )
 }
@@ -319,13 +327,98 @@ Compact.decorators = [
   ),
 ]
 
-// export const ButtonGroup: Story<ButtonProps> = () => (
-//   <Stack style={{ gridGap: 0 }}>
-//     <Group>
-//       <Button>Contained</Button>
-//       <Button variant="outlined">Outlined</Button>
-//       <Button variant="outlined">Outlined</Button>
-//       <Button variant="outlined">Outlined</Button>
-//     </Group>
-//   </Stack>
-// )
+export const GroupHorizontal: Story<ButtonGroupProps> = () => (
+  <Button.Group aria-label="button group">
+    <Button>Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Button variant="outlined">Outlined</Button>
+  </Button.Group>
+)
+GroupHorizontal.decorators = [
+  (Story) => (
+    <Stack>
+      <Story />
+    </Stack>
+  ),
+]
+
+export const GroupVertical: Story<ButtonGroupProps> = () => (
+  <Button.Group aria-label="vertical" vertical>
+    <Button>Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Button variant="outlined">Outlined</Button>
+  </Button.Group>
+)
+GroupVertical.decorators = [
+  (Story) => (
+    <Stack>
+      <Story />
+    </Stack>
+  ),
+]
+
+export const GroupSplit: Story<ButtonGroupProps> = () => {
+  const options = ['Create task', 'Update task', 'Delete task']
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement>(null)
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const handleMenuItemClick = (event: React.MouseEvent, index: number) => {
+    action('click')(event)
+    event.stopPropagation()
+    setSelectedIndex(index)
+  }
+
+  const openMenu = () => {
+    setIsOpen(true)
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <Button.Group aria-label="split button" style={{ gap: '1px' }}>
+      <Button>{options[selectedIndex]}</Button>
+      <Button
+        ref={setAnchorEl}
+        aria-label="select task action"
+        aria-haspopup="true"
+        aria-controls="menu-default"
+        id="anchor-split"
+        onClick={() => (isOpen ? closeMenu() : openMenu())}
+        style={{ padding: '0 4px' }}
+      >
+        <Icon name="arrow_drop_down" title="arrow_down"></Icon>
+      </Button>
+      <Menu
+        open={isOpen}
+        id="menu-split"
+        aria-labelledby="anchor-split"
+        onClose={closeMenu}
+        anchorEl={anchorEl}
+      >
+        {options.map((option, index) => (
+          <Menu.Item
+            key={option}
+            disabled={index === 2}
+            onClick={(event: React.MouseEvent) =>
+              handleMenuItemClick(event, index)
+            }
+          >
+            {option}
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Button.Group>
+  )
+}
+GroupSplit.decorators = [
+  (Story) => (
+    <Stack>
+      <Story />
+    </Stack>
+  ),
+]
---
+++ packages/eds-core-react/src/components/Button/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,68 @@
+import { forwardRef, HTMLAttributes } from 'react'
+import styled, { css } from 'styled-components'
+import { group as tokens } from './ButtonGroup.tokens'
+
+const { border } = tokens
+
+export type ButtonGroupProps = {
+  /** Display ButtonGroup vertically. */
+  vertical?: boolean
+} & HTMLAttributes<HTMLDivElement>
+
+const radius = border.type === 'border' && border.radius
+
+const ButtonGroupBase = styled.div<ButtonGroupProps>`
+  display: inline-flex;
+  > * {
+    border-radius: 0;
+    @media (hover: hover) and (pointer: fine) {
+      &:hover {
+        border-radius: 0;
+      }
+    }
+  }
+  ${({ vertical }) =>
+    vertical
+      ? css`
+          flex-direction: column;
+          > :first-child {
+            border-top-left-radius: ${radius};
+            border-top-right-radius: ${radius};
+          }
+          > :last-child {
+            border-bottom-left-radius: ${radius};
+            border-bottom-right-radius: ${radius};
+          }
+          > :not(:last-child) {
+            border-bottom: none;
+          }
+        `
+      : css`
+          > :first-child {
+            border-top-left-radius: ${radius};
+            border-bottom-left-radius: ${radius};
+          }
+          > :last-child {
+            border-top-right-radius: ${radius};
+            border-bottom-right-radius: ${radius};
+          }
+          > :not(:last-child) {
+            border-right: none;
+          }
+        `}
+`
+
+export const ButtonGroup = forwardRef<HTMLDivElement, ButtonGroupProps>(
+  function ButtonGroup({ children, vertical, ...rest }, ref) {
+    const props = {
+      ref,
+      vertical,
+      ...rest,
+    }
+    return (
+      <ButtonGroupBase role="group" {...props}>
+        {children}
+      </ButtonGroupBase>
+    )
+  },
+)
