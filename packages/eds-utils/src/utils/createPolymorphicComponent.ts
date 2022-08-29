19af7cb6ac7199565ec2ffb29977ec11bc0b9665
Michael Marszalek <mimarz@gmail.com>
2022-08-29 10:22:26 +0200

    🏷️  Added createPolymorphicComponent

---
+++ packages/eds-utils/src/utils/createPolymorphicComponent.ts
@@ -0,0 +1,58 @@
+import React from 'react'
+
+type ExtendedProps<
+  Props = Record<string, unknown>,
+  OverrideProps = Record<string, unknown>,
+> = OverrideProps & Omit<Props, keyof OverrideProps>
+
+type ElementType =
+  | keyof JSX.IntrinsicElements
+  | React.JSXElementConstructor<unknown>
+
+type PropsOf<C extends ElementType> = JSX.LibraryManagedAttributes<
+  C,
+  React.ComponentPropsWithoutRef<C>
+>
+
+type ComponentProp<C> = {
+  as?: C
+}
+
+type InheritedProps<
+  C extends ElementType,
+  Props = Record<string, unknown>,
+> = ExtendedProps<PropsOf<C>, Props>
+
+export type PolymorphicRef<C> = C extends React.ElementType
+  ? React.ComponentPropsWithRef<C>['ref']
+  : never
+
+export type PolymorphicComponentProps<
+  C,
+  Props = Record<string, unknown>,
+> = C extends React.ElementType
+  ? InheritedProps<C, Props & ComponentProp<C>> & { ref?: PolymorphicRef<C> }
+  : Props & { as: React.ElementType }
+
+export function createPolymorphicComponent<
+  ComponentDefaultType,
+  Props,
+  StaticComponents = Record<string, never>,
+>(component: unknown) {
+  type ComponentProps<C> = PolymorphicComponentProps<C, Props>
+
+  type _PolymorphicComponent = <C = ComponentDefaultType>(
+    props: ComponentProps<C>,
+  ) => React.ReactElement
+
+  type ComponentProperties = Omit<
+    React.FunctionComponent<ComponentProps<unknown>>,
+    never
+  >
+
+  type PolymorphicComponent = _PolymorphicComponent &
+    ComponentProperties &
+    StaticComponents
+
+  return component as PolymorphicComponent
+}
--- packages/eds-utils/src/utils/index.ts
+++ packages/eds-utils/src/utils/index.ts
@@ -3,5 +3,10 @@ export { joinHandlers } from './joinHandlers'
 export { mergeRefs } from './mergeRefs'
 export { setReactInputValue } from './setReactInputValue'
 export type { OverridableComponent } from './overridableComponent'
+export { createPolymorphicComponent } from './createPolymorphicComponent'
+export type {
+  PolymorphicComponentProps,
+  PolymorphicRef,
+} from './createPolymorphicComponent'
 
 export const trimSpaces = (text: string): string => text.replace(/ /g, '')
