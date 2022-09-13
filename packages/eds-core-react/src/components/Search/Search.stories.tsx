281cc09ceb202ea00c426b139bdeb4bdb33e21b0
Michael Marszalek <mimarz@gmail.com>
2022-09-13 09:56:53 +0200

    📝 Updated search docs

--- packages/eds-core-react/src/components/Search/Search.stories.tsx
+++ packages/eds-core-react/src/components/Search/Search.stories.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { action } from '@storybook/addon-actions'
-import styled from 'styled-components'
 import {
   Search,
   Typography,
@@ -11,29 +10,6 @@ import {
 } from '../..'
 import { Story, ComponentMeta } from '@storybook/react'
 import page from './Search.docs.mdx'
-
-const Columns = styled.div`
-  display: grid;
-  width: 100%;
-  grid-gap: 16px;
-  grid-auto-flow: column;
-  grid-auto-columns: max-content;
-`
-
-const Wrapper = styled.div`
-  background: lightblue;
-  padding: 8px;
-  width: 50%;
-  height: 100px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const StyledSearch = styled(Search)`
-  width: 50%;
-  margin-left: 32px;
-`
 
 export default {
   title: 'Inputs/Search',
@@ -46,8 +22,6 @@ export default {
 } as ComponentMeta<typeof Search>
 
 const handleOnChange = action('onChange')
-const handleOnBlur = action('onBlur')
-const handleOnFocus = action('onFocus')
 
 export const Introduction: Story<SearchProps> = () => {
   // This story is not interactive, because Search has no props beyond the default HTML ones.
@@ -61,80 +35,21 @@ export const Introduction: Story<SearchProps> = () => {
   )
 }
 
-export const WithOnFocusAndBlur: Story<SearchProps> = () => {
-  const [isFocused, setIsFocused] = useState(false)
-
-  const handleFocus = () => {
-    setIsFocused(true)
-    handleOnFocus()
-  }
-
-  const handleBlur = () => {
-    setIsFocused(false)
-    handleOnBlur()
-  }
-
-  return (
-    <div
-      style={{
-        background: isFocused ? 'cyan' : 'transparent',
-      }}
-    >
-      <Typography variant="h4" as="h2">
-        I am connected to the search input
-      </Typography>
-      <Search
-        aria-label="Focused and not focused"
-        id="search-focusedAndNot"
-        placeholder="Search"
-        onChange={handleOnChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-      />
-    </div>
-  )
-}
-WithOnFocusAndBlur.storyName = 'With on focus and blur'
-
-export const WithPredefinedValue: Story<SearchProps> = () => (
-  <Search
-    aria-label="predefined"
-    id="search-predefined"
-    placeholder="Search"
-    onChange={handleOnChange}
-    defaultValue="Predefined value"
-  />
-)
-WithPredefinedValue.storyName = 'With predefined value'
-
-export const CenteredAndStyled: Story<SearchProps> = () => (
-  <Wrapper>
-    50% width
-    <StyledSearch
-      aria-label="contained"
-      id="search-contained"
+export const accessibility: Story<SearchProps> = () => (
+  <form action="/">
+    <Search
       placeholder="Search"
+      aria-label="Search for example items"
       onChange={handleOnChange}
     />
-  </Wrapper>
-)
-CenteredAndStyled.storyName = 'Centered and styled'
-
-export const InsideAForm: Story<SearchProps> = () => (
-  <form action="/">
-    <Search placeholder="Search" onChange={handleOnChange} />
   </form>
 )
-InsideAForm.storyName = 'Inside a form element'
 
 export const Disabled: Story<SearchProps> = () => (
   <Search
     aria-label="disabled"
     id="search-disabled"
     placeholder="Search"
-    onChange={handleOnChange}
-    onFocus={handleOnFocus}
-    onBlur={handleOnBlur}
     disabled
   />
 )
@@ -152,24 +67,21 @@ export const Controlled: Story<SearchProps> = () => {
 
   return (
     <>
-      <Typography variant="body_short">Value: {searchValue}</Typography>
-      <Columns>
-        <Search
-          aria-label="external set value"
-          id="search-external"
-          placeholder="Say hello! 🙋"
-          onChange={handleOnSearchValueChange}
-          value={searchValue}
-        />
-        <Button
-          onClick={() => {
-            setSearchValue('Hello search! 👋')
-            action('Set search value')()
-          }}
-        >
-          Say hello to search!
-        </Button>
-      </Columns>
+      <Search
+        id="search-external"
+        aria-label="Search for Hello!"
+        placeholder="Say hello! 🙋"
+        onChange={handleOnSearchValueChange}
+        value={searchValue}
+      />
+      <Button
+        onClick={() => {
+          setSearchValue('Hello search! 👋')
+          action('Set search value')()
+        }}
+      >
+        Say hello to search!
+      </Button>
     </>
   )
 }
@@ -185,7 +97,7 @@ export const Compact: Story<SearchProps> = () => {
   return (
     <EdsProvider density={density}>
       <Search
-        aria-label="compact"
+        aria-label="compact search example"
         id="search-compact"
         placeholder="Search"
         onChange={handleOnChange}
