8751ff5928a5724717272c661d357243a2587944
Michael Marszalek <mimarz@gmail.com>
2022-09-14 14:31:02 +0200

    ♻️ Improve icons preview page in Storybook (#2486)
    * ⚰️ Removed dead code
    
    * 📝 Converted icons preview to mdx
    
    * 📝 Added link to EDS library from Icon component page

--- packages/eds-core-react/.storybook/preview.js
+++ packages/eds-core-react/.storybook/preview.js
@@ -1,15 +1,3 @@
-/* import { GlobalStyleDark } from '../stories/testing/GlobalStyles'
- */
-
-/* export const decorators = [
-  (Story) => (
-    <>
-      <GlobalStyleDark />
-      <Story />
-    </>
-  ),
-]
- */
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewMode: 'docs',
