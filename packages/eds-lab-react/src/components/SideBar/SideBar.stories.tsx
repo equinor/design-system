8b93b745a35a40c5eb8093f866585b1dba4fde39
oddvernes <oddbjorn.overnes@gmail.com>
2022-09-15 13:27:45 +0200

    position header inside grid

--- packages/eds-lab-react/src/components/SideBar/SideBar.stories.tsx
+++ packages/eds-lab-react/src/components/SideBar/SideBar.stories.tsx
@@ -231,6 +231,9 @@ export const WithTopbar: Story = () => {
   const SidebarContainerWithTopbar = styled(SidebarContainer)`
     display: grid;
     grid-template-rows: auto 1fr;
+    & > header {
+      position: static;
+    }
   `
 
   const menuItems: SidebarLinkProps[] = [
