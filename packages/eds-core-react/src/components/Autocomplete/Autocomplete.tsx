030f9dcde95bb5e45d1285ea86575d54f20e4ce0
oddvernes <oddbjorn.overnes@gmail.com>
2022-09-20 15:36:50 +0200

    🐛autocomplete: dropdown same z-index as menu

--- packages/eds-core-react/src/components/Autocomplete/Autocomplete.tsx
+++ packages/eds-core-react/src/components/Autocomplete/Autocomplete.tsx
@@ -542,7 +542,7 @@ function AutocompleteInner<T>(
           position: strategy,
           top: y ?? 0,
           left: x ?? 0,
-          zIndex: 1500,
+          zIndex: 1400,
         },
       })}
     >
