38888e1fcdc8750f7316d3bd321d0fd40c00242e
Oddbjørn Øvernes <oove@equinor.com>
2022-09-28 09:57:03 +0200

    ✨Divider thickness option (#2519)
    * ✨Divider: Added thickness prop
    
    * 📸 Update snapshot
    
    * 📸Banner: update snapshot
    
    * rename thickness to size

--- packages/eds-core-react/src/components/Divider/Divider.tsx
+++ packages/eds-core-react/src/components/Divider/Divider.tsx
@@ -1,35 +1,41 @@
 import { forwardRef, HTMLAttributes } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import * as tokens from './Divider.tokens'
 
-const { divider, baseDivider } = tokens
+const { divider } = tokens
 
 type StyleProps = {
   backgroundColor: string
   marginTop: string
   marginBottom: string
-  dividerHeight: string
+  dividerHeight: number
 }
 
-const StyledDivider = styled.hr<StyleProps>`
-  border: none;
-  background-color: ${(props) => props.backgroundColor};
-  margin-top: ${(props) => props.marginTop};
-  margin-bottom: ${(props) => props.marginBottom};
-  height: ${(props) => props.dividerHeight};
-`
+const StyledDivider = styled.hr<StyleProps>(
+  ({ backgroundColor, marginTop, marginBottom, dividerHeight }) => {
+    return css`
+      border: none;
+      background-color: ${backgroundColor};
+      margin-top: ${marginTop};
+      margin-bottom: calc(${marginBottom} - ${dividerHeight}px);
+      height: ${dividerHeight}px;
+    `
+  },
+)
 
 export type DividerProps = {
   /** Color variants */
   color?: 'lighter' | 'light' | 'medium'
   /** Vertical spacings */
   variant?: 'small' | 'medium'
+  /** Divider thickness in px*/
+  size?: '1' | '2'
   /** @ignore */
   className?: string
 } & HTMLAttributes<HTMLHRElement>
 
 export const Divider = forwardRef<HTMLHRElement, DividerProps>(function Divider(
-  { color = 'medium', variant = 'medium', ...rest },
+  { color = 'medium', variant = 'medium', size = '1', ...rest },
   ref,
 ) {
   const colorValue = color === 'medium' ? 'mediumColor' : color
@@ -38,7 +44,7 @@ export const Divider = forwardRef<HTMLHRElement, DividerProps>(function Divider(
     backgroundColor: divider[colorValue].background,
     marginTop: tokens[variant].spacings.top,
     marginBottom: tokens[variant].spacings.bottom,
-    dividerHeight: baseDivider.height,
+    dividerHeight: parseInt(size),
     ...rest,
   }
   return <StyledDivider {...props} ref={ref} />
