ae2ff8b4b119ef3ef5cd99115c8570dd1ed667e2
Michael Marszalek <mimarz@gmail.com>
2022-10-13 19:02:45 +0200

    🚨 Cleaned up some linting warnings

--- packages/eds-core-react/src/components/InputWrapper/InputWrapper.stories.tsx
+++ packages/eds-core-react/src/components/InputWrapper/InputWrapper.stories.tsx
@@ -1,23 +1,12 @@
 import { Story, ComponentMeta } from '@storybook/react'
-import { accessible, dropper, search } from '@equinor/eds-icons'
-import { Stack } from './../../../.storybook/components'
-import { InputWrapper, InputWrapperProps, Input, Icon, Button } from '../..'
-import styled from 'styled-components'
+import { InputWrapper, InputWrapperProps, Input } from '../..'
 
 export default {
   title: 'Inputs/InputWrapper',
   component: InputWrapper,
 } as ComponentMeta<typeof InputWrapper>
 
-const SmallButton = styled(Button)`
-  height: 24px;
-  width: 24px;
-`
-
 export const Introduction: Story<InputWrapperProps> = (args) => {
-  const { color } = args
-  const inputId = 'some-input-id'
-  const helperTextId = 'some-helper-id'
   const helperProps = {
     text: 'helperText',
   }
