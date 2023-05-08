32d1e9c8b48482b8484bb15c7640cf9515f860a9
Nina Andersen <denektenina@gmail.com>
2023-05-08 10:14:59 +0200

    ✅ Added tests for the new SideBar.Accordion and SideBar.AccordionItem

---
+++ packages/eds-core-react/src/components/SideBar/SideBarAccordion/SideBarAccordion.test.tsx
@@ -0,0 +1,170 @@
+import React from 'react'
+import { render, screen, cleanup, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { favorite_outlined } from '@equinor/eds-icons'
+import { SideBar } from '../.'
+import { SidebarAccordionProps } from './index'
+
+const defaultProps: SidebarAccordionProps = {
+  label: 'Test',
+  icon: favorite_outlined,
+}
+
+afterEach(cleanup)
+const mockResizeObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  disconnect: jest.fn(),
+  unobserve: jest.fn(),
+}))
+
+beforeAll(() => {
+  window.ResizeObserver = mockResizeObserver
+})
+
+function SideBarWrapper(children: React.ReactElement, isOpen?: boolean) {
+  return (
+    <SideBar open={isOpen}>
+      <SideBar.Content>{children}</SideBar.Content>
+    </SideBar>
+  )
+}
+
+describe('Accordion in open sidebar', () => {
+  it('Renders', () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, true),
+    })
+  })
+
+  it('Renders label', () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, true),
+    })
+
+    expect(screen.getByRole('button')).toHaveTextContent('Test')
+  })
+
+  it('Does not render tooltip', async () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, true),
+    })
+
+    const accordionButton = screen.getByRole('button')
+
+    await userEvent.hover(accordionButton)
+
+    await waitFor(() =>
+      expect(screen.queryByRole('tooltip')).not.toBeInTheDocument(),
+    )
+  })
+
+  it('If they are present, Sidebar.Accordion triggers toggleExpand- and onClick-callback when clicked', async () => {
+    const onClick = jest.fn()
+    const toggleExpand = jest.fn()
+
+    render(
+      <SideBar.Accordion
+        {...defaultProps}
+        isExpanded={false}
+        onClick={onClick}
+        toggleExpand={toggleExpand}
+      ></SideBar.Accordion>,
+      {
+        wrapper: ({ children }) => SideBarWrapper(children, true),
+      },
+    )
+
+    const accordionButton = screen.getByRole('button')
+    await userEvent.click(accordionButton)
+
+    expect(onClick).toHaveBeenCalled()
+    expect(toggleExpand).toHaveBeenCalled()
+  })
+
+  it('controls its own open-state when toggleExpand is not present', async () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, true),
+    })
+
+    const accordionButton = screen.getByRole('button')
+
+    await userEvent.click(accordionButton)
+
+    const accordionPanel = screen.getByRole('region')
+
+    expect(accordionButton).toHaveAttribute('aria-expanded', 'true')
+    expect(accordionPanel).toBeInTheDocument()
+  })
+})
+
+describe('Accordion in closed sidebar', () => {
+  it('Renders', () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, false),
+    })
+  })
+
+  it('Does not render label', () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, false),
+    })
+
+    expect(screen.getByRole('button')).not.toHaveTextContent('Test')
+  })
+
+  it('Renders tooltip on hover', async () => {
+    render(<SideBar.Accordion {...defaultProps}></SideBar.Accordion>, {
+      wrapper: ({ children }) => SideBarWrapper(children, false),
+    })
+
+    const accordionButton = screen.getByRole('button')
+
+    await userEvent.hover(accordionButton)
+    await screen.findByRole('tooltip')
+
+    expect(screen.getByRole('tooltip')).toHaveTextContent('Test')
+  })
+
+  it('If present, Sidebar.Accordion triggers only onClick callback when clicked', async () => {
+    const onClick = jest.fn()
+    const toggleExpand = jest.fn()
+
+    render(
+      <SideBar.Accordion
+        {...defaultProps}
+        isExpanded={false}
+        onClick={onClick}
+        toggleExpand={toggleExpand}
+      >
+        <SideBar.AccordionItem label={'Accordion item'} />
+      </SideBar.Accordion>,
+      {
+        wrapper: ({ children }) => SideBarWrapper(children, false),
+      },
+    )
+
+    const accordionButton = screen.getByRole('button')
+    await userEvent.click(accordionButton)
+
+    expect(onClick).toHaveBeenCalled()
+    expect(toggleExpand).not.toHaveBeenCalled()
+  })
+
+  it('Click on Sidebar.Accordion opens as a Menu', async () => {
+    render(
+      <SideBar.Accordion {...defaultProps}>
+        <SideBar.AccordionItem label={'Accordion item'} />
+      </SideBar.Accordion>,
+      {
+        wrapper: ({ children }) => SideBarWrapper(children, false),
+      },
+    )
+
+    const accordionButton = screen.getByRole('button')
+    await userEvent.click(accordionButton)
+
+    const accordionMenu = screen.getByRole('menu')
+
+    expect(accordionMenu).toBeInTheDocument()
+  })
+})
---
+++ packages/eds-core-react/src/components/SideBar/SideBarAccordionItem/SideBarAccordionItem.test.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { render, screen, cleanup, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { SideBar } from '../.'
+import { AccordionItemProps } from './index'
+import { favorite_outlined } from '@equinor/eds-icons'
+
+const defaultProps: AccordionItemProps = {
+  label: 'Accordion item',
+}
+
+afterEach(cleanup)
+const mockResizeObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  disconnect: jest.fn(),
+  unobserve: jest.fn(),
+}))
+
+beforeAll(() => {
+  window.ResizeObserver = mockResizeObserver
+})
+
+function SideBarWrapper(children: React.ReactElement, isOpen?: boolean) {
+  const toggleExpand = jest.fn()
+
+  return (
+    <SideBar open={isOpen}>
+      <SideBar.Content>
+        <SideBar.Accordion
+          label={'Accordion Header'}
+          icon={favorite_outlined}
+          isExpanded={true}
+          toggleExpand={toggleExpand}
+        >
+          {children}
+        </SideBar.Accordion>
+      </SideBar.Content>
+    </SideBar>
+  )
+}
+
+describe('AccordionItem in open sidebar', () => {
+  it('Renders', () => {
+    render(<SideBar.AccordionItem {...defaultProps}></SideBar.AccordionItem>, {
+      wrapper: ({ children }) => SideBarWrapper(children, true),
+    })
+  })
+
+  it('Renders label', () => {
+    render(
+      <SideBar.AccordionItem
+        {...defaultProps}
+        role={'link'}
+      ></SideBar.AccordionItem>,
+      {
+        wrapper: ({ children }) => SideBarWrapper(children, true),
+      },
+    )
+
+    expect(screen.getByRole('link')).toHaveTextContent('Accordion item')
+  })
+
+  it('Does not render tooltip', async () => {
+    render(
+      <SideBar.AccordionItem
+        {...defaultProps}
+        role={'link'}
+      ></SideBar.AccordionItem>,
+      {
+        wrapper: ({ children }) => SideBarWrapper(children, true),
+      },
+    )
+
+    const accordionItem = screen.getByRole('link')
+
+    await userEvent.hover(accordionItem)
+
+    await waitFor(() =>
+      expect(screen.queryByRole('tooltip')).not.toBeInTheDocument(),
+    )
+  })
+
+  it('If it is present, Sidebar.AccordionItem triggers onClick-callback when clicked', async () => {
+    const onClick = jest.fn()
+
+    render(
+      <SideBar.AccordionItem
+        {...defaultProps}
+        onClick={onClick}
+        role={'link'}
+      ></SideBar.AccordionItem>,
+      {
+        wrapper: ({ children }) => SideBarWrapper(children, true),
+      },
+    )
+
+    const accordionItem = screen.getByRole('link')
+    await userEvent.click(accordionItem)
+
+    expect(onClick).toHaveBeenCalled()
+  })
+})
