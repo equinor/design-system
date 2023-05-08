ace0dde4aed495a7036d621b4dd3b7ca2e7bb532
oddvernes <oddbjorn.overnes@gmail.com>
2023-05-08 13:57:26 +0200

    🔨✅make jest support modules

--- packages/eds-lab-react/jest.config.cjs
+++ packages/eds-lab-react/jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.ts'],
   transform: {
     '.(js|ts|tsx)':
-      '<rootDir>/node_modules/@equinor/eds-utils/src/test/transformers/babelJest.js',
+      '<rootDir>/node_modules/@equinor/eds-utils/src/test/transformers/babelJest.cjs',
   },
   moduleNameMapper: {
     '\\.(css)$':
@@ -14,4 +14,5 @@ module.exports = {
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   testEnvironment: 'jest-environment-jsdom',
+  transformIgnorePatterns: ['node_modules/(?!.pnpm|ramda)'],
 }
--- packages/eds-utils/jest.config.cjs
+++ packages/eds-utils/jest.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['./jest.setup.ts'],
   transform: {
-    '.(js|ts|tsx)': '<rootDir>/src/test/transformers/babelJest.js',
+    '.(js|ts|tsx)': '<rootDir>/src/test/transformers/babelJest.cjs',
   },
   moduleNameMapper: {
     '\\.(css)$': '<rootDir>/src/test/__mocks__/styleMock.js',
@@ -12,4 +12,5 @@ module.exports = {
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   testEnvironment: 'jest-environment-jsdom',
+  transformIgnorePatterns: ['node_modules/(?!.pnpm|ramda)'],
 }
