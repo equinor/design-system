c4f76466d5ee0f08652cdb6f12b6b8526f240708
oddvernes <oddbjorn.overnes@gmail.com>
2023-05-15 14:04:25 +0200

    Fix sidebar sorting

--- packages/eds-core-react/.storybook/preview.js
+++ packages/eds-core-react/.storybook/preview.js
@@ -1,22 +1,24 @@
 const preview = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewMode: 'docs',
-  options: {
-    storySort: {
-      method: '',
-      order: [
-        'Introduction',
-        'Data Display',
-        'Feedback',
-        'Inputs',
-        'Navigation',
-        'Surfaces',
-        'Typography',
-        'Icons',
-        ['Introduction', 'Preview', 'Icon'],
-        '*',
-        'Playground',
-      ],
+  parameters: {
+    options: {
+      storySort: {
+        method: '',
+        order: [
+          'Introduction',
+          'Data Display',
+          'Feedback',
+          'Inputs',
+          'Navigation',
+          'Surfaces',
+          'Typography',
+          'Icons',
+          ['Introduction', 'Preview', 'Icon'],
+          '*',
+          'Playground',
+        ],
+      },
     },
   },
 }
