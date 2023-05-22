d00984f939d09bc54d82e0e82760d7186efa7b58
oddvernes <oddbjorn.overnes@gmail.com>
2023-05-22 13:57:46 +0200

    icons/tokens: add back commonjs, use .mjs instead

--- packages/eds-tokens/rollup.config.js
+++ packages/eds-tokens/rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { babel } from '@rollup/plugin-babel'
 import del from 'rollup-plugin-delete'
+import pkg from './package.json'
 
 const extensions = ['.jsx', '.js', '.tsx', '.ts']
 
@@ -28,7 +29,9 @@ export default [
         preserveModules: true,
         preserveModulesRoot: 'src',
         format: 'es',
+        entryFileNames: '[name].mjs',
       },
+      { file: pkg.main, format: 'cjs' },
     ],
   },
 ]
