7a1eeb4b7977694fe7b117b42df02ffbb7353791
oddvernes <oddbjorn.overnes@gmail.com>
2023-09-14 13:30:58 +0200

    Storybook Stack: transient props

--- packages/eds-core-react/.storybook/components/Stack.tsx
+++ packages/eds-core-react/.storybook/components/Stack.tsx
@@ -3,16 +3,30 @@ import { styled, StyledObject } from 'styled-components'
 type StackProps = {
   direction?: StyledObject<any>['flexDirection']
   align?: StyledObject<any>['alignItems']
+  children: React.ReactNode
+} & React.HTMLAttributes<HTMLDivElement>
+
+type StyledStackProps = {
+  $direction?: StyledObject<any>['flexDirection']
+  $align?: StyledObject<any>['alignItems']
 }
 
-export const Stack = styled.div<StackProps>`
+const StyledStack = styled.div<StyledStackProps>`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: ${({ align }) => align || 'center'};
+  align-items: ${({ $align }) => $align || 'center'};
   gap: 1rem;
   flex-wrap: wrap;
   @media screen and (min-width: 600px) {
-    flex-direction: ${({ direction }) => direction || 'row'};
+    flex-direction: ${({ $direction }) => $direction || 'row'};
   }
 `
+
+export const Stack = ({ children, direction, align, ...rest }: StackProps) => {
+  return (
+    <StyledStack $direction={direction} $align={align} {...rest}>
+      {children}
+    </StyledStack>
+  )
+}
