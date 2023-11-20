20e08f987b74c406f9277a7bb05b62869607bad1
oddvernes <oddbjorn.overnes@gmail.com>
2023-11-20 15:11:41 +0100

    copied current tokens from workshop repo

---
+++ packages/eds-tokens/build/tailwind/eds-font-family.tailwind.js
@@ -0,0 +1,12 @@
+/**
+ * EDS typography tokens
+ * For use as value of fontFamily property in a Tailwind theme/extend object
+ * 
+ * Do not edit directly
+ * Generated on Mon, 20 Nov 2023 13:05:34 GMT
+ */
+
+module.exports = {
+  "sans": "\"Equinor Variable\"",
+  "mono": "\"Equinor Mono\""
+};
---
+++ packages/eds-tokens/build/tailwind/eds-typography.tailwind.js
@@ -0,0 +1,30 @@
+/**
+ * EDS typography tokens
+ * For use as value of fontSize property in a Tailwind theme/extend object
+ * 
+ * Do not edit directly
+ * Generated on Mon, 20 Nov 2023 13:05:34 GMT
+ */
+
+module.exports = {
+  "lg": ["18.5px",{"lineHeight":"24px","fontWeight":386}],
+  "lg-bolder": ["18.5px",{"lineHeight":"24px","fontWeight":469}],
+  "lg-squished": ["18.5px",{"lineHeight":"20px","fontWeight":386}],
+  "lg-squished-bolder": ["18.5px",{"lineHeight":"20px","fontWeight":469}],
+  "base": ["16px",{"lineHeight":"20px","fontWeight":400}],
+  "base-bolder": ["16px",{"lineHeight":"20px","fontWeight":500}],
+  "base-squished": ["16px",{"lineHeight":"16px","fontWeight":400}],
+  "base-squished-bolder": ["16px",{"lineHeight":"16px","fontWeight":500}],
+  "sm": ["14px",{"lineHeight":"20px","fontWeight":439}],
+  "sm-bolder": ["14px",{"lineHeight":"20px","fontWeight":529}],
+  "sm-squished": ["14px",{"lineHeight":"16px","fontWeight":439}],
+  "sm-squished-bolder": ["14px",{"lineHeight":"16px","fontWeight":529}],
+  "xs": ["12px",{"lineHeight":"16px","fontWeight":471}],
+  "xs-bolder": ["12px",{"lineHeight":"16px","fontWeight":563}],
+  "xs-squished": ["12px",{"lineHeight":"12px","fontWeight":471}],
+  "xs-squished-bolder": ["12px",{"lineHeight":"12px","fontWeight":563}],
+  "xxs": ["10.5px",{"lineHeight":"16px","fontWeight":496}],
+  "xxs-bolder": ["10.5px",{"lineHeight":"16px","fontWeight":612}],
+  "xxs-squished": ["10.5px",{"lineHeight":"12px","fontWeight":496}],
+  "xxs-squished-bolder": ["10.5px",{"lineHeight":"12px","fontWeight":612}]
+};
