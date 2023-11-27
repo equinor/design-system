19bba43f2054d6b6cd815a15066af333b874d078
oddvernes <oddbjorn.overnes@gmail.com>
2023-11-27 14:11:57 +0100

    tailwind: use index file

---
+++ packages/eds-tokens/build/tailwind/index.js
@@ -0,0 +1,4 @@
+import { colors } from './eds-colors-primitive.tailwind'
+import { fontFamily } from './eds-font-family.tailwind'
+import { typography } from './eds-typography.tailwind'
+export { colors, fontFamily, typography }
