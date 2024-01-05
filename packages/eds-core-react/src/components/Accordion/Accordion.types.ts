cd3de72034c0f515c752cc6e3d7e3ad7a1a74fcb
Oddbjørn Øvernes <oove@equinor.com>
2024-01-05 11:27:30 +0100

    📝 Accordion: Update docs to explain headerlevels (#3206)

--- packages/eds-core-react/src/components/Accordion/Accordion.types.ts
+++ packages/eds-core-react/src/components/Accordion/Accordion.types.ts
@@ -1,5 +1,5 @@
 export type AccordionProps = {
-  /** The header level, i.e. h1, h2, h3 etc. */
+  /** The header level, i.e. h1, h2, h3 etc. Note: This only changes the element type, the style is the same for all headerlevels */
   headerLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   /** Which side the chevron should be on  */
   chevronPosition?: 'left' | 'right'
