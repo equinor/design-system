83271040a799db74b4099efd5080e3c9731c4e8e
oddvernes <oddbjorn.overnes@gmail.com>
2024-03-19 15:33:02 +0100

    remove deprecated propsTable

--- packages/eds-core-react/.storybook/components/PropsTable.tsx
+++
@@ -1,6 +0,0 @@
-import { Controls, PRIMARY_STORY } from '@storybook/addon-docs'
-
-//@TODO: replace ArgsTable here when storybook adds support for subComponents in <Controls /> or however they'll do it. See https://github.com/storybookjs/storybook/issues/20782
-export const PropsTable = ({ story = PRIMARY_STORY, ...props }) => (
-  <Controls {...props} />
-)
--- packages/eds-lab-react/.storybook/components/index.ts
+++ packages/eds-lab-react/.storybook/components/index.ts
@@ -1,4 +1,3 @@
-export { PropsTable } from './PropsTable'
 export { Story } from './Story'
 export { Stack } from './Stack'
 export { Links } from './Links'
