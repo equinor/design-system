5fa10a70cc2c1b76dafa9532aaf921a911ca298e
Torleif Halseth <halseth.torleif@gmail.com>
2024-03-20 14:30:24 +0100

    ✨ define get trim values internally and add baselineTrimGrid when we want to align to 4px grid

--- packages/eds-core-react/src/components/Typography/Heading/Heading.tsx
+++ packages/eds-core-react/src/components/Typography/Heading/Heading.tsx
@@ -2,11 +2,14 @@
 import styled, { css } from 'styled-components'
 import { forwardRef } from 'react'
 import { HeadingInlineDocumentation } from './HeadingInlineDocumentation'
-import { createStyleObject } from '@capsizecss/core'
 import { metrics } from '../metrics'
 import { BaselineGrid } from './BaselineGrid'
-import { calculateLineHeight } from './typography.utils'
-import { CapsizeStyles, HeadingLevel, TypographySize } from './heading.types'
+import {
+  calculateLineHeight,
+  formatTextBoxTrimValues,
+  getTextBoxTrimValues,
+} from './typography.utils'
+import { HeadingLevel, TypographySize } from './heading.types'
 import { tokens } from '@equinor/eds-tokens'
 import { headingTokens } from './heading.tokens'
 
@@ -15,13 +18,17 @@ export type StyleHeadingProps = {
   $fontSize: string
   $lineHeight: string
   $color: string
-  $capsizeStyles: CapsizeStyles
+  $trimValues: {
+    capHeightTrim: string
+    baselineTrim: string
+  }
+  $baselineShift?: string
 }
 
 const StyledHeading = styled.h1<StyleHeadingProps>`
   font-weight: 400;
   margin: 0;
-  ${({ $fontFamily, $fontSize, $lineHeight, $color, $capsizeStyles }) => css`
+  ${({ $fontFamily, $fontSize, $lineHeight, $color, $trimValues }) => css`
     font-family: ${$fontFamily};
     font-size: ${$fontSize};
     line-height: ${$lineHeight};
@@ -29,13 +36,13 @@ const StyledHeading = styled.h1<StyleHeadingProps>`
 
     &::before {
       content: '';
-      margin-bottom: ${$capsizeStyles['::before'].marginBottom};
+      margin-bottom: ${$trimValues.capHeightTrim};
       display: table;
     }
 
     &::after {
       content: '';
-      margin-top: ${$capsizeStyles['::after'].marginTop};
+      margin-top: ${$trimValues.baselineTrim};
       display: table;
     }
   `}
@@ -60,11 +67,24 @@ export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
       lineHeightMultiplier: currentHeadingToken.lineHeightMultiplier,
     })
     const color = tokens.typography.heading?.[level]?.color ?? '#000'
-    const capsizeStyles: CapsizeStyles = createStyleObject({
+
+    const textBoxTrimValues = getTextBoxTrimValues({
       fontSize: currentHeadingToken.fontSize,
-      leading: lineHeight,
+      lineHeight,
       fontMetrics: metrics[currentHeadingToken.fontAlias],
     })
+
+    const formattedTrimValues = formatTextBoxTrimValues({
+      fontSize: currentHeadingToken.fontSize,
+      capHeightTrim: textBoxTrimValues.capHeightTrim,
+      baselineTrim: textBoxTrimValues.baselineTrim,
+      baselineTrimGrid: textBoxTrimValues.baselineTrimGrid,
+    })
+
+    const trimValuesWithUnit = {
+      capHeightTrim: `${formattedTrimValues.capHeightTrim}em`,
+      baselineTrim: `${formattedTrimValues.baselineTrimGrid}em`,
+    }
 
     const heading = (
       <StyledHeading
@@ -74,20 +94,22 @@ export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
         $lineHeight={`${lineHeight}px`}
         $fontFamily={currentHeadingToken.fontFamily}
         $color={color}
-        $capsizeStyles={capsizeStyles}
+        $trimValues={trimValuesWithUnit}
       >
-        {props.isDocumentationVisible && (
-          <HeadingInlineDocumentation
-            size={size}
-            $fontSize={`${currentHeadingToken.fontSize}px`}
-            $lineHeight={`${lineHeight}px`}
-            $fontFamily={currentHeadingToken.fontFamily}
-            $color={color}
-            $capsizeStyles={capsizeStyles}
-            level={level}
-          />
-        )}
         {props.children}
+        {props.isDocumentationVisible && (
+          <>
+            <HeadingInlineDocumentation
+              size={size}
+              $fontSize={`${currentHeadingToken.fontSize}px`}
+              $lineHeight={`${lineHeight}px`}
+              $fontFamily={currentHeadingToken.fontFamily}
+              $color={color}
+              $trimValues={trimValuesWithUnit}
+              level={level}
+            />
+          </>
+        )}
       </StyledHeading>
     )
 
