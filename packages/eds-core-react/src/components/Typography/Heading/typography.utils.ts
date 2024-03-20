c01597b42c1cabe699dae7079cca4518eddc6651
Torleif Halseth <halseth.torleif@gmail.com>
2024-03-20 14:30:24 +0100

    ✨ define get trim values internally and add baselineTrimGrid when we want to align to 4px grid

--- packages/eds-core-react/src/components/Typography/Heading/typography.utils.ts
+++ packages/eds-core-react/src/components/Typography/Heading/typography.utils.ts
@@ -1,3 +1,5 @@
+import { FontMetrics } from '../metrics'
+
 export const calculateLineHeight = ({
   fontSize,
   lineHeightMultiplier,
@@ -7,3 +9,56 @@ export const calculateLineHeight = ({
 }): number => {
   return 4 * Math.round((fontSize * lineHeightMultiplier) / 4)
 }
+
+function roundUpToNearest4(n: number) {
+  return Math.ceil(n / 4) * 4
+}
+
+export const getTextBoxTrimValues = ({
+  fontSize,
+  lineHeight,
+  fontMetrics,
+}: {
+  fontSize: number
+  lineHeight: number
+  fontMetrics: FontMetrics
+}) => {
+  const contentArea = fontMetrics.ascent + fontMetrics.descent
+  const ascentScale = fontMetrics.ascent / fontMetrics.unitsPerEm
+  const descentScale = fontMetrics.descent / fontMetrics.unitsPerEm
+  const capHeightScale = fontMetrics.capHeight / fontMetrics.unitsPerEm
+  const lineHeightScale = contentArea / fontMetrics.unitsPerEm
+
+  const lineHeightNormal = fontSize * lineHeightScale
+  const specifiedLineHeightOffset = (lineHeightNormal - lineHeight) / 2
+  const capHeightTrim =
+    (ascentScale - capHeightScale - specifiedLineHeightOffset / fontSize) *
+    fontSize *
+    -1
+  const baselineTrim =
+    (descentScale - specifiedLineHeightOffset / fontSize) * fontSize * -1
+  const baselineTrimGrid =
+    baselineTrim + roundUpToNearest4(lineHeightNormal) - lineHeightNormal
+
+  return {
+    contentArea,
+    ascentScale,
+    descentScale,
+    capHeightScale,
+    lineHeightScale,
+    capHeightTrim,
+    baselineTrim,
+    baselineTrimGrid,
+  }
+}
+
+export const formatTextBoxTrimValues = ({
+  fontSize,
+  capHeightTrim,
+  baselineTrim,
+  baselineTrimGrid,
+}) => ({
+  capHeightTrim: Math.round((capHeightTrim / fontSize) * 10000) / 10000,
+  baselineTrim: Math.round((baselineTrim / fontSize) * 10000) / 10000,
+  baselineTrimGrid: Math.round((baselineTrimGrid / fontSize) * 10000) / 10000,
+})
