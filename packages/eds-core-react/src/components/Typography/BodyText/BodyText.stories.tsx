181b2936c02fa21d68c2d395e8a4a6547e824d16
Torleif Halseth <halseth.torleif@gmail.com>
2024-04-03 08:52:25 +0200

    ✨ feat: add more realistic demo of body text in combination with heading

--- packages/eds-core-react/src/components/Typography/BodyText/BodyText.stories.tsx
+++ packages/eds-core-react/src/components/Typography/BodyText/BodyText.stories.tsx
@@ -4,6 +4,7 @@ import { BaselineGrid } from '../_components/BaselineGrid'
 import { PropertyDocumentation } from '../_components/PropertyDocumentation'
 import { bodyTextTokens, uiTextTokens } from '../_typography.tokens'
 import { BodyTextProps } from '../typography.types'
+import { Heading } from '../Heading/Heading'
 
 const TEXT = 'This is some text, hi ho! This is line two'
 
@@ -46,15 +47,17 @@ export const Introduction: StoryFn<BodyTextProps> = (args) => {
 export const Demo: StoryFn<BodyTextProps> = () => {
   return (
     <BaselineGrid>
-      <BodyText size="LG" as="h1">
-        LG
+      <Heading>Lorem Ipsum</Heading>
+      <BodyText size="BASE">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
       </BodyText>
-      <BodyText size="2XS" as="h2">
-        2XS
-      </BodyText>
-
-      <BodyText size="BASE" as="h2">
-        {TEXT}
+      <BodyText size="BASE">
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </BodyText>
     </BaselineGrid>
   )
