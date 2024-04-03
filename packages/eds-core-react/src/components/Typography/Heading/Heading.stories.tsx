3a54a607eeb379991b06ee4efe20a3b371f3e0e4
Torleif Halseth <halseth.torleif@gmail.com>
2024-04-03 08:30:14 +0200

    ✨ feat: improve storybook

--- packages/eds-core-react/src/components/Typography/Heading/Heading.stories.tsx
+++ packages/eds-core-react/src/components/Typography/Heading/Heading.stories.tsx
@@ -5,14 +5,25 @@ import { HeadingProps } from '../typography.types'
 import { PropertyDocumentation } from '../_components/PropertyDocumentation'
 import { headingTokens } from '../_typography.tokens'
 
+const TEXT = 'This is some text, hi ho! This is line two'
+
 const meta: Meta<typeof Heading> = {
   title: 'Typography/Heading',
   component: Heading,
+  args: {
+    children: TEXT,
+    size: 'BASE',
+  },
   argTypes: {
     as: {
       options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
       control: {
         type: 'select',
+      },
+    },
+    children: {
+      control: {
+        type: 'text',
       },
     },
     size: {
@@ -23,7 +34,6 @@ const meta: Meta<typeof Heading> = {
         },
       },
       options: ['3XS', '2XS', 'XS', 'SM', 'BASE', 'LG'],
-
       control: {
         type: 'select',
       },
@@ -33,7 +43,9 @@ const meta: Meta<typeof Heading> = {
 
 export default meta
 
-const TEXT = 'This is some text, hi ho! This is line two'
+export const Introduction: StoryFn<HeadingProps> = (args) => {
+  return <Heading {...args} />
+}
 
 export const Demo: StoryFn<HeadingProps> = () => {
   return (
@@ -54,19 +66,24 @@ export const Demo: StoryFn<HeadingProps> = () => {
 
 export const Sizes: StoryFn<HeadingProps> = () => {
   return (
-    <BaselineGrid>
+    <>
       <Heading size="3XS">{TEXT}</Heading>
+      <PropertyDocumentation size="3XS" tokens={headingTokens} />
       <Heading size="2XS">{TEXT}</Heading>
+      <PropertyDocumentation size="2XS" tokens={headingTokens} />
       <Heading size="XS">{TEXT}</Heading>
+      <PropertyDocumentation size="XS" tokens={headingTokens} />
       <Heading size="SM">{TEXT}</Heading>
+      <PropertyDocumentation size="SM" tokens={headingTokens} />
       <Heading size="BASE" as="h2">
         {TEXT}
       </Heading>
+      <PropertyDocumentation size="BASE" tokens={headingTokens} />
       <Heading size="LG" as="h1">
         {TEXT}
       </Heading>
       <PropertyDocumentation size="LG" tokens={headingTokens} />
-    </BaselineGrid>
+    </>
   )
 }
 
