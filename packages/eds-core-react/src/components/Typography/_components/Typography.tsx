8815e373e202f370efa7ef4756fa815cf7a88c98
oddvernes <oddbjorn.overnes@gmail.com>
2024-04-26 15:29:28 +0200

    wip typography grid solution

--- packages/eds-core-react/src/components/Typography/_components/Typography.tsx
+++ packages/eds-core-react/src/components/Typography/_components/Typography.tsx
@@ -1,52 +1,65 @@
 // eslint-disable-next-line import/no-named-as-default
+import { HTMLAttributes, AnchorHTMLAttributes } from 'react'
 import styled, { css } from 'styled-components'
 
 export type StyleHeadingProps = {
   $fontFamily: string
   $fontSize: string
   $lineHeight: string
-  $capHeightTrim: string
-  $baselineTrim: string
   $offset?: number
-}
+  $onGrid?: boolean
+} & (HTMLAttributes<HTMLElement> | AnchorHTMLAttributes<HTMLAnchorElement>)
 
 export const Typography = styled.p<StyleHeadingProps>`
-  font-weight: 400;
+  font-weight: 400; //font weight will be provided via token probably
   margin: 0;
   ${({
     $fontFamily,
     $fontSize,
     $lineHeight,
-    $capHeightTrim,
-    $baselineTrim,
     $offset = 0,
+    $onGrid = true,
   }) => css`
     font-family: ${$fontFamily};
     font-size: ${$fontSize};
     line-height: ${$lineHeight};
-    --offset-equinor: 0.05em;
+    //line-height: round(${$fontSize} * 1.125, 4px); //works?
+    color: var(--text-custom-color, inherit);
+    //background-color: rgb(255 0 0 / 0.3);
     --_offset: calc(${$offset} * 1em);
+    --_grid-base: 4px;
+    //style links this way using token color?
+    /* &[href],
+    & [href] {
+      color: var(--custom-color-link, var(--eds-whatever-the-link-color-token-is));
+    } */
 
-    &::before {
-      content: '';
-      margin-bottom: ${$baselineTrim};
-      /*emulate texbox-trim*/
-      margin-bottom: calc((((1lh - 1cap) / 2) - var(--_offset, 0em)) * -1);
-      display: table;
-      /*use padding to round off textbox height to multiples of 4 in supported browsers */
-      @supports (height: round(10px, 1px)) {
-        --_rest: calc((round(up, 1cap, 4px) - 1cap) / 2);
-        padding-top: var(--_rest);
-        padding-bottom: var(--_rest);
-      }
+    @supports (height: round(up, 10px, 1px)) {
+      /*This calculates the rest-values to make the total height rounded to 4px.
+      When onGrid is true, the rest is only added to the top of the text-box.*/
+      --_rest-top: ${$onGrid
+        ? 'calc(round(up, 1cap, var(--_grid-base)) - 1cap)'
+        : 'calc((round(up, 1cap, var(--_grid-base)) - 1cap) / 2)'};
+      --_rest-bottom: ${$onGrid
+        ? '0cap'
+        : 'calc((round(up, 1cap, var(--_grid-base)) - 1cap) / 2)'};
     }
 
+    /*This emulates text-box-trim*/
+    --_trim-top: calc(((((1lh - 1cap) / 2) - var(--_offset)) * -1));
+    --_trim-bottom: calc(((((1lh - 1cap) / 2) + var(--_offset)) * -1));
+
+    &::before {
+      //Because of the negative margins, margin-bottom adjusts the top and vice versa
+      margin-bottom: calc(var(--_trim-top) + var(--_rest-top, 0cap));
+    }
     &::after {
-      content: '';
-      margin-top: ${$capHeightTrim};
-      /*emulate texbox-trim*/
-      margin-top: calc((((1lh - 1cap) / 2) + var(--_offset, 0em)) * -1);
+      margin-top: calc(var(--_trim-bottom) + var(--_rest-bottom, 0cap));
+    }
+    &::before,
+    &::after {
       display: table;
+      content: '';
     }
   `}
 `
--- packages/eds-core-react/src/components/Typography/typography.utils.ts
+++ packages/eds-core-react/src/components/Typography/typography.utils.ts
@@ -1,6 +1,6 @@
-import { FontMetrics, metrics } from './metrics'
 import { TypographySize, TypographyTokenCollection } from './typography.types'
 
+//@todo: can this be expressed in css i.e. calc(round(${$fontSize} * ${$lineHeightMultiplier}, 4px))
 export const calculateLineHeight = ({
   fontSize,
   lineHeightMultiplier,
@@ -10,62 +10,6 @@ export const calculateLineHeight = ({
 }): number => {
   return 4 * Math.round((fontSize * lineHeightMultiplier) / 4)
 }
-
-function roundUpToNearest4(n: number) {
-  return Math.ceil(n / 4) * 4
-}
-
-export const getTextBoxTrimValues = ({
-  fontSize,
-  lineHeight,
-  fontMetrics,
-}: {
-  fontSize: number
-  lineHeight: number
-  fontMetrics: FontMetrics
-}) => {
-  const contentArea = fontMetrics.ascent + fontMetrics.descent
-  const ascentScale = fontMetrics.ascent / fontMetrics.unitsPerEm
-  const descentScale = fontMetrics.descent / fontMetrics.unitsPerEm
-  const capHeightScale = fontMetrics.capHeight / fontMetrics.unitsPerEm
-  const lineHeightScale = contentArea / fontMetrics.unitsPerEm
-
-  const lineHeightNormal = fontSize * lineHeightScale
-  const specifiedLineHeightOffset = (lineHeightNormal - lineHeight) / 2
-  const capHeightTrim =
-    (ascentScale - capHeightScale - specifiedLineHeightOffset / fontSize) *
-    fontSize *
-    -1
-  const computedCapHeight = fontSize * capHeightScale
-  const baselineTrim =
-    (descentScale - specifiedLineHeightOffset / fontSize) * fontSize * -1
-  const baselineTrimGrid =
-    baselineTrim + roundUpToNearest4(computedCapHeight) - computedCapHeight
-
-  return {
-    contentArea,
-    ascentScale,
-    descentScale,
-    capHeightScale,
-    lineHeightScale,
-    lineHeightNormal,
-    specifiedLineHeightOffset,
-    capHeightTrim,
-    baselineTrim,
-    baselineTrimGrid,
-  }
-}
-
-export const formatTextBoxTrimValues = ({
-  fontSize,
-  capHeightTrim,
-  baselineTrim,
-  baselineTrimGrid,
-}) => ({
-  capHeightTrim: Math.round((capHeightTrim / fontSize) * 10000) / 10000,
-  baselineTrim: Math.round((baselineTrim / fontSize) * 10000) / 10000,
-  baselineTrimGrid: Math.round((baselineTrimGrid / fontSize) * 10000) / 10000,
-})
 
 export const getTypographyProperties = ({
   tokens,
@@ -80,32 +24,12 @@ export const getTypographyProperties = ({
     lineHeightMultiplier: token.lineHeightMultiplier,
   })
 
-  const textBoxTrimValues = getTextBoxTrimValues({
-    fontSize: token.fontSize,
-    lineHeight,
-    fontMetrics: metrics[token.fontAlias],
-  })
-
-  const formattedTextBoxTrimValues = formatTextBoxTrimValues({
-    fontSize: token.fontSize,
-    capHeightTrim: textBoxTrimValues.capHeightTrim,
-    baselineTrim: textBoxTrimValues.baselineTrim,
-    baselineTrimGrid: textBoxTrimValues.baselineTrimGrid,
-  })
-
-  const trimValuesWithUnit = {
-    capHeightTrimInEm: `${formattedTextBoxTrimValues.capHeightTrim}em`,
-    baselineTrimInEm: `${formattedTextBoxTrimValues.baselineTrim}em`,
-    baselineTrimGridInEm: `${formattedTextBoxTrimValues.baselineTrimGrid}em`,
-  }
-
   return {
     fontSize: token.fontSize,
     lineHeight: lineHeight,
-    ...textBoxTrimValues,
     fontSizeInRem: `${token.fontSize / 16}rem`,
     lineHeightInRem: `${lineHeight / 16}rem`,
-    ...trimValuesWithUnit,
     fontFamily: token.fontFamily,
+    verticalOffset: token.verticalOffset,
   }
 }
