aecd92a218342c85579b40bc8350d6cc2cc15711
Ronnie Laugen <rhenri@equinor.com>
2024-05-03 14:05:31 +0200

    fix: Shims for popover (jsdom doesn't support it)

--- packages/eds-data-grid-react/jest.setup.ts
+++ packages/eds-data-grid-react/jest.setup.ts
@@ -7,3 +7,5 @@ expect.extend(toHaveNoViolations)
 // Workaround for jest-axe error: https://github.com/nickcolley/jest-axe/issues/147
 const { getComputedStyle } = window
 window.getComputedStyle = (elt) => getComputedStyle(elt)
+HTMLDivElement.prototype.showPopover = jest.fn()
+HTMLDivElement.prototype.hidePopover = jest.fn()
--- packages/eds-lab-react/jest.setup.ts
+++ packages/eds-lab-react/jest.setup.ts
@@ -1,3 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+HTMLDivElement.prototype.showPopover = jest.fn()
+HTMLDivElement.prototype.hidePopover = jest.fn()
