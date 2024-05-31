dde8a4f97a5e9dcaeda9ee3001f8913a7ff8849b
Ana Cepuran <135026519+ana-cepuran@users.noreply.github.com>
2024-05-31 14:56:43 +0200

    Update EdsDataGrid.tsx
    Add the enableSubRowSelection parameter to the EdsDataGrid through props.

--- packages/eds-data-grid-react/src/EdsDataGrid.tsx
+++ packages/eds-data-grid-react/src/EdsDataGrid.tsx
@@ -46,6 +46,7 @@ export function EdsDataGrid<T>({
   rowSelection,
   enableRowSelection,
   enableMultiRowSelection,
+  enableSubRowSelection,
   selectedRows,
   rowSelectionState,
   enableColumnFiltering,
