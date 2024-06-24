1ec7a270d1394f77ebbad510751d019afc49af01
oddvernes <oddbjorn.overnes@gmail.com>
2024-06-24 15:25:42 +0200

    updated/added docs

--- packages/eds-core-react/.storybook/components/BaselineGrid.tsx
+++ packages/eds-core-react/.storybook/components/BaselineGrid.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 
 export const BaselineGrid = styled.div`
   /*TODO: find color that work in dark mode, or use an existing token if possible*/
-  --line: light-dark(#f2f2f2, #f2f2f2);
+  --_line: var(--baseline-grid-color, light-dark(#f2f2f2, #f2f2f2));
   background: linear-gradient(
     to bottom,
-    var(--line),
-    var(--line) 50%,
+    var(--_line),
+    var(--_line) 50%,
     transparent 50%,
     transparent
   );
