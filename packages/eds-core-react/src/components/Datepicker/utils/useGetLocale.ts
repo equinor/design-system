ef4db871455212b41d97ee26e6c7f1430f29b868
Ronnie Laugen <rhenri@equinor.com>
2024-09-16 13:32:17 +0200

    feat(datepickers): Check for navigator-existence to work with SSR

--- packages/eds-core-react/src/components/Datepicker/utils/useGetLocale.ts
+++ packages/eds-core-react/src/components/Datepicker/utils/useGetLocale.ts
@@ -3,7 +3,8 @@ import { useLocale } from 'react-aria'
 export const useGetLocale = (locale?: string) => {
   const { locale: externalLocale } = useLocale()
   // react-aria defaults to navigator.language if no locale is provided. If these are equal, we override by using the system default locale
-  const defaultLocale = navigator.language
+  const defaultLocale =
+    (typeof navigator !== 'undefined' && navigator.language) || 'en-US'
   const fallbackLocale = new Intl.DateTimeFormat().resolvedOptions().locale
   return (
     locale ??
