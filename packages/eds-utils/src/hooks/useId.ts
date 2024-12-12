0520b3221b4f1300a18d4900d1476f50794d2336
Sebastian Vittersø <svit@equinor.com>
2024-12-12 15:34:01 +0100

    fix(useId): make useId stable for client/server e.g. nextjs

--- packages/eds-utils/src/hooks/useId.ts
+++ packages/eds-utils/src/hooks/useId.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
 
-export const useId = (idOverride: string, type?: string): string => {
+let counter = 0
+
+export const useId = (idOverride?: string, type?: string): string => {
   const [defaultId, setDefaultId] = useState(idOverride)
   const id = idOverride || defaultId
 
   useEffect(() => {
     if (defaultId == null) {
-      setDefaultId(
-        `eds-${type ? type + `-` : ''}${Math.round(Math.random() * 1e5)}`,
-      )
+      setDefaultId(`eds-${type ? type + `-` : ''}${counter++}`)
     }
   }, [defaultId, type])
   return id
