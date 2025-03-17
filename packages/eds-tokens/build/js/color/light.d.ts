f4b08832b96042c67512007c40de2f1351bf8609
Torleif Halseth <halseth.torleif@gmail.com>
2025-03-17 14:57:35 +0100

    ✨ update color tokens and variables

--- packages/eds-tokens/build/js/color/light.d.ts
+++ packages/eds-tokens/build/js/color/light.d.ts
@@ -15,12 +15,12 @@ export const EDS_THEME_COLOR_TEXT_DANGER_SUBTLE: string;
 export const EDS_THEME_COLOR_TEXT_DANGER_DEFAULT: string;
 export const EDS_THEME_COLOR_TEXT_SUCCESS_SUBTLE: string;
 export const EDS_THEME_COLOR_TEXT_SUCCESS_DEFAULT: string;
-export const EDS_THEME_COLOR_BACKGROUND_NEUTRAL_DEFAULT: string;
-export const EDS_THEME_COLOR_BACKGROUND_NEUTRAL_DEFAULT_HOVER: string;
-export const EDS_THEME_COLOR_BACKGROUND_NEUTRAL_DEFAULT_ACTIVE: string;
-export const EDS_THEME_COLOR_BACKGROUND_NEUTRAL_STRONG: string;
-export const EDS_THEME_COLOR_BACKGROUND_NEUTRAL_STRONG_HOVER: string;
-export const EDS_THEME_COLOR_BACKGROUND_NEUTRAL_STRONG_ACTIVE: string;
+export const EDS_THEME_COLOR_BACKGROUND_DEFAULT: string;
+export const EDS_THEME_COLOR_BACKGROUND_DEFAULT_HOVER: string;
+export const EDS_THEME_COLOR_BACKGROUND_DEFAULT_ACTIVE: string;
+export const EDS_THEME_COLOR_BACKGROUND_STRONG: string;
+export const EDS_THEME_COLOR_BACKGROUND_STRONG_HOVER: string;
+export const EDS_THEME_COLOR_BACKGROUND_STRONG_ACTIVE: string;
 export const EDS_THEME_COLOR_BACKGROUND_ACCENT_DEFAULT: string;
 export const EDS_THEME_COLOR_BACKGROUND_ACCENT_DEFAULT_HOVER: string;
 export const EDS_THEME_COLOR_BACKGROUND_ACCENT_DEFAULT_ACTIVE: string;
@@ -72,13 +72,13 @@ export const EDS_THEME_COLOR_BORDER_INFO_STRONG: string; /** Application backgro
 export const EDS_THEME_UI_COLOR_BACKGROUND_BASE: string; /** The header, footer, toolbars, panels, and primary content can use this background color */
 export const EDS_THEME_UI_COLOR_BACKGROUND_LAYER_1: string; /** The header, footer, toolbars, panels, and primary content can use this background color */
 export const EDS_THEME_UI_COLOR_BACKGROUND_LAYER_2: string; /** Popover elements will appear above all other elements */
-export const EDS_THEME_UI_COLOR_BACKGROUND_ELEVATED: string; /** The color of the mask overlay that appears when you enter a modal state. */
-export const EDS_THEME_BACKDROP_COLOR_BACKGROUND: string;
-export const EDS_THEME_INPUT_COLOR_BACKGROUND: string;
-export const EDS_THEME_INPUT_COLOR_BORDER_DISABLED: string;
-export const EDS_THEME_INPUT_COLOR_TEXT_DISABLED: string;
+export const EDS_THEME_UI_COLOR_BACKGROUND_ELEVATED: string;
 export const EDS_THEME_BUTTON_COLOR_BACKGROUND_DISABLED: string;
 export const EDS_THEME_BUTTON_COLOR_BORDER_DISABLED: string;
 export const EDS_THEME_BUTTON_COLOR_TEXT_DISABLED: string;
+export const EDS_THEME_INPUT_COLOR_BACKGROUND: string;
+export const EDS_THEME_INPUT_COLOR_BORDER_DISABLED: string;
+export const EDS_THEME_INPUT_COLOR_TEXT_DISABLED: string;
 export const EDS_THEME_LINK_COLOR_TEXT: string;
-export const EDS_THEME_FOCUS_COLOR_BORDER: string;
+export const EDS_THEME_FOCUS_RING_COLOR_BORDER: string; /** The color of the mask overlay that appears when you enter a modal state. */
+export const EDS_THEME_OVERLAY_COLOR_BACKGROUND: string;
