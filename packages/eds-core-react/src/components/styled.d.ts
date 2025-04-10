ae98db1df403d0d39eed9af9e9540e6dd24e67f2
Torleif Halseth <halseth.torleif@gmail.com>
2025-04-10 13:37:17 +0200

    fix: type issue when extending styled-components theme

--- packages/eds-core-react/src/components/styled.d.ts
+++ packages/eds-core-react/src/components/styled.d.ts
@@ -1,8 +1,8 @@
-// import original module declarations
 import 'styled-components'
-import { ComponentToken } from '@equinor/eds-tokens'
-// and extend them!
+import type { ComponentToken } from '@equinor/eds-tokens'
+
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  export interface DefaultTheme extends ComponentToken {}
+  export interface DefaultTheme extends ComponentToken {
+    // You can add any additional theme properties here if needed
+  }
 }
--- packages/eds-utils/src/hooks/useToken.ts
+++ packages/eds-utils/src/hooks/useToken.ts
@@ -1,21 +1,22 @@
 import { useCallback } from 'react'
 import type { ComponentToken } from '@equinor/eds-tokens'
 import { mergeDeepRight } from 'ramda'
+import { DefaultTheme } from 'styled-components'
 
 export type Density = 'compact' | 'comfortable'
 
 type UseToken = (
   options: { density: Density },
   token: ComponentToken,
-) => () => ComponentToken
+) => () => DefaultTheme
 
 export const useToken: UseToken = (options, token) =>
   useCallback(() => {
     const { density } = options
 
     if (density === 'compact') {
-      return mergeDeepRight(token, token.modes.compact) as ComponentToken
+      return mergeDeepRight(token, token.modes.compact) as DefaultTheme
     }
 
-    return token
+    return token as DefaultTheme
   }, [options, token])
