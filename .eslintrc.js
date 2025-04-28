c5c2521f41a26b9a0ea60bc6f76ceb389143eae8
Frida <fridaerdal@hotmail.no>
2025-04-28 12:26:01 +0200

    No unsafe argument lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -132,6 +132,7 @@ module.exports = {
         '@typescript-eslint/no-unsafe-call': ['warn'],
         '@typescript-eslint/no-unsafe-return': ['warn'],
         '@typescript-eslint/no-unsafe-assignment': ['warn'],
+        '@typescript-eslint/no-unsafe-argument': ['warn'],
         '@typescript-eslint/no-explicit-any': ['warn'],
         '@typescript-eslint/no-misused-promises': [
           'error',
