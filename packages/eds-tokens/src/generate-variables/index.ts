b5a6a619dbb33423cabc7a2808523e27817d1407
Torleif Halseth <halseth.torleif@gmail.com>
2025-06-02 10:31:12 +0200

    feat: move build of variables to eds-tokens

---
+++ packages/eds-tokens/src/generate-variables/index.ts
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { StyleDictionary } from 'style-dictionary-utils'
+import {
+  pxTransform,
+  PX_TO_REM_NAME,
+  PX_FORMATTED_NAME,
+  FONT_QUOTE_NAME,
+  fontQuote,
+  pxFormatted,
+  pxToRem,
+  createClassicColorVariables,
+  createMatrixColorVariables,
+  createSpacingAndTypographyVariables,
+} from '@equinor/eds-tokens-build'
+
+const outputDirectory = `${process.cwd()}/build`
+export const cssBuildPath = `${outputDirectory}/css`
+export const jsBuildPath = `${outputDirectory}/js`
+export const jsonBuildPath = `${outputDirectory}/json`
+
+StyleDictionary.registerTransform(pxFormatted)
+StyleDictionary.registerTransform(pxTransform)
+StyleDictionary.registerTransform(pxToRem)
+StyleDictionary.registerTransform(fontQuote)
+
+export async function generate() {
+  const TOKENS_DIR_FILE_PATH = `${process.cwd()}/tokens`
+  console.info('Running Style Dictionary build script')
+  console.info('Tokens directory:', TOKENS_DIR_FILE_PATH)
+
+  const colorBuildPath = 'color/'
+
+  const cssTransforms = [
+    'name/kebab',
+    PX_TO_REM_NAME,
+    PX_FORMATTED_NAME,
+    FONT_QUOTE_NAME,
+  ]
+
+  await createMatrixColorVariables({
+    tokensDir: TOKENS_DIR_FILE_PATH,
+    colorBuildPath: colorBuildPath,
+    prefix: 'eds-color',
+  })
+
+  await createClassicColorVariables({
+    tokensDir: TOKENS_DIR_FILE_PATH,
+    cssBuildPath: cssBuildPath,
+    colorBuildPath: colorBuildPath,
+    cssTransforms,
+  })
+  await createSpacingAndTypographyVariables({
+    tokensDir: TOKENS_DIR_FILE_PATH,
+    cssBuildPath: cssBuildPath,
+    cssTransforms,
+  })
+}
+
+generate()
+  .then(() => {
+    console.log('✅ Variables generated successfully')
+  })
+  .catch((error) => {
+    console.error('❌ Error generating color variables:', error)
+  })
---
+++ packages/eds-tokens/vite.generate-variables.config.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-default-export */
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      name: 'generate-variables',
+      fileName: 'generate-variables',
+      entry: 'src/generate-variables/index.ts',
+    },
+    rollupOptions: {
+      output: {
+        dir: 'build-generate-variables',
+      },
+      external: [
+        'fs',
+        'path',
+        'os',
+        'crypto',
+        'style-dictionary',
+        'style-dictionary-utils',
+        '@equinor/eds-tokens-sync',
+        '@equinor/eds-tokens-build',
+      ],
+    },
+  },
+})
