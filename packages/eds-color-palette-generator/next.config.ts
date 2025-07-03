bb12ce1e2d71f5d17465602d0b126c242d431bef
Torleif Halseth <halseth.torleif@gmail.com>
2025-07-03 15:47:07 +0200

    feat(eds-color-palette-generator): add Docker support and update configuration

--- packages/eds-color-palette-generator/next.config.ts
+++ packages/eds-color-palette-generator/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  output: 'standalone',
 }
 
 export default nextConfig
