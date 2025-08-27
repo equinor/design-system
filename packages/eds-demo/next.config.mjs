ed8b6fefd367b6fa7e18456349968471ba643026
Torleif Halseth <halseth.torleif@gmail.com>
2025-08-27 13:48:33 +0200

    feat: setup radix, docker and standalone build

--- packages/eds-demo/next.config.mjs
+++ packages/eds-demo/next.config.mjs
@@ -4,6 +4,7 @@ import createMDX from "@next/mdx";
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   devIndicators: false,
+  output: "standalone",
 };
 
 const withMDX = createMDX({
