754e26d9050f73a8e580a9d3e27fde44ea4162f7
Torleif Halseth <halseth.torleif@gmail.com>
2025-08-27 15:50:21 +0200

    fix: install dockerfile and other setup

--- packages/eds-demo/next.config.mjs
+++ packages/eds-demo/next.config.mjs
@@ -4,7 +4,6 @@ import createMDX from "@next/mdx";
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   devIndicators: false,
-  output: "standalone",
 };
 
 const withMDX = createMDX({
