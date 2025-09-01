d2909bd5131da2ab495ab9ecc42aee748bfc33ff
Torleif Halseth <halseth.torleif@gmail.com>
2025-09-01 10:40:56 +0200

    feat: use pnpm in palette generator and modify docker file to work with the new setup

--- packages/eds-color-palette-generator/next.config.ts
+++ packages/eds-color-palette-generator/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  output: 'standalone',
 }
 
 export default nextConfig
--- packages/eds-color-palette-generator/src/app/layout.tsx
+++ packages/eds-color-palette-generator/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import '@equinor/eds-tokens/css/variables-static.css'
 import './globals.css'
 import '@/styles/dialog.css' // Import dialog styles globally
 import { ColorSchemeProvider } from '@/context/ColorSchemeContext'
