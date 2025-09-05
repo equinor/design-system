70fdb23aaa4dc286363ed32b67f8afdb29e08357
Frida <fridaerdal@hotmail.no>
2025-09-05 11:04:14 +0200

    fix(eds-core-react): 🐛 add Banner.Content to resolve HTML nesting warnings
    - Creates Banner.Content component for complex content with headers, lists etc. - Resolves validateDOMNesting errors when using complex children in banners - Banner.Message unchanged, maintains backward compatibility

--- packages/eds-core-react/src/components/Banner/index.ts
+++ packages/eds-core-react/src/components/Banner/index.ts
@@ -2,26 +2,31 @@ import { Banner as BaseBanner, BannerProps } from './Banner'
 import { BannerIcon, BannerIconProps } from './BannerIcon'
 import { BannerMessage, BannerMessageProps } from './BannerMessage'
 import { BannerActions, BannerActionsProps } from './BannerActions'
+import { BannerContent, BannerContentProps } from './BannerContent'
 
 type BannerCompoundProps = typeof BaseBanner & {
   Icon: typeof BannerIcon
   Message: typeof BannerMessage
   Actions: typeof BannerActions
+  Content: typeof BannerContent
 }
 
 const Banner = BaseBanner as BannerCompoundProps
 Banner.Icon = BannerIcon
 Banner.Message = BannerMessage
 Banner.Actions = BannerActions
+Banner.Content = BannerContent
 
 Banner.Icon.displayName = 'Banner.Icon'
 Banner.Message.displayName = 'Banner.Message'
 Banner.Actions.displayName = 'Banner.Actions'
+Banner.Content.displayName = 'Banner.Content'
 
-export { Banner, BannerIcon, BannerMessage, BannerActions }
+export { Banner, BannerIcon, BannerMessage, BannerActions, BannerContent }
 export type {
   BannerProps,
   BannerMessageProps,
   BannerIconProps,
   BannerActionsProps,
+  BannerContentProps,
 }
