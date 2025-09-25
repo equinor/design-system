1a563dcef41f461e71e3edfa4544de5a1fe19020
camillaknutsen <camilla.knutsen89@gmail.com>
2025-09-25 13:21:52 +0200

    refactor(eds-demo): ♻️ refactor ActionButton, SearchInput and Tab components to visually see they are interactive and have cursor pointer

--- packages/eds-demo/src/app/_components/ActionButton.tsx
+++ packages/eds-demo/src/app/_components/ActionButton.tsx
@@ -3,15 +3,16 @@ import React from "react";
 type Props = {
   className?: string;
   children: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLDivElement>;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const ActionButton = ({ className, children, ...rest }: Props) => {
   return (
-    <div
+    <button
+      type="button"
       className={`cursor-default select-none w-11 h-11 rounded-full justify-center items-center flex ${className}`}
       {...rest}
     >
       <div className="relative w-6 h-6">{children}</div>
-    </div>
+    </button>
   );
 };
