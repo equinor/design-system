f1f35566f2f49e634f41503aaf9adc5b7b418a9a
Edvard Pires Bjørgen <edbjorgen@gmail.com>
2025-09-29 11:43:03 +0200

    fix unused link in pages index.tsx

--- apps/design-system-docs/src/pages/index.tsx
+++ apps/design-system-docs/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx'
-import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import { HomepageFeatures } from '@site/src/components/HomepageFeatures'
