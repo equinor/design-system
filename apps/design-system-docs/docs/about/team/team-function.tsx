8b029252667b2a7d5d4294a50d0b11dbcbe81de7
Chibuzor Nwemambu <75029767+Chibuzor-Nwemambu@users.noreply.github.com>
2025-10-01 15:10:32 +0200

    feat: Redesign the Meet the team page (#4015)
    Redesigned and updated info in the "Meet the team" page
    Changed the name from "Meet the team" to "The team"

---
+++ apps/design-system-docs/docs/about/team/team-function.tsx
@@ -0,0 +1,17 @@
+import './team.css'
+
+type TeamProps = {
+  image: string
+  name: string
+  role: string
+}
+
+export const Team = ({ image, name, role }: TeamProps) => {
+  return (
+    <div>
+      <img src={image} className="circle" alt={name} />
+      <p className="text--center padding-horiz--md">{name}</p>
+      <p className="text--center padding-horiz--md">{role}</p>
+    </div>
+  )
+}
