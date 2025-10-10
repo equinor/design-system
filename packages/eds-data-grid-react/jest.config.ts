c49b7da2840839f51a796a2bd4709b39899fed08
Frida Erdal <31915755+pomfrida@users.noreply.github.com>
2025-10-10 11:24:12 +0200

    chore: 🔧 Align peer deps across packages (#4066)
    * chore: 🔧 Align peer deps across packages
    
    * Update lockfile
    
    * Update jest.config.ts in datagrid

--- packages/eds-data-grid-react/jest.config.ts
+++ packages/eds-data-grid-react/jest.config.ts
@@ -2,27 +2,32 @@ import type { Config } from 'jest'
 
 const config: Config = {
   displayName: 'eds-data-grid',
+  verbose: true,
+  testEnvironment: 'jest-environment-jsdom',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+
   transform: {
     '.(js|ts|tsx)':
       '<rootDir>/node_modules/@equinor/eds-utils/src/test/transformers/babelJest.cjs',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  verbose: true,
-  setupFilesAfterEnv: ['./jest.setup.ts'],
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(\\.pnpm|ramda|@equinor/eds-core-react)/)',
+  ],
+
   moduleNameMapper: {
     '\\.(css)$':
       '<rootDir>/node_modules/@equinor/eds-utils/src/test/__mocks__/styleMock.js',
+
     '^react$': '<rootDir>/node_modules/react',
     '^styled-components$': '<rootDir>/node_modules/styled-components',
+
     '^@equinor/eds-core-react$':
       '<rootDir>/node_modules/@equinor/eds-core-react/dist/esm/index.js',
   },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  testEnvironment: 'jest-environment-jsdom',
-  transformIgnorePatterns: ['node_modules/(?!.pnpm|ramda)'],
 }
 
 // eslint-disable-next-line import/no-default-export
 export default config
-
-/* eslint-disable */
