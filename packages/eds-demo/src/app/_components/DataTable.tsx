e01833dddd2e58227f01781dcfffceeddf34a991
camillaknutsen <camilla.knutsen89@gmail.com>
2025-10-15 09:59:00 +0200

    fix(eds-demo): 💄 Comment out styles and data attribute in DataTable to not show active header to align with figma demo

--- packages/eds-demo/src/app/_components/DataTable.tsx
+++ packages/eds-demo/src/app/_components/DataTable.tsx
@@ -66,7 +66,7 @@ export const DataTable = ({
               data-debug
               key={column.id}
               className={`${thClass} ${column.active ? activeThClass : ""} ${idx === 0 ? "w-12" : "w-1/5"} self-stretch font-normal h-[44px] px-4 py-1 justify-start text-left text-sm leading-tight grow shrink basis-0 cursor-pointer`}
-              data-color-appearance={column.active ? "accent" : undefined}
+              /*data-color-appearance={column.active ? "accent" : undefined}*/
             >
               {idx === 0 ? (
                 <div className="w-[18px] h-[18px] justify-start items-center gap-1 flex">
