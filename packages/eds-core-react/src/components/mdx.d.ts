5a1455c18e91c0b123018b0a349aa6a35fc300ab
Torleif Halseth <halseth.torleif@gmail.com>
2025-10-22 13:49:24 +0200

    fix: add @types/mdx to dependencies and update tsconfig for mdx support

--- packages/eds-core-react/src/components/mdx.d.ts
+++
@@ -1,8 +0,0 @@
-import type { JSX } from 'react'
-
-declare module '*.mdx' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let MDXComponent: (props: any) => JSX.Element
-  // eslint-disable-next-line import/no-default-export
-  export default MDXComponent
-}
