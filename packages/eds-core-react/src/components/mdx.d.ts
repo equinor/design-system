42395a0c2bcf4dd202b3e628c03cc67df9024bb8
Torleif Halseth <halseth.torleif@gmail.com>
2025-10-22 11:56:21 +0200

    fix(mdx): add explicit any type to MDXComponent props and update typeRoots in tsconfig

--- packages/eds-core-react/src/components/mdx.d.ts
+++ packages/eds-core-react/src/components/mdx.d.ts
@@ -1,7 +1,8 @@
 import type { JSX } from 'react'
 
 declare module '*.mdx' {
-  let MDXComponent: (props) => JSX.Element
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let MDXComponent: (props: any) => JSX.Element
   // eslint-disable-next-line import/no-default-export
   export default MDXComponent
 }
