74a6df4ec3124eb0c40d9f6121a7ce5e119d4ebe
Torleif Halseth <halseth.torleif@gmail.com>
2025-10-28 20:09:32 +0100

    refactor(eds-utils): remove unnecessary imports in useAutoResize tests for cleaner code

--- packages/eds-utils/src/hooks/useAutoResize.test.tsx
+++ packages/eds-utils/src/hooks/useAutoResize.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { useAutoResize } from './useAutoResize'
 
 const TestComponent = ({ maxHeight, ...rest }: { maxHeight?: number }) => {
