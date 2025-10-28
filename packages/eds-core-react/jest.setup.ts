1a93f1363ebf8f0462764308b4fe893422c2a282
Torleif Halseth <halseth.torleif@gmail.com>
2025-10-28 19:49:58 +0100

    chore(eds-core-react): mock React.useId for consistent snapshot IDs in tests

--- packages/eds-core-react/jest.setup.ts
+++ packages/eds-core-react/jest.setup.ts
@@ -1,6 +1,11 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import { toHaveNoViolations } from 'jest-axe'
+import React from 'react'
+
+// Mock React.useId to return consistent IDs for snapshot tests
+// This ensures the same IDs are generated in both local and CI environments
+jest.spyOn(React, 'useId').mockReturnValue('test-id')
 
 expect.extend(toHaveNoViolations)
 
