4e016f57c95f39e95431818c65656ea1b68561c9
Torleif Halseth <halseth.torleif@gmail.com>
2025-10-29 11:26:59 +0100

    fix: Correct import path for spacing CSS in storybook

--- packages/eds-core-react/src/components/Spacing/Spacing.stories.tsx
+++ packages/eds-core-react/src/components/Spacing/Spacing.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import '../src/components/Typography/spacing.css'
+import './spacing.css'
 import './Spacing.stories.css'
 
 const meta: Meta = {
