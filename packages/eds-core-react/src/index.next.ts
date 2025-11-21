5adb069bd38219f33f671d063426b82facfb2207
Frida <fridaerdal@hotmail.no>
2025-11-21 13:53:12 +0100

    Add placeholder to stisfy build

--- packages/eds-core-react/src/index.next.ts
+++ packages/eds-core-react/src/index.next.ts
@@ -1,3 +1,5 @@
 'use client'
 
-export * from './components/next'
+// EDS 2.0 components will be exported here when available
+// Temporary placeholder to satisfy build tools
+export const __EDS_2_0__ = 'EDS 2.0 beta - components coming soon'
