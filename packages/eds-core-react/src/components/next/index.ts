d078d9365640d15c0b515702af816802a8ffd033
camillaknutsen <camilla.knutsen89@gmail.com>
2025-12-11 13:23:34 +0100

    feat(next): add Label component
    - Uses TypographyNext as parent label element
    - Support for optional and required boolean props
    - Shows "(Optional)" or "(Required)" indicator text

--- packages/eds-core-react/src/components/next/index.ts
+++ packages/eds-core-react/src/components/next/index.ts
@@ -6,3 +6,6 @@ export type { PlaceholderProps } from './Placeholder'
 
 export { Input } from './Input'
 export type { InputProps } from './Input'
+
+export { Label } from './Label'
+export type { LabelProps } from './Label'
