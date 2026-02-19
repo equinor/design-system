45a9b0ed71aeab1173cd3747cd8e336c8a5d126e
Edvard Pires Bjørgen <edbjorgen@gmail.com>
2026-02-19 13:46:25 +0100

    feat(eds-tokens): add nested TypeScript token output for color

---
+++ packages/eds-tokens/build/ts/color/color-scheme/dark-semantic.ts
@@ -0,0 +1,162 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const color = {
+  bg: {
+    neutral: {
+      canvas: '#0b0b0b',
+      surface: '#202223',
+      fillMuted: {
+        default: '#525c65',
+        hover: '#5d6b76',
+        active: '#6b7d8b',
+      },
+      fillEmphasis: {
+        default: '#b4c7d7',
+        hover: '#cadae7',
+        active: '#deeaf4',
+      },
+    },
+    accent: {
+      canvas: '#0a0b0b',
+      surface: '#1e2323',
+      fillMuted: {
+        default: '#3c6266',
+        hover: '#3e7378',
+        active: '#41878e',
+      },
+      fillEmphasis: {
+        default: '#8cd2da',
+        hover: '#ace3e9',
+        active: '#c7f1f6',
+      },
+    },
+    success: {
+      canvas: '#0a0c0a',
+      surface: '#1e231e',
+      fillMuted: {
+        default: '#3c673a',
+        hover: '#3e793c',
+        active: '#418e3e',
+      },
+      fillEmphasis: {
+        default: '#8cdb87',
+        hover: '#aceba8',
+        active: '#c7f7c3',
+      },
+    },
+    info: {
+      canvas: '#0a0b0c',
+      surface: '#1d2226',
+      fillMuted: {
+        default: '#33607e',
+        hover: '#316f98',
+        active: '#2e82b7',
+      },
+      fillEmphasis: {
+        default: '#7dceff',
+        hover: '#a2e0ff',
+        active: '#c4eeff',
+      },
+    },
+    warning: {
+      canvas: '#0c0b0a',
+      surface: '#27201b',
+      fillMuted: {
+        default: '#7e4e25',
+        hover: '#97571b',
+        active: '#b46201',
+      },
+      fillEmphasis: {
+        default: '#ffad63',
+        hover: '#ffc791',
+        active: '#ffddb9',
+      },
+    },
+    danger: {
+      canvas: '#0d0a0a',
+      surface: '#2a1e1e',
+      fillMuted: {
+        default: '#923a3c',
+        hover: '#b03940',
+        active: '#d43745',
+      },
+      fillEmphasis: {
+        default: '#ffa3a1',
+        hover: '#ffc1bf',
+        active: '#ffd9d7',
+      },
+    },
+  },
+  border: {
+    neutral: {
+      subtle: '#525c65',
+      medium: '#738696',
+      strong: '#9fb4c6',
+    },
+    accent: {
+      subtle: '#3c6266',
+      medium: '#439199',
+      strong: '#6ec0c9',
+    },
+    success: {
+      subtle: '#3c673a',
+      medium: '#439941',
+      strong: '#6eca6a',
+    },
+    info: {
+      subtle: '#33607e',
+      medium: '#2d8bc5',
+      strong: '#5abbfb',
+    },
+    warning: {
+      subtle: '#7e4e25',
+      medium: '#c36800',
+      strong: '#f99539',
+    },
+    danger: {
+      subtle: '#923a3c',
+      medium: '#e53748',
+      strong: '#ff8082',
+    },
+  },
+  text: {
+    neutral: {
+      subtle: '#d6e3ee',
+      strong: '#f5fdff',
+      subtleOnEmphasis: '#333639',
+      strongOnEmphasis: '#030303',
+    },
+    accent: {
+      subtle: '#bcebf1',
+      strong: '#e6ffff',
+      subtleOnEmphasis: '#2c3839',
+      strongOnEmphasis: '#030303',
+    },
+    success: {
+      subtle: '#bcf2b8',
+      strong: '#e6ffe3',
+      subtleOnEmphasis: '#2c392b',
+      strongOnEmphasis: '#030303',
+    },
+    info: {
+      subtle: '#b7e8ff',
+      strong: '#ecffff',
+      subtleOnEmphasis: '#2a3741',
+      strongOnEmphasis: '#030304',
+    },
+    warning: {
+      subtle: '#ffd4aa',
+      strong: '#fff7e6',
+      subtleOnEmphasis: '#413226',
+      strongOnEmphasis: '#040303',
+    },
+    danger: {
+      subtle: '#ffd0ce',
+      strong: '#fff5f4',
+      subtleOnEmphasis: '#492d2c',
+      strongOnEmphasis: '#040303',
+    },
+  },
+} as const
