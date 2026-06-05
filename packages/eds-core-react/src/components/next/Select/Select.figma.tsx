30077f2b4c871b57c5cb9ea9989e4cc786cad733
camillaknutsen <camilla.knutsen89@gmail.com>
2026-06-05 11:06:28 +0200

    docs(next): add description and placeholder to Select Figma Code Connect

--- packages/eds-core-react/src/components/next/Select/Select.figma.tsx
+++ packages/eds-core-react/src/components/next/Select/Select.figma.tsx
@@ -13,14 +13,29 @@ figma.connect(
         true: 'Label',
         false: undefined,
       }),
+      description: figma.boolean('Title + Description', {
+        true: 'Help with more details',
+        false: undefined,
+      }),
+      placeholder: figma.string('Placeholder'),
       helperMessage: figma.boolean('Helper Message', {
         true: 'Helper message',
         false: undefined,
       }),
     },
-    example: ({ disabled, readOnly, invalid, label, helperMessage }) => (
+    example: ({
+      disabled,
+      readOnly,
+      invalid,
+      label,
+      description,
+      placeholder,
+      helperMessage,
+    }) => (
       <Select
         label={label}
+        description={description}
+        placeholder={placeholder}
         options={['Option 1', 'Option 2', 'Option 3']}
         disabled={disabled}
         readOnly={readOnly}
